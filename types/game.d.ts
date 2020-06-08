3eff8d8bbd7f29dc91faf9db7554d71285009784
FloRad <florad92@gmail.com>
2020-06-08 22:10:31 +0200

    fixed notifications

--- types/game.d.ts
+++ types/game.d.ts
@@ -4,7 +4,7 @@ declare let socket: any;
 declare let canvas: any;
 declare let keyboard: any;
 declare let game: Game;
-declare let ui: { notification: Notifications };
+declare let ui: { notifications: Notifications, tables: any };
 
 /**
  * The core Game instance which encapsulates the data, settings, and states relevant for managing the game experience.
