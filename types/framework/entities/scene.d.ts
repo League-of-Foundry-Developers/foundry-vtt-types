b4fe7124e98ac454d0d235700decd24d357cda25
FloRad <florad92@gmail.com>
2020-07-26 18:55:54 +0200

    Added Properties to settings, chatmessage and Scenes

--- types/framework/entities/scene.d.ts
+++ types/framework/entities/scene.d.ts
@@ -2,12 +2,16 @@
  * The collection of Scene entities
  */
 declare class Scenes extends Collection<Scene> {
-	// @TODO: Declare
+  /**
+   * Return a reference to the Scene which is currently active
+   */
+  active: Scene;
+  // @TODO: Declare
 }
 
 /**
  * The Scene entity
  */
 declare class Scene extends Entity {
-	// @TODO: Declare
+  // @TODO: Declare
 }
