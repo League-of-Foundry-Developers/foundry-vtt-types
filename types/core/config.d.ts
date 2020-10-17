65682ee72033c833005212c0e7c16a74937a22f7
FloRad <florad92@gmail.com>
2020-10-17 21:31:05 +0200

    Added Actor and Item typelabels for Foundry 0.7.4

--- types/core/config.d.ts
+++ types/core/config.d.ts
@@ -1,132 +1,133 @@
 // @TODO: Assign class types
 
 declare const CONFIG: {
-	
 	/**
 	 * Configure debugging flags to display additional information
 	 */
 	debug: {
-		hooks: boolean,
-		sight: boolean
-	},
+		hooks: boolean;
+		sight: boolean;
+	};
 
 	/**
 	 * Configuration for the default Actor entity class
 	 */
 	Actor: {
-		entityClass: typeof Actor,
-		collection: Actors,
-		sheetClasses: typeof ActorSheet
-	},
+		entityClass: typeof Actor;
+		collection: Actors;
+		sheetClasses: typeof ActorSheet;
+		typeLabels: any;
+	};
 
 	/**
 	 * Configuration for the default Item entity class
 	 */
 	Item: {
-		entityClass: typeof Item,
-		collection: Items,
-		sheetClasses: typeof ItemSheet
-	},
+		entityClass: typeof Item;
+		collection: Items;
+		sheetClasses: typeof ItemSheet;
+		typeLabels: any;
+	};
 
 	/**
 	 * Configuration for the JournalEntry entity
 	 */
 	JournalEntry: {
-		entityClass: typeof JournalEntry,
-		sheetClass: typeof JournalSheet,
+		entityClass: typeof JournalEntry;
+		sheetClass: typeof JournalSheet;
 		noteIcons: {
-			"Anchor": string,
-			[key: string]: string
-		},
-		sidebarIcon: string
-	},
+			Anchor: string;
+			[key: string]: string;
+		};
+		sidebarIcon: string;
+	};
 
 	/**
 	 * Configuration for the default Scene entity class
 	 */
 	Scene: {
-		entityClass: typeof Scene,
-		collection: Scenes,
-		sheetClass: any,
-		notesClass: any,
-		sidebarIcon: string
-	},
+		entityClass: typeof Scene;
+		collection: Scenes;
+		sheetClass: any;
+		notesClass: any;
+		sidebarIcon: string;
+	};
 
 	/**
 	 * Configuration for the default Playlist entity class
 	 */
 	Playlist: {
-		entityClass,
-		sheetClass,
-		sidebarIcon: string
-	},
+		entityClass;
+		sheetClass;
+		sidebarIcon: string;
+	};
 
 	/**
 	 * Configuration for RollTable random draws
 	 */
 	RollTable: {
-		entityClass,
-		sheetClass,
-		sidebarIcon: string,
-		resultIcon: string
-	},
+		entityClass;
+		sheetClass;
+		sidebarIcon: string;
+		resultIcon: string;
+	};
 
 	/**
 	 * The control icons used for rendering common HUD operations
 	 */
 	controlIcons: {
-		combat: string,
-		visibility: string,
-		effects: string,
-		lock: string,
-		up: string,
-		down: string,
-		defeated: string,
-		[key: string]: string
-	},
+		combat: string;
+		visibility: string;
+		effects: string;
+		lock: string;
+		up: string;
+		down: string;
+		defeated: string;
+		[key: string]: string;
+	};
 
 	/**
 	 * Suggested font families that are displayed wherever a choice is presented
 	 */
-	fontFamilies: string[],
+	fontFamilies: string[];
 
 	/**
 	 * The default font family used for text labels on the PIXI Canvas
 	 */
-	defaultFontFamily: string,
+	defaultFontFamily: string;
 
 	/**
 	 * Available Weather Effects implemntations
 	 */
-	weatherEffects: any,
+	weatherEffects: any;
 
 	/**
 	 * An array of status effect icons which can be applied to Tokens
 	 */
-	statusEffects: string[],
+	statusEffects: string[];
 
 	/**
 	 * A mapping of core audio effects used which can be replaced by systems or mods
 	 */
 	sounds: {
-		dice: string,
-		lock: string,
-		notification: string,
-		combat: string
-	},
+		dice: string;
+		lock: string;
+		notification: string;
+		combat: string;
+	};
 
 	/**
 	 * Define the set of supported languages for localization
 	 */
 	supportedLanguages: {
-		en: string,
-		[key: string]: string
-	},
+		en: string;
+		[key: string]: string;
+	};
 
 	/**
 	 * Maximum canvas zoom scale
 	 */
-	maxCanvasZoom: number,
-	
+	maxCanvasZoom: number;
+
 	[key: string]: any;
 };
