e5e18df82f426775ad220427a62177baafeb3120
FloRad <florad92@gmail.com>
2020-10-18 18:22:14 +0200

    updated EmbeddedEntity and ActiveEffects

--- types/framework/entities/embeddedentity.d.ts
+++ types/framework/entities/embeddedentity.d.ts
@@ -1,5 +1,54 @@
 declare abstract class EmbeddedEntity {
 	constructor(data: any, parent: Entity);
+
+	/**
+	 * The embedded entity data object
+	 */
+	data: any;
+
+	/**
+	 * The parent Entity to which this belongs
+	 */
+	parent: Entity;
+
+	/**
+	 * Assign a "flag" to this EmbeddedEntity.
+	 * Flags represent key-value type data which can be used to store flexible or arbitrary data required by either
+	 * the core software, game systems, or user-created modules.
+	 *
+	 * Each flag should be set using a scope which provides a namespace for the flag to help prevent collisions.
+	 *
+	 * Flags set by the core software use the "core" scope.
+	 * Flags set by game systems or modules should use the canonical name attribute for the module
+	 * Flags set by an individual world should "world" as the scope.
+	 *
+	 * Flag values can assume almost any data type. Setting a flag value to null will delete that flag.
+	 *
+	 * @param {string} scope    The flag scope which namespaces the key
+	 * @param {string} key      The flag key
+	 * @param {*} value         The flag value
+	 *
+	 * @return {Promise}        A Promise resolving to the updated EmbeddedEntity
+	 */
+	setFlag(scope: string, key: string, value: any): Promise<EmbeddedEntity>;
+
+	/**
+	 * Get the value of a "flag" for this EmbeddedEntity
+	 * See the setFlag method for more details on flags
+	 *
+	 * @param {string} scope    The flag scope which namespaces the key
+	 * @param {string} key      The flag key
+	 * @return {*}              The flag value
+	 */
+	getFlag(scope: string, key: string): any;
+
+	/**
+	 * Remove a flag assigned to the EmbeddedEntity
+	 * @param {string} scope    The flag scope which namespaces the key
+	 * @param {string} key      The flag key
+	 * @return {Promise}        A Promise resolving to the updated Entity
+	 */
+	unsetFlag(scope: string, key: string): Promise<EmbeddedEntity>;
 }
 
 /**
@@ -25,7 +74,7 @@ declare class ActiveEffect extends EmbeddedEntity {
 	 * A convenience method for creating an ActiveEffect instance within a parent Actor or Item.
 	 * @see {@link Entity#createEmbeddedEntity}
 	 * @param {Options} options     Configuration options which modify the request.
-	 * @return                       The created ActiveEffect data.
+	 * @return                      The created ActiveEffect data.
 	 */
 	create(options: any): Promise<any>;
 
@@ -36,7 +85,7 @@ declare class ActiveEffect extends EmbeddedEntity {
 	 * @param {Options} options     Configuration options which modify the request.
 	 * @return                      The updated ActiveEffect data.
 	 */
-	update(data, options: any): Promise<any>;
+	update(data, options?: any): Promise<any>;
 
 	/**
 	 * A convenience method for deleting an ActiveEffect instance in an parent Actor or Item.
@@ -44,7 +93,7 @@ declare class ActiveEffect extends EmbeddedEntity {
 	 * @param {Options} options     Configuration options which modify the request.
 	 * @return                      The deleted ActiveEffect _id.
 	 */
-	delete(options: any): Promise<string>;
+	delete(options?: any): Promise<string>;
 
 	/**
 	 * A factory method which creates an ActiveEffect instance using the configured class.
