3a5d4b2ee1a8e4902aae5fb184cc08d7275f49ea
Kai Moschcau <mail@kmoschcau.de>
2020-12-29 17:16:02 +0100

    Update audio and add Howler types dependency

--- types/core/audio.d.ts
+++ types/core/audio.d.ts
@@ -1,6 +1,12 @@
+import { Howl } from 'howler'
+
 /**
  * A helper class to provide common functionality for working with HTML5 audio and Howler instances
  * A singleton instance of this class is available as ``game.audio``
+ *
+ * Audio playback in Foundry VTT is managed by Howler.js (https://howlerjs.com/). Several methods and
+ * attributes in this API return :class:`Howl` instances. See the Howler documentation for details
+ * and example usage of the Howl API.
  */
 declare class AudioHelper {
 	/** The set of Howl instances which have been created for different audio paths */
@@ -11,20 +17,52 @@ declare class AudioHelper {
 	 * This Array contains the Howl instances which are requested for playback prior to a gesture.
 	 * Once a gesture is observed, we begin playing all elements of this Array.
 	 */
-	pending: any[];
+	pending: Howl[];
 
-	/** A flag for whether video playback is currently locked by awaiting a user gesture */
+	/**
+	 * A flag for whether video playback is currently locked by awaiting a user gesture
+	 */
 	locked: boolean;
+
+	/**
+	 * The Native interval for the AudioHelper to analyse audio levels from streams
+	 * Any interval passed to startLevelReports() would need to be a multiple of this value.
+	 * Defaults to 50ms.
+	 */
+	levelAnalyserNativeInterval: 50;
 
 	constructor();
 
+	/**
+	 * Register client-level settings for global volume overrides
+	 */
 	static registerSettings(): void;
 
 	/**
-	 * Create a Howl instance
+	 * Create a Howl instance for a given audio source URL
 	 */
-	create({ src, preload, autoplay, volume, loop}:
-		{ src: string, preload: boolean, autoplay: boolean, volume: number, loop: boolean}): void;
+	create({
+		src,
+		preload,
+		autoplay,
+		html5,
+		volume,
+		loop
+	}: {
+		src: string,
+		preload: boolean,
+		autoplay: boolean,
+		html5: boolean
+		volume: number,
+		loop: boolean
+	}): Howl;
+
+	/**
+	 * Test whether a source file has a supported audio extension type
+	 * @param src      A requested audio source path
+	 * @return Does the filename end with a valid audio extension?
+	 */
+	static hasAudioExtension(src: string): boolean;
 
 	/**
 	 * Play a single audio effect by it's source path and Howl ID
@@ -35,6 +73,12 @@ declare class AudioHelper {
 	 * Register an event listener to await the first mousemove gesture and begin playback once observed
 	 */
 	awaitFirstGesture(): void;
+
+	/**
+	* Handle the first observed user gesture
+	* @param event   The mouse-move event which enables playback
+	*/
+	_onFirstGesture(event: Event): void;
 
 	preload(data: any): void;
 	
@@ -54,8 +98,15 @@ declare class AudioHelper {
 	 * // Play the sound of a locked door for all players
 	 * Audio.play({src: "sounds/lock.wav", volume: 0.8, autoplay: true, loop: false}, true);
 	 */
-	static play(data: { src: string, autoplay: boolean, volume: number, loop: boolean},
-		push: boolean): void;
+	static play(
+		data: {
+			src: string,
+			volume: number,
+			autoplay: boolean,
+			loop: boolean
+		},
+		push: boolean
+	): Howl;
 
 	/**
 	 * Create a Howl object and load it to be ready for later playback
@@ -66,18 +117,54 @@ declare class AudioHelper {
 	/**
 	 * Returns the volume value based on a range input volume control's position.
 	 * This is using an exponential approximation of the logarithmic nature of audio level perception
-	 * Based on https://www.dr-lex.be/info-stuff/volumecontrols.html
-	 * We're using x^3 by default instead of x^4 otherwise the audio becomes nearly silent around the 40% mark.
-	 * @param control	Value between [0, 1] of the range input
-	 * @param order		(optional) the exponent of the curve (default: 3)
+	 * @param value   Value between [0, 1] of the range input
+	 * @param order          [optional] the exponent of the curve (default: 1.5)
 	 */
-	static inputToVolume(control: number, order: number): number;
+	static inputToVolume(control: number | string, order: number): number;
 
 	/**
 	 * Counterpart to inputToVolume()
 	 * Returns the input range value based on a volume
-	 * @param control	Value between [0, 1] of the volume level
-	 * @param order		(optional) the exponent of the curve (default: 3)
+	 * @param volume     Value between [0, 1] of the volume level
+	 * @param order      [optional] the exponent of the curve (default: 1.5)
 	 */
 	static volumeToInput(volume: number, order: number): number;
+
+	/**
+	 * Returns a singleton AudioContext if one can be created.
+	 * An audio context may not be available due to limited resources or browser compatibility
+	 * in which case null will be returned
+	 *
+	 * @return A singleton AudioContext or null if one is not available
+	 */
+	getAudioContext(): AudioContext | null
+
+	/**
+	 * Registers a stream for periodic reports of audio levels.
+	 * Once added, the callback will be called with the maximum decibel level of
+	 * the audio tracks in that stream since the last time the event was fired.
+	 * The interval needs to be a multiple of AudioHelper.levelAnalyserNativeInterval which defaults at 50ms
+	 *
+	 * @param id             An id to assign to this report. Can be used to stop reports
+	 * @param stream         The MediaStream instance to report activity on.
+	 * @param callback       The callback function to call with the decibel level. `callback(dbLevel)`
+	 * @param interval       (optional) The interval at which to produce reports.
+	 * @param smoothing      (optional) The smoothingTimeConstant to set on the audio analyser. Refer to AudioAnalyser API docs.
+	 * @return Returns whether or not listening to the stream was successful
+	 */
+	startLevelReports(
+		id: string,
+		stream: MediaStream,
+		callback: (maxDecibel: number, fftArray: Float32Array) => void,
+		interval: 50,
+		smoothing: 0.1
+	): boolean;
+
+	/**
+	 * Stop sending audio level reports
+	 * This stops listening to a stream and stops sending reports.
+	 * If we aren't listening to any more streams, cancel the global analyser timer.
+	 * @param id      The id of the reports that passed to startLevelReports.
+	 */
+	stopLevelReports(id: string): void;
 }
