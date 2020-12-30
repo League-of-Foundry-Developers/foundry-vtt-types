eef0eed78ce2b7c507ef459ccc735ad330de6033
Kai Moschcau <mail@kmoschcau.de>
2020-12-30 16:49:01 +0100

    Update fonts.d.ts

--- types/core/fonts.d.ts
+++ types/core/fonts.d.ts
@@ -1,7 +1,7 @@
-
 /**
  * Load font, and perform a callback once the font has been rendered
+ * @deprecated since 0.6.4, to be removed in 0.8.x
  * @param fontName
  * @param callback
  */
-declare function loadFont(fontName: string, callback: Function): void;
+declare function loadFont(fontName: string, callback: () => void): void;
