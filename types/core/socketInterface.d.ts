9170be2acc17cecbbb0ca32b08c2e2908eaf34cf
Kai Moschcau <mail@kmoschcau.de>
2021-01-05 22:56:02 +0100

    Add SocketInterface
    This adds two undefined types, which have to be resolved later
    - SocketRequest
    - SocketResponse

---
+++ types/core/socketInterface.d.ts
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+declare class SocketInterface {
+  /**
+   * Handle an error returned from the database, displaying it on screen and in
+   * the console
+   * @param err - The provided Error message
+   * @internal
+   */
+  static _handleError (err: Error): Error
+
+  /**
+   * Standardize the way that socket messages are dispatched and their results
+   * are handled
+   * @param eventName - The socket event name being handled
+   * @param request - Data provided to the Socket event
+   * @returns A Promise which resolves to the SocketResponse
+   */
+  static dispatch (
+    eventName: string,
+    request: SocketRequest
+  ): Promise<SocketResponse>
+}
