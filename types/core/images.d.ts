e63cbfab3bf072ff27ffa9bd20ee74acccd64f8e
Kai Moschcau <mail@kmoschcau.de>
2021-01-05 19:26:55 +0100

    Standardize images.d.ts

--- types/core/images.d.ts
+++ types/core/images.d.ts
@@ -1,90 +1,112 @@
 /**
- * An interface for options for the `ImageHelper.createThumbnail()` and
- * `ImageHelper.compositeCanvasTexture()` methods.
+ * An interface for options for the {@link ImageHelper.createThumbnail} and
+ * {@link ImageHelper.compositeCanvasTexture} methods.
  */
 declare interface CompositeOptions {
-	/**
-	 * The desired width of the output texture
-	 */
-	width?: number,
+  /**
+   * Center the texture in the rendered frame?
+   * @defaultValue `true`
+   */
+  center?: boolean
 
-	/**
-	 * The desired height of the output texture
-	 */
-	height?: number,
+  /**
+   * The desired height of the output texture
+   * @defaultValue The height of the object passed to
+   *               {@link ImageHelper.compositeCanvasTexture}
+   */
+  height?: number
 
-	/**
-	 * A horizontal translation to apply to the object
-	 */
-	tx?: number,
+  /**
+   * A horizontal translation to apply to the object
+   * @defaultValue `0`
+   */
+  tx?: number
 
-	/**
-	 * A vertical translation to apply to the object
-	 */
-	ty?: number,
+  /**
+   * A vertical translation to apply to the object
+   * @defaultValue `0`
+   */
+  ty?: number
 
-	/**
-	 * Center the texture in the rendered frame?
-	 * @defaultValue `true`
-	 */
-	center?: boolean
+  /**
+   * The desired width of the output texture
+   * @defaultValue The width of the object passed to
+   *               {@link ImageHelper.compositeCanvasTexture}
+   */
+  width?: number
 }
 
 /**
- * An interface for return values of the `ImageHelper.createThumbnail()` method.
+ * An interface for return values of the {@link ImageHelper.createThumbnail}
+ * method.
  */
 declare interface ThumbnailReturn {
-	/**
-	 * The originally passed `string` URL or `PIXI.DisplayObject`
-	 */
-	src: string | PIXI.DisplayObject,
+  /**
+   * The height of the {@link PIXI.Sprite}, created by
+   * {@link ImageHelper.createThumbnail}
+   */
+  height: number
 
-	/**
-	 * The `PIXI.Texture,` returned from `compositeCanvasTexture()`, with `destroy(true)` already called on it.
-	 */
-	texture: PIXI.Texture,
+  /**
+   * The originally passed `string` URL or {@link PIXI.DisplayObject}
+   */
+  src: string | PIXI.DisplayObject
 
-	/**
-	 * The base64 encoded image data, returned from `textureToImage()`
-	 */
-	thumb: string,
+  /**
+   * The {@link PIXI.Texture}, returned from
+   * {@link ImageHelper.compositeCanvasTexture}, with `destroy(true)` already
+   * called on it.
+   */
+  texture: PIXI.Texture
 
-	/**
-	 * The width of the `PIXI.Sprite`, created by `createThumbnail()`
-	 */
-	width: number,
+  /**
+   * The base64 encoded image data, returned from
+   * {@link ImageHelper.textureToImage}
+   */
+  thumb: string
 
-	/**
-	 * The height of the `PIXI.Sprite`, created by `createThumbnail()`
-	 */
-	height: number
+  /**
+   * The width of the {@link PIXI.Sprite}, created by
+   * {@link ImageHelper.createThumbnail}
+   */
+  width: number
 }
 
 /**
  * A helper class to provide common functionality for working with Image objects
  */
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 declare class ImageHelper {
-	/**
-	 * Create thumbnail preview for a provided image path.
-	 * @param src The string URL or `PIXI.DisplayObject` of the texture to render to a thumbnail
-	 * @param options Additional named options passed to the `compositeCanvasTexture()` method; defaults to `{}`
-	 * @return The parsed and converted thumbnail data
-	 */
-	static createThumbnail(src: string | PIXI.DisplayObject, options?: CompositeOptions): Promise<ThumbnailReturn>;
+  /**
+   * Composite a canvas object by rendering it to a single texture
+   * @param object - The DisplayObject to render to a texture
+   * @param options - Additional named options
+   *                  (default: `{}`)
+   * @returns The composite Texture object
+   */
+  static compositeCanvasTexture (
+    object: PIXI.DisplayObject,
+    options?: CompositeOptions
+  ): PIXI.Texture
 
-	/**
-	 * Composite a canvas object by rendering it to a single texture
-	 *
-	 * @param object The `PIXI.DisplayObject` to render to a texture
-	 * @param options Additional named options; defaults to `{}`
-	 * @return The composite Texture object
-	 */
-	static compositeCanvasTexture(object: PIXI.DisplayObject, options?: CompositeOptions): PIXI.Texture;
+  /**
+   * Create thumbnail preview for a provided image path.
+   * @param src - The string URL or DisplayObject of the texture to render to a
+   *              thumbnail
+   * @param options - Additional named options passed to the
+   *                  {@link ImageHelper.compositeCanvasTexture} method
+   *                  (default: `{}`)
+   * @returns The parsed and converted thumbnail data
+   */
+  static createThumbnail (
+    src: string | PIXI.DisplayObject,
+    options?: CompositeOptions
+  ): Promise<ThumbnailReturn>
 
-	/**
-	 * Extract a texture to a base64 PNG string
-	 * @param texture The PIXI.Texture object to extract
-	 * @return A base64 png string of the texture
-	 */
-	static textureToImage(texture: PIXI.Texture): string;
+  /**
+   * Extract a texture to a base64 PNG string
+   * @param texture - The Texture object to extract
+   * @returns A base64 png string of the texture
+   */
+  static textureToImage (texture: PIXI.Texture): string
 }
