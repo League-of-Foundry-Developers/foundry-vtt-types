0389bf3ec7ad9f4f1a3ff8cd804dd8d005e55f42
Kai Moschcau <mail@kmoschcau.de>
2021-01-07 20:30:19 +0100

    Add gameTime.d.ts

---
+++ types/core/gameTime.d.ts
@@ -0,0 +1,74 @@
+declare interface Timestamps {
+  clientTime: number
+  serverTime: number
+  worldTime: number
+}
+
+/**
+ * A singleton class {@link game#time} which keeps the official Server and World
+ * time stamps.
+ * Uses a basic implementation of
+ * https://www.geeksforgeeks.org/cristians-algorithm/ for synchronization.
+ */
+declare class GameTime {
+  /**
+   * The amount of time to delay before re-syncing the official server time.
+   * @defaultValue `1000 * 60 * 5`
+   */
+  SYNC_INTERVAL_MS: number
+
+  /**
+   * The average one-way latency across the most recent 5 trips
+   * @defaultValue `0`
+   */
+  _dt: number
+
+  /**
+   * The most recent five synchronization durations
+   * @defaultValue `[]`
+   */
+  _dts: number[]
+
+  /**
+   * The most recently synchronized timestamps retrieved from the server.
+   * @defaultValue `{}`
+   */
+  _time: Timestamps
+
+  /**
+   * @param socket - Originally documented as type `Socket`.
+   */
+  constructor (socket: WebSocket)
+
+  /**
+   * The current server time based on the last synchronization point and the
+   * approximated one-way latency.
+   */
+  get serverTime (): number
+
+  /**
+   * The current World time based on the last recorded value of the core.time
+   * setting
+   */
+  get worldTime (): number
+
+  /**
+   * Advance the game time by a certain number of seconds
+   * @param seconds - The number of seconds to advance (or rewind if negative)
+   *                  by
+   * @returns The new game time
+   */
+  advance (seconds: number): Promise<number>
+
+  /**
+   * Handle follow-up actions when the official World time is changed
+   * @param worldTime - The new canonical World time.
+   */
+  onUpdateWorldTime (worldTime: number): void
+
+  /**
+   * Synchronize the local client game time with the official time kept by the
+   * server
+   */
+  sync (socket: WebSocket): Promise<GameTime>
+}
