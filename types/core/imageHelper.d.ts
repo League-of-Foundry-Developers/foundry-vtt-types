4c8ba993a45500c0c059801ea68f0fe7d3050317
Kai Moschcau <mail@kmoschcau.de>
2021-01-10 19:38:28 +0100

    Standardize documentation of external types

--- types/core/imageHelper.d.ts
+++ types/core/imageHelper.d.ts
@@ -6,9 +6,11 @@ declare class ImageHelper {
   /**
    * Composite a canvas object by rendering it to a single texture
    * @param object - The {@link PIXI.DisplayObject} to render to a texture
+   *                 (type: `PIXI.DisplayObject`)
    * @param options - Additional named options
    *                  (default: `{}`)
    * @returns The composite {@link PIXI.Texture} object
+   *          (type: `PIXI.Texture`)
    */
   static compositeCanvasTexture (
     object: PIXI.DisplayObject,
@@ -19,6 +21,7 @@ declare class ImageHelper {
    * Create thumbnail preview for a provided image path.
    * @param src - The string URL or {@link PIXI.DisplayObject} of the texture to
    *              render to a thumbnail
+   *              (type: `PIXI.DisplayObject`)
    * @param options - Additional named options passed to the
    *                  {@link ImageHelper.compositeCanvasTexture} method
    *                  (default: `{}`)
@@ -32,6 +35,7 @@ declare class ImageHelper {
   /**
    * Extract a texture to a base64 PNG string
    * @param texture - The {@link PIXI.Texture} object to extract
+   *                  (type: `PIXI.Texture`)
    * @returns A base64 png string of the texture
    */
   static textureToImage (texture: PIXI.Texture): string
@@ -89,6 +93,7 @@ declare namespace ImageHelper {
 
     /**
      * The originally passed `string` URL or {@link PIXI.DisplayObject}
+     * (type: `string | PIXI.DisplayObject`)
      */
     src: string | PIXI.DisplayObject
 
@@ -96,6 +101,7 @@ declare namespace ImageHelper {
      * The {@link PIXI.Texture}, returned from
      * {@link ImageHelper.compositeCanvasTexture}, with `destroy(true)` already
      * called on it.
+     * (type: `PIXI.Texture`)
      */
     texture: PIXI.Texture
 
--- types/templateUtils.d.ts
+++ types/templateUtils.d.ts
@@ -1,26 +1,28 @@
 /**
  * Global template cache
- * (Record\<string, HandlebarsTemplateDelegate\>)
+ * (type: `Record<string, Handlebars.TemplateDelegate>`)
  */
-declare let _templateCache: Record<string, HandlebarsTemplateDelegate>
+declare let _templateCache: Record<string, Handlebars.TemplateDelegate>
 
 /**
  * Get a template from the server by fetch request and caching the retrieved
  * result
  * @param path - The web-accessible HTML template URL
  * @returns A Promise which resolves to the compiled Handlebars template
- *          (Promise\<HandlebarsTemplateDelegate\>)
+ *          (type: `Promise<Handlebars.TemplateDelegate>`)
  */
-declare function getTemplate (path: string): Promise<HandlebarsTemplateDelegate>
+declare function getTemplate (
+  path: string
+): Promise<Handlebars.TemplateDelegate>
 
 /**
  * Load and cache a set of templates by providing an Array of paths
  * @param paths - An array of template file paths to load
- * @returns (Promise\<HandlebarsTemplateDelegate[]\>)
+ * @returns (type: `Promise<Handlebars.TemplateDelegate[]>`)
  */
 declare function loadTemplates (
   paths: string[]
-): Promise<HandlebarsTemplateDelegate[]>
+): Promise<Handlebars.TemplateDelegate[]>
 
 /**
  * Get and render a template using provided data and handle the returned HTML
