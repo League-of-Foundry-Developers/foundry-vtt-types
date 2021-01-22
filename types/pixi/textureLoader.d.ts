385cf440fe890c4c959e0c2de8ad20ce5794ff2c
Kai Moschcau <mail@kmoschcau.de>
2021-01-22 15:09:32 +0100

    Add two missing global methods for TextureLoader

--- types/pixi/textureLoader.d.ts
+++ types/pixi/textureLoader.d.ts
@@ -1,3 +1,19 @@
+/* -------------------------------------------- */
+
+/**
+ * Test whether a file source exists by performing a HEAD request against it
+ * @param src - The source URL or path to test
+ * @returns Does the file exist at the provided url?
+ */
+declare function srcExists (src: string): boolean
+
+/* -------------------------------------------- */
+
+/**
+ * Get a single texture from the cache
+ */
+declare function getTexture (src: string): PIXI.Texture
+
 /**
  * Load a single texture and return a Promise which resolves once the texture is ready to use
  * @param src - The requested texture source
