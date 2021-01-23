50425fb88d85a85b64f8c36ce90a95e004accc97
Kai Moschcau <mail@kmoschcau.de>
2021-01-23 17:44:00 +0100

    Reformat ImageHelper

--- types/core/imageHelper.d.ts
+++ types/core/imageHelper.d.ts
@@ -4,8 +4,22 @@
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 declare class ImageHelper {
   /**
+   * Create thumbnail preview for a provided image path.
+   * @param src     - The string URL or DisplayObject of the texture to render to a thumbnail
+   * @param options - Additional named options passed to the {@link ImageHelper.compositeCanvasTexture} method
+   *                  (default: `{}`)
+   * @returns The parsed and converted thumbnail data
+   */
+  static createThumbnail (
+    src: string | PIXI.DisplayObject,
+    options?: ImageHelper.CompositeOptions
+  ): Promise<ImageHelper.ThumbnailReturn>
+
+  /* -------------------------------------------- */
+
+  /**
    * Composite a canvas object by rendering it to a single texture
-   * @param object - The DisplayObject to render to a texture
+   * @param object  - The DisplayObject to render to a texture
    * @param options - Additional named options
    *                  (default: `{}`)
    * @returns The composite Texture object
@@ -15,19 +29,7 @@ declare class ImageHelper {
     options?: ImageHelper.CompositeOptions
   ): PIXI.Texture
 
-  /**
-   * Create thumbnail preview for a provided image path.
-   * @param src - The string URL or DisplayObject of the texture to render to a
-   *              thumbnail
-   * @param options - Additional named options passed to the
-   *                  {@link ImageHelper.compositeCanvasTexture} method
-   *                  (default: `{}`)
-   * @returns The parsed and converted thumbnail data
-   */
-  static createThumbnail (
-    src: string | PIXI.DisplayObject,
-    options?: ImageHelper.CompositeOptions
-  ): Promise<ImageHelper.ThumbnailReturn>
+  /* -------------------------------------------- */
 
   /**
    * Extract a texture to a base64 PNG string
@@ -39,8 +41,8 @@ declare class ImageHelper {
 
 declare namespace ImageHelper {
   /**
-   * An interface for options for the {@link ImageHelper.createThumbnail} and
-   * {@link ImageHelper.compositeCanvasTexture} methods.
+   * An interface for options for the {@link ImageHelper.createThumbnail} and {@link ImageHelper.compositeCanvasTexture}
+   * methods.
    */
   interface CompositeOptions {
     /**
@@ -51,8 +53,7 @@ declare namespace ImageHelper {
 
     /**
      * The desired height of the output texture
-     * @defaultValue The height of the object passed to
-     *               {@link ImageHelper.compositeCanvasTexture}
+     * @defaultValue The height of the object passed to {@link ImageHelper.compositeCanvasTexture}
      */
     height?: number
 
@@ -70,20 +71,17 @@ declare namespace ImageHelper {
 
     /**
      * The desired width of the output texture
-     * @defaultValue The width of the object passed to
-     *               {@link ImageHelper.compositeCanvasTexture}
+     * @defaultValue The width of the object passed to {@link ImageHelper.compositeCanvasTexture}
      */
     width?: number
   }
 
   /**
-   * An interface for return values of the {@link ImageHelper.createThumbnail}
-   * method.
+   * An interface for return values of the {@link ImageHelper.createThumbnail} method.
    */
   interface ThumbnailReturn {
     /**
-     * The height of the {@link PIXI.Sprite}, created by
-     * {@link ImageHelper.createThumbnail}
+     * The height of the {@link PIXI.Sprite}, created by {@link ImageHelper.createThumbnail}
      */
     height: number
 
@@ -93,21 +91,17 @@ declare namespace ImageHelper {
     src: string | PIXI.DisplayObject
 
     /**
-     * The Texture, returned from
-     * {@link ImageHelper.compositeCanvasTexture}, with `destroy(true)` already
-     * called on it.
+     * The Texture, returned from {@link ImageHelper.compositeCanvasTexture}, with `destroy(true)` already called on it.
      */
     texture: PIXI.Texture
 
     /**
-     * The base64 encoded image data, returned from
-     * {@link ImageHelper.textureToImage}
+     * The base64 encoded image data, returned from {@link ImageHelper.textureToImage}
      */
     thumb: string
 
     /**
-     * The width of the {@link PIXI.Sprite}, created by
-     * {@link ImageHelper.createThumbnail}
+     * The width of the {@link PIXI.Sprite}, created by {@link ImageHelper.createThumbnail}
      */
     width: number
   }
