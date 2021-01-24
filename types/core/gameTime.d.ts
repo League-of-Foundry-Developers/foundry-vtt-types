af6b27408c7e2e4af605b7ffd492937dca46df8e
Kai Moschcau <mail@kmoschcau.de>
2021-01-24 14:22:49 +0100

    Reformat GameTime

--- types/core/gameTime.d.ts
+++ types/core/gameTime.d.ts
@@ -1,15 +1,15 @@
 /**
- * A singleton class {@link game#time} which keeps the official Server and World
- * time stamps.
- * Uses a basic implementation of
- * https://www.geeksforgeeks.org/cristians-algorithm/ for synchronization.
+ * A singleton class {@link game#time} which keeps the official Server and World time stamps.
+ * Uses a basic implementation of https://www.geeksforgeeks.org/cristians-algorithm/ for synchronization.
  */
 declare class GameTime {
+  constructor (socket: SocketIOClient.Socket)
+
   /**
-   * The amount of time to delay before re-syncing the official server time.
-   * @defaultValue `1000 * 60 * 5`
+   * The most recently synchronized timestamps retrieved from the server.
+   * @defaultValue `{}`
    */
-  SYNC_INTERVAL_MS: number
+  _time: GameTime.Timestamps
 
   /**
    * The average one-way latency across the most recent 5 trips
@@ -23,33 +23,43 @@ declare class GameTime {
    */
   _dts: number[]
 
-  /**
-   * The most recently synchronized timestamps retrieved from the server.
-   * @defaultValue `{}`
-   */
-  _time: GameTime.Timestamps
-
-  constructor (socket: SocketIOClient.Socket)
+  /* -------------------------------------------- */
+  /*  Properties                                  */
+  /* -------------------------------------------- */
 
   /**
-   * The current server time based on the last synchronization point and the
-   * approximated one-way latency.
+   * The current server time based on the last synchronization point and the approximated one-way latency.
    */
   get serverTime (): number
 
+  /* -------------------------------------------- */
+
   /**
-   * The current World time based on the last recorded value of the core.time
-   * setting
+   * The current World time based on the last recorded value of the core.time setting
    */
   get worldTime (): number
 
+  /* -------------------------------------------- */
+  /*  Methods                                     */
+  /* -------------------------------------------- */
+
   /**
    * Advance the game time by a certain number of seconds
-   * @param seconds - The number of seconds to advance (or rewind if negative)
-   *                  by
+   * @param seconds - The number of seconds to advance (or rewind if negative) by
    * @returns The new game time
    */
   advance (seconds: number): Promise<number>
+
+  /* -------------------------------------------- */
+
+  /**
+   * Synchronize the local client game time with the official time kept by the server
+   */
+  sync (socket: SocketIOClient.Socket): Promise<GameTime>
+
+  /* -------------------------------------------- */
+  /*  Event Handlers and Callbacks                */
+  /* -------------------------------------------- */
 
   /**
    * Handle follow-up actions when the official World time is changed
@@ -58,10 +68,10 @@ declare class GameTime {
   onUpdateWorldTime (worldTime: number): void
 
   /**
-   * Synchronize the local client game time with the official time kept by the
-   * server
+   * The amount of time to delay before re-syncing the official server time.
+   * @defaultValue `1000 * 60 * 5`
    */
-  sync (socket: SocketIOClient.Socket): Promise<GameTime>
+  static SYNC_INTERVAL_MS: number
 }
 
 declare namespace GameTime {
