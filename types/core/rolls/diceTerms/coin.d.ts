4a2041b3f30b1a636e140cb6198e922525073bb7
Kai Moschcau <mail@kmoschcau.de>
2021-01-28 21:53:53 +0100

    Reformat Coin

--- types/core/rolls/diceTerms/coin.d.ts
+++ types/core/rolls/diceTerms/coin.d.ts
@@ -2,6 +2,38 @@
  * Define a two-sided coin term that can be used as part of a Roll formula
  */
 declare class Coin extends DiceTerm {
+  constructor(termData?: DiceTerm.TermData);
+
+  /* -------------------------------------------- */
+
+  /**
+   * @override
+   */
+  roll(options?: { maximize: boolean; minimize: boolean }): DiceTerm.Result;
+
+  /* -------------------------------------------- */
+
+  /**
+   * @override
+   */
+  static getResultLabel(result: string): string;
+
+  /* -------------------------------------------- */
+  /*  Term Modifiers                              */
+  /* -------------------------------------------- */
+
+  /**
+   * Call the result of the coin flip, marking any coins that matched the called target as a success
+   *
+   * 3dcc1      Flip 3 coins and treat "heads" as successes
+   * 2dcc0      Flip 2 coins and treat "tails" as successes
+   *
+   * @param modifier - The matched modifier query
+   */
+  call(modifier: string): string;
+
+  /* -------------------------------------------- */
+
   /**
    * @defaultValue `'c'`
    */
@@ -11,25 +43,4 @@ declare class Coin extends DiceTerm {
    * @defaultValue `{ c: 'call' }`
    */
   static MODIFIER: Record<string, string>;
-
-  constructor(termData?: DiceTerm.TermData);
-
-  /**
-   * @override
-   */
-  static getResultLabel(result: string): string;
-
-  /**
-   * Call the result of the coin flip, marking any coins that matched the called
-   * target as a success
-   * 3dcc1 - Flip 3 coins and treat "heads" as successes
-   * 2dcc0 - Flip 2 coins and treat "tails" as successes
-   * @param modifier - The matched modifier query
-   */
-  call(modifier: string): string;
-
-  /**
-   * @override
-   */
-  roll(options?: { maximize: boolean; minimize: boolean }): DiceTerm.Result;
 }
