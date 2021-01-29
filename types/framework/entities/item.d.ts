0fcf9dd6b178bd84759ca7aa9e3af5331d18950f
Johannes Loher <johannes.loher@fg4f.de>
2021-01-29 10:31:18 +0100

    Get rid of circular reference

--- types/framework/entities/item.d.ts
+++ types/framework/entities/item.d.ts
@@ -84,7 +84,7 @@ declare class Item<D extends Item.Data = Item.Data<any>> extends Entity<D> {
   /**
    * A convenience reference to the Actor entity which owns this item, if any
    */
-  get actor(): Actor<this, Actor.Data<any, Actor.OwnedItem<this>>> | null;
+  get actor(): Actor<this, Actor.Data<any, D>> | null;
 
   /**
    * A convenience reference to the image path (data.img) used to represent this Item
