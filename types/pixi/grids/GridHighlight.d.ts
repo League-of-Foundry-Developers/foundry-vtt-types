24d0ad21bf04628f6bada83d7b8bbb9d99000eb4
Peter <petgu214@gmail.com>
2021-02-02 00:13:53 +0100

    Fixed Eslint comments and filled out hexagonalGrid

--- types/pixi/grids/GridHighlight.d.ts
+++ types/pixi/grids/GridHighlight.d.ts
@@ -1,53 +1,51 @@
 /**
  * A special Graphics class which handles Grid layer highlighting
- * @type {PIXI.Graphics}
  */
 declare class GridHighlight extends PIXI.Graphics {
-    
-    /**
+  /**
      * Track the Grid Highlight name
-     * @type {string}
      */
-    name: string
+  name: string
 
-    /**
+  /**
      * Track distinct positions which have already been highlighted
-     * @type {Set}
      */
-    positions: Set
+  positions: Set<any>
 
-    /* -------------------------------------------- */
+  constructor (name: any, ...args: any[])
+  /* -------------------------------------------- */
 
   /**
    * Record a position that is highlighted and return whether or not it should be rendered
-   * @param {number} x    The x-coordinate to highlight
-   * @param {number} y    The y-coordinate to highlight
-   * @return {boolean}    Whether or not to draw the highlight for this location
+   * @param x - The x-coordinate to highlight
+   *            (type: `number`)
+   * @param y - The y-coordinate to highlight
+   *            (type: `number`)
+   * @returns  - Whether or not to draw the highlight for this location
+   *            (type: `boolean`)
    */
-  highlight(
-      x: number
-      y: number
-    ): boolean
+  highlight (
+    x: number,
+    y: number
+  ): boolean
 
-    /* -------------------------------------------- */
+  /* -------------------------------------------- */
 
   /**
    * Extend the Graphics clear logic to also reset the highlighted positions
-   * @param args
+   * @param args - (type: `any[]`)
    */
-  clear(
-      ...args: any[]
-  ): void
+  clear (
+    ...args: any[]
+  ): any
 
   /* -------------------------------------------- */
 
   /**
    * Extend how this Graphics container is destroyed to also remove parent layer references
-   * @param args
+   * @param args - (type: `any[]`)
    */
-  destroy(
-      ...args any[]
-  )
-
-  constructor (name: any, ...args: any[])
+  destroy (
+    ...args: any[]
+  ): any
 }
--- types/pixi/grids/baseGrid.d.ts
+++ types/pixi/grids/baseGrid.d.ts
@@ -2,49 +2,60 @@
  * The base grid class.
  * This double-dips to implement the "gridless" option
  */
-declare class BaseGrid extends PIXI.Container{
-
-    /**
+declare class BaseGrid extends PIXI.Container {
+  /**
      * Grid Unit Width
      */
-    w: any
+  w: any
 
-    /**
+  /**
      * Grid Unit Height
      */
-    h: any
+  h: any
 
-    /**
+  /**
      * Highlight active grid spaces
-     * @type {Object}
      */
-    highlight: object
+  highlight: object
 
-    /**
+  constructor (options: PIXI.Container)
+  /**
      * TODO: comment what this does
      */
-    draw (): this
+  draw (): this
 
-    /**
+  /**
    * Highlight a grid position for a certain coordinates
-   * @param {GridHighlight} layer The highlight layer to use
-   * @param {number} x            The x-coordinate of the highlighted position
-   * @param {number} y            The y-coordinate of the highlighted position
-   * @param {number} color        The hex fill color of the highlight
-   * @param {number} border       The hex border color of the highlight
-   * @param {number} alpha        The opacity of the highlight
-   * @param {PIXI.Polygon} shape  A predefined shape to highlight
-   *                              (type: ``PIXI.Polygon``) 
+   * @param layer  - The highlight layer to use
+   *                 (type: `GridHighlight`)
+   * @param x      - The x-coordinate of the highlighted position
+   *                 (type: `number`)
+   * @param y      - The y-coordinate of the highlighted position
+   *                 (type: `number`)
+   * @param color  - The hex fill color of the highlight
+   *                 (type: `number`)
+   *                 (default: `0x33BBFF`)
+   * @param border - The hex border color of the highlight
+   *                 (type: `number`)
+   *                 (default: `null`)
+   * @param alpha  - The opacity of the highlight
+   *                 (type: `number`)
+   *                 (default: `0.25`)
+   * @param shape  - A predefined shape to highlight
+   *                 (type: `PIXI.Polygon`)
+   *                 (default: `null`)
    */
-  highlightGridPosition(
-        layer: GridHighlight
-        x: number
-        y: number
-        color: number
-        border: number
-        alpha: number
-        shape: PIXI.Polygon
-    ): void
+  highlightGridPosition (
+    layer: GridHighlight,
+    options: {
+      x: number
+      y: number
+      color: number
+      border: number
+      alpha: number
+      shape: PIXI.Polygon
+    }
+  ): void
 
   /* -------------------------------------------- */
   /*  Grid Measurement Methods
@@ -52,26 +63,32 @@ declare class BaseGrid extends PIXI.Container{
 
   /**
    * Given a pair of coordinates (x, y) - return the top-left of the grid square which contains that point
-   * @param {number} x          The x-coordinate
-   * @param {number} y          The y-coordinate
-   * @return {number[]}    An Array [x, y] of the top-left coordinate of the square which contains (x, y)
+   * @param x - The x-coordinate
+   *            (type: `number`)
+   * @param y - The y-coordinate
+   *            (type: `number`)
+   * @returns - An Array [x, y] of the top-left coordinate of the square which contains (x, y)
+   *            (type: `number[]`)
    */
-  getTopLeft(
-      x: number
-      y: number
+  getTopLeft (
+    x: number,
+    y: number
   ): number[]
 
   /* -------------------------------------------- */
 
   /**
    * Given a pair of coordinates (x, y), return the center of the grid square which contains that point
-   * @param {number} x          The x-coordinate
-   * @param {number} y          The y-coordinate
-   * @return {number[]}         An array [cx, cy] of the central point of the grid space which contains (x, y)
+   * @param x - The x-coordinate
+   *            (type: `number`)
+   * @param y - The y-coordinate
+   *            (type: `number`)
+   * @returns - An array [cx, cy] of the central point of the grid space which contains (x, y)
+   *            (type: `number[]`)
    */
-  getCenter(
-      x: number
-      y: number
+  getCenter (
+    x: number,
+    y: number
   ): number[]
 
   /* -------------------------------------------- */
@@ -80,34 +97,39 @@ declare class BaseGrid extends PIXI.Container{
    * Given a pair of coordinates (x1,y1), return the grid coordinates (x2,y2) which represent the snapped position
    * Under a "gridless" system, every pixel position is a valid snapping position
    *
-   * @param {number} x          The exact target location x
-   * @param {number} y          The exact target location y
-   * @param {number|null} [interval]  An interval of grid spaces at which to snap.
-   *                                  At interval=1, snapping occurs at pixel intervals defined by the grid size
-   *                                  At interval=2, snapping would occur at the center-points of each grid size
-   *                                  At interval=null, no snapping occurs
-   *                                  (default: ``null``)
-   *
-   * @return {{x, y}}           An object containing the coordinates of the snapped location
+   * @param x        - The exact target location x
+   *                   (type: `number`)
+   * @param y        - The exact target location y
+   *                   (type: `number`)
+   * @param interval - An interval of grid spaces at which to snap.
+   *                   At interval=1, snapping occurs at pixel intervals defined by the grid size
+   *                   At interval=2, snapping would occur at the center-points of each grid size
+   *                   At interval=null, no snapping occurs
+   *                   (type: `number|null`)
+   *                   (default: `null`)
+   * @returns        - An object containing the coordinates of the snapped location
+   *                   (type: `{number, number}`)
    */
-   getSnappedPosition(
-       x: number
-       y: number
-       interval: number|null
-   ): number[]
-
-   /* -------------------------------------------- */
+  getSnappedPosition (
+    x: number,
+    y: number,
+    interval: number|null
+  ): {x: number, y: number}
+  /* -------------------------------------------- */
 
   /**
    * Given a pair of pixel coordinates, return the grid position as an Array.
    * Always round down to the nearest grid position so the pixels are within the grid space (from top-left).
-   * @param {number} x    The x-coordinate pixel position
-   * @param {number} y    The y-coordinate pixel position
-   * @return {number[]}   An array representing the position in grid units
+   * @param x - The x-coordinate pixel position
+   *            (type: `number`)
+   * @param y - The y-coordinate pixel position
+   *            (type: `number`)
+   * @returns - An array [x, y] representing the position in grid units
+   *            (type: `number[]`)
    */
-  getGridPositionFromPixels(
-      x: number
-      y: number
+  getGridPositionFromPixels (
+    x: number,
+    y: number
   ): number[]
 
   /* -------------------------------------------- */
@@ -115,58 +137,70 @@ declare class BaseGrid extends PIXI.Container{
   /**
    * Given a pair of grid coordinates, return the pixel position as an Array.
    * Always round up to a whole pixel so the pixel is within the grid space (from top-left).
-   * @param {number} x    The x-coordinate grid position
-   * @param {number} y    The y-coordinate grid position
-   * @return {number[]}   An array representing the position in pixels
+   * @param x - The x-coordinate grid position
+   *            (type: `number`)
+   * @param y - The y-coordinate grid position
+   *            (type: `number`)
+   * @returns - An array [x, y] representing the position in pixels
+   *            (type: `number[]`)
    */
-  getPixelsFromGridPosition(
-      x: number
-      y: number
+  getPixelsFromGridPosition (
+    x: number,
+    y: number
   ): number[]
 
   /* -------------------------------------------- */
 
   /**
    * Shift a pixel position [x,y] by some number of grid units dx and dy
-   * @param {number} x    The starting x-coordinate in pixels
-   * @param {number} y    The starting y-coordinate in pixels
-   * @param {number} dx   The number of grid positions to shift horizontally
-   * @param {number} dy   The number of grid positions to shift vertically
-   * @return {number[]}   An array representing the new position in pixels
+   * @param x  - The starting x-coordinate in pixels
+   *             (type: `number`)
+   * @param y  - The starting y-coordinate in pixels
+   *             (type: `number`)
+   * @param dx - The number of grid positions to shift horizontally
+   *             (type: `number`)
+   * @param dy - The number of grid positions to shift vertically
+   *             (type: `number`)
+   * @returns  - An array [x, y] representing the new position in pixels
+   *             (type: `number[]`)
    */
-  shiftPosition(
-      x: number
-      y: number
-      dx: number
-      dy: number
+  shiftPosition (
+    x: number,
+    y: number,
+    dx: number,
+    dy: number
   ): number[]
 
   /* -------------------------------------------- */
 
   /**
    * Measure the distance traversed over an array of measured segments
-   * @param {object[]} segments     An Array of measured movement segments
-   * @param {Options} options       Additional options which modify the measurement
-   *                                (default: ``{}``)
-   * @return {number[]}             An Array of distance measurements for each segment
+   * @param segments - An Array of measured movement segments
+   *                   (type: `object[]`)
+   * @param options  - Additional options which modify the measurement
+   *                   (type: `Options`)
+   *                   (default: `{}`)
+   * @returns        - An Array of distance measurements for each segment
+   *                   (type: `number[]`)
    */
-  measureDistances(
-      segments: object[]
-      options: Options
+  measureDistances (
+    segments: object[],
+    options: Options
   ): number[]
 
   /* -------------------------------------------- */
 
   /**
    * Get the grid row and column positions which are neighbors of a certain position
-   * @param {number} row  The grid row coordinate against which to test for neighbors
-   * @param {number} col  The grid column coordinate against which to test for neighbors
-   * @return {number[]}   An array of grid positions which are neighbors of the row and column
+   * @param row - The grid row coordinate against which to test for neighbors
+   *              (type: `number`)
+   * @param col - The grid column coordinate against which to test for neighbors
+   *              (type: `number`)
+   * @returns   - An array of grid positions which are neighbors of the row and column
+   *              (type: `number[]`)
    */
-  getNeighbors(
-      row: number
-      col: number
+  getNeighbors (
+    row: number,
+    col: number
   ): number[]
-
-  constructor (options: object)
 }
