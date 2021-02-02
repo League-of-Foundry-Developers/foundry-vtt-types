d560633151c3e5f4b03d68a263b78113ed3c00ea
Johannes Loher <johannes.loher@fg4f.de>
2021-02-02 14:03:52 +0100

    Add testing capabilities using tsd

---
+++ test-d/typesUtils.test-d.ts
@@ -0,0 +1,8 @@
+import '../types/typesUtils';
+import { expectType } from 'tsd';
+
+const membersBecomeOptional: DeepPartial<{ a: string }> = {};
+expectType<{ a?: string }>(membersBecomeOptional);
+
+const nestedMembersBecomeOptional: DeepPartial<{ a: { b: string } }> = { a: {} };
+expectType<{ a?: { b?: string } }>(nestedMembersBecomeOptional);
