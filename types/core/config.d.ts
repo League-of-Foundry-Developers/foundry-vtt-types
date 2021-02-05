980e291f01106b71748bb43c1867ee524e43c4d9
Johannes Loher <johannes.loher@fg4f.de>
2021-02-05 18:12:04 +0100

    Add defaultValue for statusEffects

--- types/core/config.d.ts
+++ types/core/config.d.ts
@@ -971,7 +971,7 @@ declare const CONFIG: {
   /**
    * Configure the default Token text style so that it may be reused and overridden by modules
    * @defaultValue
-   * ```
+   * ```typescript
    * new PIXI.TextStyle({
    *   fontFamily: 'Signika',
    *   fontSize: 36,
@@ -1075,6 +1075,171 @@ declare const CONFIG: {
 
   /**
    * An array of status effect icons which can be applied to Tokens
+   * @defaultValue
+   * ```typescript
+   * [
+   *   {
+   *     id: 'dead';
+   *     label: 'EFFECT.StatusDead';
+   *     icon: 'icons/svg/skull.svg';
+   *   },
+   *   {
+   *     id: 'unconscious';
+   *     label: 'EFFECT.StatusUnconscious';
+   *     icon: 'icons/svg/unconscious.svg';
+   *   },
+   *   {
+   *     id: 'sleep';
+   *     label: 'EFFECT.StatusAsleep';
+   *     icon: 'icons/svg/sleep.svg';
+   *   },
+   *   {
+   *     id: 'stun';
+   *     label: 'EFFECT.StatusStunned';
+   *     icon: 'icons/svg/daze.svg';
+   *   },
+   *   {
+   *     id: 'prone';
+   *     label: 'EFFECT.StatusProne';
+   *     icon: 'icons/svg/falling.svg';
+   *   },
+   *   {
+   *     id: 'restrain';
+   *     label: 'EFFECT.StatusRestrained';
+   *     icon: 'icons/svg/net.svg';
+   *   },
+   *   {
+   *     id: 'paralysis';
+   *     label: 'EFFECT.StatusParalysis';
+   *     icon: 'icons/svg/paralysis.svg';
+   *   },
+   *   {
+   *     id: 'fly';
+   *     label: 'EFFECT.StatusFlying';
+   *     icon: 'icons/svg/wing.svg';
+   *   },
+   *   {
+   *     id: 'blind';
+   *     label: 'EFFECT.StatusBlind';
+   *     icon: 'icons/svg/blind.svg';
+   *   },
+   *   {
+   *     id: 'deaf';
+   *     label: 'EFFECT.StatusDeaf';
+   *     icon: 'icons/svg/deaf.svg';
+   *   },
+   *   {
+   *     id: 'silence';
+   *     label: 'EFFECT.StatusSilenced';
+   *     icon: 'icons/svg/silenced.svg';
+   *   },
+   *   {
+   *     id: 'fear';
+   *     label: 'EFFECT.StatusFear';
+   *     icon: 'icons/svg/terror.svg';
+   *   },
+   *   {
+   *     id: 'burning';
+   *     label: 'EFFECT.StatusBurning';
+   *     icon: 'icons/svg/fire.svg';
+   *   },
+   *   {
+   *     id: 'frozen';
+   *     label: 'EFFECT.StatusFrozen';
+   *     icon: 'icons/svg/frozen.svg';
+   *   },
+   *   {
+   *     id: 'shock';
+   *     label: 'EFFECT.StatusShocked';
+   *     icon: 'icons/svg/lightning.svg';
+   *   },
+   *   {
+   *     id: 'corrode';
+   *     label: 'EFFECT.StatusCorrode';
+   *     icon: 'icons/svg/acid.svg';
+   *   },
+   *   {
+   *     id: 'bleeding';
+   *     label: 'EFFECT.StatusBleeding';
+   *     icon: 'icons/svg/blood.svg';
+   *   },
+   *   {
+   *     id: 'disease';
+   *     label: 'EFFECT.StatusDisease';
+   *     icon: 'icons/svg/biohazard.svg';
+   *   },
+   *   {
+   *     id: 'poison';
+   *     label: 'EFFECT.StatusPoison';
+   *     icon: 'icons/svg/poison.svg';
+   *   },
+   *   {
+   *     id: 'radiation';
+   *     label: 'EFFECT.StatusRadiation';
+   *     icon: 'icons/svg/radiation.svg';
+   *   },
+   *   {
+   *     id: 'regen';
+   *     label: 'EFFECT.StatusRegen';
+   *     icon: 'icons/svg/regen.svg';
+   *   },
+   *   {
+   *     id: 'degen';
+   *     label: 'EFFECT.StatusDegen';
+   *     icon: 'icons/svg/degen.svg';
+   *   },
+   *   {
+   *     id: 'upgrade';
+   *     label: 'EFFECT.StatusUpgrade';
+   *     icon: 'icons/svg/upgrade.svg';
+   *   },
+   *   {
+   *     id: 'downgrade';
+   *     label: 'EFFECT.StatusDowngrade';
+   *     icon: 'icons/svg/downgrade.svg';
+   *   },
+   *   {
+   *     id: 'target';
+   *     label: 'EFFECT.StatusTarget';
+   *     icon: 'icons/svg/target.svg';
+   *   },
+   *   {
+   *     id: 'eye';
+   *     label: 'EFFECT.StatusMarked';
+   *     icon: 'icons/svg/eye.svg';
+   *   },
+   *   {
+   *     id: 'curse';
+   *     label: 'EFFECT.StatusCursed';
+   *     icon: 'icons/svg/sun.svg';
+   *   },
+   *   {
+   *     id: 'bless';
+   *     label: 'EFFECT.StatusBlessed';
+   *     icon: 'icons/svg/angel.svg';
+   *   },
+   *   {
+   *     id: 'fireShield';
+   *     label: 'EFFECT.StatusFireShield';
+   *     icon: 'icons/svg/fire-shield.svg';
+   *   },
+   *   {
+   *     id: 'coldShield';
+   *     label: 'EFFECT.StatusIceShield';
+   *     icon: 'icons/svg/ice-shield.svg';
+   *   },
+   *   {
+   *     id: 'magicShield';
+   *     label: 'EFFECT.StatusMagicShield';
+   *     icon: 'icons/svg/mage-shield.svg';
+   *   },
+   *   {
+   *     id: 'holyShield';
+   *     label: 'EFFECT.StatusHolyShield';
+   *     icon: 'icons/svg/holy-shield.svg';
+   *   }
+   * ]
+   * ```
    */
   statusEffects: { id: string; label: string; icon: string }[];
 
