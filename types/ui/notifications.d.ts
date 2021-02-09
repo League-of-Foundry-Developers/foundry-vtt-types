43bd89b2eb9622a11dcddf533036a28d730593c8
Johannes Loher <johannes.loher@fg4f.de>
2021-02-09 16:20:03 +0100

    Update Notifications to 0.7.9

--- types/ui/notifications.d.ts
+++ types/ui/notifications.d.ts
@@ -4,26 +4,45 @@
  * Each notification is displayed for 5 seconds at which point further notifications are pulled from the queue.
  *
  * @example
+ * ```typescript
  * ui.notifications.info("This is an info message");
  * ui.notifications.warn("This is a warning message");
  * ui.notifications.error("This is an error message");
  * ui.notifications.info("This is a 4th message which will not be shown until the first info message is done");
+ * ```
  */
 declare class Notifications extends Application {
-  /** Notifications which are currently displayed */
-  active: any[];
-
-  /** Submitted notifications which are queued for display */
-  queue: any[];
-
   constructor(options: Application.Options);
 
+  /** Notifications which are currently displayed */
+  active: JQuery[];
+
+  /** Submitted notifications which are queued for display */
+  queue: Notifications.Notification[];
+
+  /** @override */
+  static get defaultOptions(): Application.Options;
+
   /**
-   * Display a notification with the "error" type
-   * @param message - The content of the notification message
-   * @param options - Notification options passed to the notify function
+   * Initialize the Notifications system by displaying any system-generated messages which were passed from the server.
    */
-  error(message: string, options?: { permanent?: boolean }): void;
+  initialize(): void;
+
+  /** @override */
+  protected _renderInner(data: object, options?: Application.RenderOptions): JQuery;
+
+  /** @override */
+  protected _render(force?: boolean, options?: Application.RenderOptions): Promise<void>;
+
+  /**
+   * Push a new notification into the queue
+   * @param message   - The content of the notification message
+   * @param type      - The type of notification, currently "info", "warning", and "error" are supported
+   *                    (default: `'info'`)
+   * @param permanent - Whether the notification should be permanently displayed unless otherwise dismissed
+   *                    (default: `false`)
+   */
+  notify(message: string, type?: 'info' | 'warning' | 'error', { permanent }?: { permanent?: boolean }): void;
 
   /**
    * Display a notification with the "info" type
@@ -33,14 +52,6 @@ declare class Notifications extends Application {
   info(message: string, options?: { permanent?: boolean }): void;
 
   /**
-   * Push a new notification into the queue
-   * @param message - The content of the notification message
-   * @param type - The type of notification, currently "info", "warning", and "error" are supported
-   * @param permanent - Whether the notification should be permanently displayed unless otherwise dismissed
-   */
-  notify(message: string, type: 'info' | 'warning' | 'error', options?: { permanent?: boolean }): void;
-
-  /**
    * Display a notification with the "warning" type
    * @param message - The content of the notification message
    * @param options - Notification options passed to the notify function
@@ -48,7 +59,23 @@ declare class Notifications extends Application {
   warn(message: string, options?: { permanent?: boolean }): void;
 
   /**
+   * Display a notification with the "error" type
+   * @param message - The content of the notification message
+   * @param options - Notification options passed to the notify function
+   */
+  error(message: string, options?: { permanent?: boolean }): void;
+
+  /**
    * Retrieve a pending notification from the queue and display it
    */
-  protected fetch(): Promise<any>;
+  protected fetch(): void;
+}
+
+declare namespace Notifications {
+  interface Notification {
+    message: string;
+    type: 'info' | 'warning' | 'error';
+    timestamp: number;
+    permanent: boolean;
+  }
 }
