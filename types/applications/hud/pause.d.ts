8f5d76377c1015332dc936a3ee0773fa6ab63172
Johannes Loher <johannes.loher@fg4f.de>
2021-02-09 15:23:42 +0100

    Update Pause to 0.7.9

--- types/applications/hud/pause.d.ts
+++ types/applications/hud/pause.d.ts
@@ -1,6 +1,17 @@
-// @TODO:
-
 /**
  * Pause notification in the HUD
  */
-declare class Pause extends Application {}
+declare class Pause<D extends Pause.Data = Pause.Data> extends Application {
+  static get defaultOptions(): Application.Options;
+
+  /**
+   * Prepare the default data which is required to render the Pause UI
+   */
+  getData(): D | Promise<D>;
+}
+
+declare namespace Pause {
+  interface Data {
+    paused: boolean;
+  }
+}
