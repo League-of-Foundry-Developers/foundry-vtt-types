f03204d8c16ea567968ad2af950b62d73d758409
Trevor Richards <trev@trevdev.ca>
2021-02-17 12:43:47 -0800

    add ready: false to canvas global type

--- types/game.d.ts
+++ types/game.d.ts
@@ -4,7 +4,7 @@
  * - `/stream`: as `{ ready: false }`
  * @defaultValue `null`
  */
-declare let canvas: Canvas | null;
+declare let canvas: Canvas | { ready: false } | null;
 
 declare let game: Game;
 
