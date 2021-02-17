3bee4c74186c4f861229b7f8388d7e49bce107b2
Kai Moschcau <mail@kmoschcau.de>
2021-02-17 11:54:46 +0100

    Update ItemDirectory to 0.7.9

--- types/applications/sidebarTabs/sidebarDirectories/itemDirectory.d.ts
+++ types/applications/sidebarTabs/sidebarDirectories/itemDirectory.d.ts
@@ -1,1 +1,31 @@
-declare class ItemDirectory {}
+/**
+ * A directory of Item entities in the Sidebar
+ */
+declare class ItemDirectory extends SidebarDirectory {
+  /**
+   * @override
+   */
+  static get entity(): string;
+
+  /**
+   * @override
+   */
+  static get collection(): Items;
+
+  /**
+   * @param selector - (unused)
+   * @override
+   */
+  protected _canDragStart(selector: string | null): boolean;
+
+  /**
+   * @param selector - (unused)
+   * @override
+   */
+  protected _canDragDrop(selector: string | null): boolean;
+
+  /**
+   * @override
+   */
+  protected _getEntryContextOptions(): ContextMenu.Item[];
+}
