57d9dc83a60278bcb337a7916bd2cca4a2772afe
Kai Moschcau <mail@kmoschcau.de>
2021-02-17 12:01:04 +0100

    Update JournalDirectory to 0.7.9

--- types/applications/sidebarTabs/sidebarDirectories/journalDirectory.d.ts
+++ types/applications/sidebarTabs/sidebarDirectories/journalDirectory.d.ts
@@ -1,1 +1,43 @@
-declare class JournalDirectory {}
+/**
+ * A directory list of JournalEntry entities in the Sidebar
+ */
+declare class JournalDirectory extends SidebarDirectory {
+  /**
+   * @override
+   */
+  static get defaultOptions(): JournalDirectory.Options;
+
+  /**
+   * @override
+   */
+  get title(): string;
+
+  /**
+   * @override
+   */
+  static get entity(): string;
+
+  /**
+   * @override
+   */
+  static get collection(): Journal;
+
+  /**
+   * @override
+   */
+  protected _getEntryContextOptions(): ContextMenu.Item[];
+}
+
+declare namespace JournalDirectory {
+  interface Options extends SidebarDirectory.Options {
+    /**
+     * @defaultValue `'journal'`
+     */
+    id: string;
+
+    /**
+     * @defaultValue `'templates/sidebar/journal-directory.html'`
+     */
+    template: string;
+  }
+}
