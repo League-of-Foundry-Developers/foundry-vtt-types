e7133117b786d2b659353d23a6901cc58057e167
Kai Moschcau <mail@kmoschcau.de>
2021-02-17 11:30:34 +0100

    Update CompendiumDirectory to 0.7.9

--- types/applications/sidebarTabs/compendiumDirectory.d.ts
+++ types/applications/sidebarTabs/compendiumDirectory.d.ts
@@ -1,1 +1,65 @@
-declare class CompendiumDirectory {}
+/**
+ * A compendium of knowledge arcane and mystical!
+ */
+declare class CompendiumDirectory extends SidebarTab {
+  /**
+   * @override
+   */
+  static get defaultOptions(): CompendiumDirectory.Options;
+
+  /**
+   * @param options - (unused)
+   * @override
+   */
+  getData(options?: Application.RenderOptions): CompendiumDirectory.Data;
+
+  /**
+   * @override
+   */
+  activateListeners(html: JQuery): void;
+
+  /**
+   * Compendium sidebar Context Menu creation
+   */
+  protected _contextMenu(html: JQuery): ContextMenu.Item[];
+
+  /**
+   * Handle a Compendium Pack creation request
+   */
+  protected _onCreateCompendium(event: JQuery.ClickEvent): Promise<void>;
+
+  /**
+   * Handle a Compendium Pack deletion request
+   * @param pack - The pack object requested for deletion
+   */
+  protected _onDeleteCompendium(pack: Compendium): Promise<Compendium | void>;
+}
+
+declare namespace CompendiumDirectory {
+  interface Data {
+    user: User;
+    packs: Record<string, PackData>;
+  }
+
+  interface Options extends SidebarTab.Options {
+    /**
+     * @defaultValue `'compendium'`
+     */
+    id: string;
+
+    /**
+     * @defaultValue `'templates/sidebar/compendium.html'`
+     */
+    template: string;
+
+    /**
+     * @defaultValue `'Compendium Packs'`
+     */
+    title: string;
+  }
+
+  interface PackData {
+    label: string;
+    packs: Compendium[];
+  }
+}
