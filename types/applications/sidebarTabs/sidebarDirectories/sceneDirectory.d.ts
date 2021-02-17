276e222d3b8fb7d15b6cd2389e0111ea11a0bea0
Kai Moschcau <mail@kmoschcau.de>
2021-02-17 12:52:49 +0100

    Update SceneDirectory to 0.7.9

--- types/applications/sidebarTabs/sidebarDirectories/sceneDirectory.d.ts
+++ types/applications/sidebarTabs/sidebarDirectories/sceneDirectory.d.ts
@@ -1,1 +1,25 @@
-declare class SceneDirectory {}
+/**
+ * A directory listing of active game scenes
+ */
+declare class SceneDirectory extends SidebarDirectory {
+  /**
+   * @override
+   */
+  static get entity(): string;
+
+  /**
+   * @override
+   */
+  static get collection(): Scenes;
+
+  /**
+   * @override
+   * @remarks This is never called.
+   */
+  protected _onCreate(event: MouseEvent): Promise<Scene>;
+
+  /**
+   * @override
+   */
+  protected _getEntryContextOptions(): ContextMenu.Item[];
+}
