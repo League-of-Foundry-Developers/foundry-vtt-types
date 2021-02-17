865a71bd28ef12d697daa33fbfae64ec7e84413f
Kai Moschcau <mail@kmoschcau.de>
2021-02-17 12:46:38 +0100

    Update RollTableDirectory to 0.7.9

--- types/applications/sidebarTabs/sidebarDirectories/rollTableDirectory.d.ts
+++ types/applications/sidebarTabs/sidebarDirectories/rollTableDirectory.d.ts
@@ -2,6 +2,24 @@
  * A directory list of RollTable entities in the Sidebar
  */
 declare class RollTableDirectory extends SidebarDirectory {
-  // @TODO Declare Classes
-  [key: string]: any;
+  static get defaultOptions(): RollTableDirectory.Options;
+
+  /**
+   * Reference named Entity class which this directory lists
+   */
+  static get entity(): string;
+
+  /**
+   * Reference the EntityCollection of Entity instances which this directory lists
+   */
+  static get collection(): RollTables;
+}
+
+declare namespace RollTableDirectory {
+  interface Options extends SidebarDirectory.Options {
+    /**
+     * @defaultValue `'tables'`
+     */
+    id: SidebarDirectory.Options['id'];
+  }
 }
