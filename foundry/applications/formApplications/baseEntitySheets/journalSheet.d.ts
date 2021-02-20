1b93bd3bc0edb624bffe2eab4b8e9bc96a45a31f
Dawid Warmuz <warmuz.dawid@gmail.com>
2021-02-20 15:33:59 +0100

    JournalSheet typescript definitions

--- foundry/applications/formApplications/baseEntitySheets/journalSheet.d.ts
+++ foundry/applications/formApplications/baseEntitySheets/journalSheet.d.ts
@@ -1,23 +1,70 @@
 /**
  * The JournalEntry Configuration Sheet
- *
- * @param entity - The JournalEntry instance which is being edited
- * @param options - Application options
  */
-declare class JournalSheet extends BaseEntitySheet {
-  /**
-   * Provide a unique CSS ID for Entity Sheets
-   */
+declare class JournalSheet<
+  D extends object = BaseEntitySheet.Data<JournalEntry>,
+  O extends JournalEntry = D extends BaseEntitySheet.Data<infer T> ? T : JournalEntry
+> extends BaseEntitySheet<D, O> {
+  constructor(object: O, options?: JournalSheet.Options);
+
+  /** @override */
+  static get defaultOptions(): JournalSheet.Options;
+
+  /** @override */
   get id(): string;
 
-  /**
-   * Suppress the JournalEntry title when an image is shown to players whom do not have at least limited permission
-   */
+  /** @override */
+  get template(): string;
+
+  /** @override */
   get title(): string;
 
+  /** @override */
+  getData(options?: Application.RenderOptions): Promise<D> | D;
+
   /**
-   * @param options - (unused)
-   * @override
+   * Guess the default view mode for the sheet based on the player's permissions to the Entry
    */
-  getData(options?: any): BaseEntitySheet.Data<Entity>;
+  protected _inferDefaultMode(): JournalSheet.SheetMode;
+
+  /** @override */
+  protected _render(force?: boolean, options?: JournalSheet.RenderOptions): Promise<void>;
+
+  /** @override */
+  protected _getHeaderButtons(): Application.HeaderButton[];
+
+  /** @override */
+  protected _updateObject(event: Event, formData: object): Promise<O>;
+
+  /**
+   * Handle requests to switch the rendered mode of the Journal Entry sheet
+   * Save the form before triggering the show request, in case content has changed
+   */
+  protected _onSwapMode(event: Event, mode: JournalSheet.SheetMode): Promise<void>;
+
+  /**
+   * Handle requests to show the referenced Journal Entry to other Users
+   * Save the form before triggering the show request, in case content has changed
+   */
+  protected _onShowPlayers(event: Event): Promise<void>;
+}
+
+declare namespace JournalSheet {
+  interface RenderOptions extends Application.RenderOptions {
+    sheetMode: SheetMode;
+  }
+
+  interface Options extends BaseEntitySheet.Options {
+    sheetMode: SheetMode;
+  }
+
+  interface Data<O extends JournalEntry = JournalEntry> extends BaseEntitySheet.Data<O> {
+    image: string;
+    folders: Folder[];
+  }
+
+  enum SheetMode {
+    text = 'text',
+    image = 'image'
+  }
 }
