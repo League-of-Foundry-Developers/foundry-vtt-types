b1dc56e229505ce3fd37e25c63ec5dcec11838ba
Kai Moschcau <mail@kmoschcau.de>
2021-02-24 21:22:48 +0100

    Update FolderConfig to 0.7.9

--- foundry/applications/formApplications/folderConfig.d.ts
+++ foundry/applications/formApplications/folderConfig.d.ts
@@ -1,6 +1,58 @@
 /**
  * Edit a folder, configuring its name and appearance
  */
-declare class FolderConfig extends FormApplication {
-  _updateObject(event?: Event, formData?: object): Promise<any>; // TODO: Add correct return type
+declare class FolderConfig extends FormApplication<FolderConfig.Data, Folder.Data> {
+  /**
+   * @override
+   */
+  static get defaultOptions(): FolderConfig.Options;
+
+  /**
+   * @override
+   */
+  get id(): string;
+
+  /**
+   * @override
+   */
+  get title(): string;
+
+  /**
+   * @override
+   */
+  getData(options?: Application.RenderOptions): Promise<FolderConfig.Data>;
+
+  /**
+   * @param event - (unused)
+   * @override
+   */
+  protected _updateObject(event: Event, formData: Folder.Data): Promise<Folder | Folder | null>;
+}
+
+declare namespace FolderConfig {
+  interface Data {
+    folder: Folder.Data;
+    sortingModes: {
+      a: 'FOLDER.SortAlphabetical';
+      m: 'FOLDER.SortManual';
+    };
+    submitText: string;
+  }
+
+  interface Options extends FormApplication.Options {
+    /**
+     * @defaultValue `['sheet', 'folder-edit']`
+     */
+    classes: string[];
+
+    /**
+     * @defaultValue `'templates/sidebar/folder-edit.html'`
+     */
+    template: string;
+
+    /**
+     * @defaultValue `360`
+     */
+    width: number;
+  }
 }
