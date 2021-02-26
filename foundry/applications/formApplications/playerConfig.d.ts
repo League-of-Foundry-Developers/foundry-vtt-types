80e173fcb36fb68ff0189bbd4971eaaf502916d8
Kai Moschcau <mail@kmoschcau.de>
2021-02-26 20:46:26 +0100

    Update PlayerConfig to 0.7.9

--- foundry/applications/formApplications/playerConfig.d.ts
+++ foundry/applications/formApplications/playerConfig.d.ts
@@ -1,7 +1,76 @@
 /**
- * The player configuration menu
- * This form is used to allow the client to edit some preferences about their own User entity
+ * The Player Configuration application provides a form used to allow the current client to
+ * edit preferences and configurations about their own User entity.
  */
-declare class PlayerConfig extends FormApplication {
-  _updateObject(event?: Event, formData?: object): Promise<any>; // TODO: Add correct return type
+declare class PlayerConfig extends FormApplication<PlayerConfig.Data, User> {
+  /**
+   * @param user    - The User entity being configured.
+   * @param options - Additional rendering options which modify the behavior of the form.
+   */
+  constructor(user: User, options: PlayerConfig.Options);
+
+  user: User;
+
+  /**
+   * Assign the default options which are supported by the entity edit sheet
+   */
+  static get defaultOptions(): PlayerConfig.Options;
+
+  get title(): string;
+
+  /**
+   * Provide data to the form
+   * @returns The data provided to the template when rendering the form
+   */
+  getData(): PlayerConfig.Data;
+
+  /**
+   * Activate the default set of listeners for the Entity sheet
+   * These listeners handle basic stuff like form submission or updating images
+   *
+   * @param html - The rendered template ready to have listeners attached
+   */
+  activateListeners(html: JQuery): void;
+
+  /**
+   * Handle changing the user avatar image by opening a FilePicker
+   */
+  protected _onEditAvatar(event: Event): Promise<FilePicker.Result>;
+
+  /**
+   * This method is called upon form submission after form data is validated
+   * @param event    - The initial triggering submission event
+   * @param formData - The object of validated form data with which to update the object
+   */
+  protected _updateObject(event: Event, formData: User.Data): Promise<User>;
+}
+
+declare namespace PlayerConfig {
+  interface Data {
+    user: PlayerConfig['user'];
+    actors: Actor[];
+    options: PlayerConfig['options'];
+  }
+
+  interface Options extends FormApplication.Options {
+    /**
+     * @defaultValue `'player-config'`
+     */
+    id: string;
+
+    /**
+     * @defaultValue `'templates/user/player-config.html'`
+     */
+    template: string;
+
+    /**
+     * @defaultValue `400`
+     */
+    width: number;
+
+    /**
+     * @defaultValue `'auto'`
+     */
+    height: 'auto' | number;
+  }
 }
