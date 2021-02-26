552b25e2ee4bece52d6cfd11ee50f595a4978815
Kai Moschcau <mail@kmoschcau.de>
2021-02-26 21:08:29 +0100

    Update PlaylistConfig to 0.7.9

--- foundry/applications/formApplications/playlistConfig.d.ts
+++ foundry/applications/formApplications/playlistConfig.d.ts
@@ -1,9 +1,40 @@
 /**
  * Playlist Configuration Sheet
- *
- * @param object - The Playlist being edited
- * @param options - Additional application rendering options
  */
-declare class PlaylistConfig extends FormApplication {
-  _updateObject(event?: Event, formData?: object): Promise<any>; // TODO: Add correct return type
+declare class PlaylistConfig extends BaseEntitySheet<PlaylistConfig.Data, Playlist> {
+  static get defaultOptions(): PlaylistConfig.Options;
+
+  /**
+   * @override
+   */
+  get title(): string;
+
+  /**
+   * @param options - (unused)
+   * @override
+   */
+  getData(options?: Application.RenderOptions): PlaylistConfig.Data;
+}
+
+declare namespace PlaylistConfig {
+  interface Data extends Duplicated<PlaylistConfig['object']['data']> {
+    modes: Record<string, ValueOf<typeof CONST['PLAYLIST_MODES']>>;
+  }
+
+  interface Options extends BaseEntitySheet.Options {
+    /**
+     * @defaultValue `'playlist-config'`
+     */
+    id: string;
+
+    /**
+     * @defaultValue `'templates/playlist/edit-playlist.html'`
+     */
+    template: string;
+
+    /**
+     * @defaultValue `360`
+     */
+    width: number;
+  }
 }
