295f3890e0d6eac52733225ccdbf7b8a8295e22d
Kai Moschcau <mail@kmoschcau.de>
2021-02-26 20:25:17 +0100

    Update MeasuredTemplateConfig to 0.7.9

--- foundry/applications/formApplications/measuredTemplateConfig.d.ts
+++ foundry/applications/formApplications/measuredTemplateConfig.d.ts
@@ -1,10 +1,57 @@
 /**
- * Template Measurement Config Sheet
- *
- * @param template - The template object being configured
- * @param options - Additional application rendering options
- * @param preview - Configure a preview version of a sound which is not yet saved
+ * A configuration Form Application for modifying the properties of a MeasuredTemplate object.
+ * @see {@link MeasuredTemplate}
  */
-declare class MeasuredTemplateConfig extends FormApplication {
-  _updateObject(event?: Event, formData?: object): Promise<any>; // TODO: Add correct return type
+declare class MeasuredTemplateConfig extends FormApplication<MeasuredTemplateConfig.Data, MeasuredTemplate> {
+  /**
+   * @override
+   */
+  static get defaultOptions(): MeasuredTemplateConfig.Options;
+
+  /**
+   * @override
+   */
+  getData(): MeasuredTemplateConfig.Data;
+
+  /**
+   * @override
+   */
+  protected _updateObject(event: Event, formData: MeasuredTemplate.Data): Promise<MeasuredTemplate | null>;
+}
+
+declare namespace MeasuredTemplateConfig {
+  interface Data {
+    object: Duplicated<MeasuredTemplateConfig['object']['data']>;
+    options: Options;
+    templateTypes: typeof CONFIG['MeasuredTemplate']['types'];
+    gridUnits: Scene.Data;
+    submitText: string;
+  }
+
+  interface Options extends FormApplication.Options {
+    /**
+     * @defaultValue `'template-config'`
+     */
+    id: string;
+
+    /**
+     * @defaultValue `['sheet', 'template-sheet']`
+     */
+    classes: string[];
+
+    /**
+     * @defaultValue `'Measurement Template Configuration'`
+     */
+    title: string;
+
+    /**
+     * @defaultValue `'templates/scene/template-config.html'`
+     */
+    template: string;
+
+    /**
+     * @defaultValue `400`
+     */
+    width: number;
+  }
 }
