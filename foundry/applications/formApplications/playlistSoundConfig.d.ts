c3c9b673d5dd9a7ce624467683ca6fd23daf9b02
Kai Moschcau <mail@kmoschcau.de>
2021-02-26 21:29:23 +0100

    Update PlaylistSoundConfig to 0.7.9

--- foundry/applications/formApplications/playlistSoundConfig.d.ts
+++ foundry/applications/formApplications/playlistSoundConfig.d.ts
@@ -1,9 +1,68 @@
 /**
  * Playlist Sound Configuration Sheet
- *
- * @param sound - The sound object being configured
- * @param options - Additional application rendering options
  */
-declare class PlaylistSoundConfig extends FormApplication {
-  _updateObject(event?: Event, formData?: object): Promise<Playlist.Sound>;
+declare class PlaylistSoundConfig extends FormApplication<PlaylistSoundConfig.Data, Playlist.Sound> {
+  /**
+   * @param playlist - The Playlist entity within which the Sound is configured
+   * @param sound    - An Object for the Playlist Sound data
+   * @param options  - Additional application rendering options
+   */
+  constructor(playlist: PlaylistSoundConfig['playlist'], sound: Playlist.Sound, options: PlaylistSoundConfig.Options);
+
+  playlist: Playlist;
+
+  /**
+   * @override
+   */
+  static get defaultOptions(): PlaylistSoundConfig.Options;
+
+  /**
+   * @override
+   */
+  get title(): string;
+
+  /**
+   * @param options - (unused)
+   * @override
+   */
+  getData(options?: Application.RenderOptions): PlaylistSoundConfig.Data;
+
+  /**
+   * @param event - (unused)
+   * @override
+   */
+  protected _updateObject(event: Event, formData: PlaylistSoundConfig.Data): Promise<Playlist>;
+
+  /**
+   * @override
+   */
+  activateListeners(html: JQuery): void;
+
+  /**
+   * Auto-populate the track name using the provided filename, if a name is not already set
+   */
+  protected _onSourceChange(event: Event): void;
+}
+
+declare namespace PlaylistSoundConfig {
+  interface Data extends Duplicated<PlaylistSoundConfig['object']> {
+    lvolume: number;
+  }
+
+  interface Options extends FormApplication.Options {
+    /**
+     * @defaultValue `'track-config'`
+     */
+    id: string;
+
+    /**
+     * @defaultValue `'templates/playlist/edit-track.html'`
+     */
+    template: string;
+
+    /**
+     * @defaultValue `360`
+     */
+    width: number;
+  }
 }
