d104f5d52170c8ebe40168ab34c955ad42bdd39a
Joe Nosie <joenosie@protonmail.com>
2021-02-26 18:40:46 -0600

    Add NotesLayer for 0.7.9

---
+++ foundry/pixi/containers/canvasLayers/placeablesLayers/notesLayer.d.ts
@@ -0,0 +1,43 @@
+/**
+ * The Notes Layer Container
+ */
+declare class NotesLayer extends PlaceablesLayer<Note> {
+  /**
+   * @override
+   * @defaultValue
+   * ```
+   * mergeObject(super.layerOptions, {
+   *   canDragCreate: false,
+   *   objectClass: Note,
+   *   sheetClass: NoteConfig,
+   *   sortActiveTop: true,
+   *   zIndex: 60
+   * })
+   * ```
+   */
+  static get layerOptions(): PlaceablesLayer.LayerOptions;
+
+  /** @override */
+  activate(): this;
+
+  /** @override */
+  deactivate(): this;
+
+  /**
+   * Register game settings used by the NotesLayer
+   */
+  static registerSettings(): void;
+
+  /** @override */
+  protected _onMouseDown(event: PIXI.InteractionEvent): void;
+
+  /**
+   * Handle JournalEntry entity drop data
+   */
+  protected _onDropData(event: PIXI.InteractionEvent, data: JournalEntry): Promise<false | undefined>;
+
+  /**
+   * @defaultValue `"notesDisplayToggle"`
+   */
+  static TOGGLE_SETTING: string;
+}
