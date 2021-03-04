f4bf270f64d5ef8a4c8147cbb48ff1f22842db7e
Kai Moschcau <mail@kmoschcau.de>
2021-03-04 19:00:31 +0100

    Update CombatantConfig defaultOpts and update

--- foundry/applications/formApplications/combatantConfig.d.ts
+++ foundry/applications/formApplications/combatantConfig.d.ts
@@ -2,7 +2,19 @@
  * Configure or create a single Combatant within a Combat entity.
  */
 declare class CombatantConfig extends FormApplication {
-  static get defaultOptions(): CombatantConfig.Options;
+  /**
+   * @defaultValue
+   * ```typescript
+   * mergeObject(super.defaultOptions, {
+   *   id: "combatant-config",
+   *   title: game.i18n.localize("COMBAT.CombatantConfig"),
+   *   classes: ["sheet", "combat-sheet"],
+   *   template: "templates/sheets/combatant-config.html",
+   *   width: 420
+   * });
+   * ```
+   */
+  static get defaultOptions(): FormApplication.Options;
 
   /**
    * @override
@@ -13,37 +25,15 @@ declare class CombatantConfig extends FormApplication {
    * @param event - (unused)
    * @override
    */
-  protected _updateObject<U>(
-    event: Event,
-    formData: Expanded<U> extends DeepPartial<Combat.Combatant> ? U : DeepPartial<Combat.Combatant>
-  ): Promise<U>;
+  protected _updateObject(event: Event, formData: CombatantConfig.FormData): Promise<Combat.Combatant>;
 }
 
 declare namespace CombatantConfig {
-  interface Options extends FormApplication.Options {
-    /**
-     * @defaultValue `'combatant-config'`
-     */
-    id: string;
-
-    /**
-     * @defaultValue `game.i18n.localize('COMBAT.CombatantConfig')`
-     */
-    title: string;
-
-    /**
-     * @defaultValue `['sheet', 'combat-sheet']`
-     */
-    classes: string[];
-
-    /**
-     * @defaultValue `'templates/sheets/combatant-config.html'`
-     */
-    template: string;
-
-    /**
-     * @defaultValue `420`
-     */
-    width: number;
+  interface FormData {
+    defeated: boolean;
+    hidden: boolean;
+    img: string;
+    initiative: number | null;
+    name: string;
   }
 }
