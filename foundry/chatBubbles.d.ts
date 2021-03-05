8a304fdb403fae363a3c09b658d9b9540d29f234
Dawid Warmuz <warmuz.dawid@gmail.com>
2021-03-05 12:13:09 +0100

    [ChatBubbles] Define all the types

--- foundry/chatBubbles.d.ts
+++ foundry/chatBubbles.d.ts
@@ -1,5 +1,3 @@
-// @TODO:
-
 /**
  * The Chat Bubble Class
  * This application displays a temporary message sent from a particular Token in the active Scene.
@@ -7,9 +5,21 @@
  */
 declare class ChatBubbles {
   /**
+   * @defaultValue `'templates/hud/chat-bubble.html'`
+   */
+  template: string;
+
+  /**
    * Track active Chat Bubbles
+   * @defaultValue `{}`
    */
   bubbles: object;
+
+  /**
+   * Track which Token was most recently panned to highlight
+   * Use this to avoid repeat panning
+   */
+  protected _panned: Token | null;
 
   constructor();
 
@@ -20,10 +30,53 @@ declare class ChatBubbles {
 
   /**
    * Speak a message as a particular Token, displaying it as a chat bubble
-   * @param token - The speaking Token
+   * @param token   - The speaking Token
    * @param message - The spoken message text
-   * @param emote - Whether to style the speech bubble as an emote
+   * @param emote   - Whether to style the speech bubble as an emote
    * @returns A Promise which resolves once the chat bubble has been created
    */
   say(token: Token, message: string, { emote }: { emote: boolean }): Promise<void>;
+
+  /**
+   * Clear any existing chat bubble for a certain Token
+   */
+  protected _clearBubble(token: Token): Promise<void>;
+
+  /**
+   * Render the HTML template for the chat bubble
+   * @param data - Template data
+   * @returns The rendered HTML
+   */
+  protected _renderHTML({ token, message, emote }: { token: Token; message: string; emote: boolean }): Promise<string>;
+
+  /**
+   * Before displaying the chat message, determine it's constrained and unconstrained dimensions
+   * @param message - The message content
+   * @returns The rendered message dimensions
+   */
+  protected _getMessageDimensions(message: string): ChatBubbles.Dimensions;
+
+  /**
+   * Assign styling parameters to the chat bubble, toggling either a left or right display (randomly)
+   */
+  protected _setPosition(token: Token, html: JQuery, dimensions: ChatBubbles.Dimensions): void;
+
+  /**
+   * Determine the length of time for which to display a chat bubble.
+   * Research suggests that average reading speed is 200 words per minute.
+   * Since these are short-form messages, we multiply reading speed by 1.5.
+   * Clamp the result between 1 second (minimum) and 20 seconds (maximum)
+   *
+   * @param html - The HTML message
+   * @returns The number of milliseconds for which to display the message
+   */
+  protected _getDuration(html: JQuery): number;
+}
+
+declare namespace ChatBubbles {
+  interface Dimensions {
+    width: number;
+    height: number;
+    unconstrained: number;
+  }
 }
