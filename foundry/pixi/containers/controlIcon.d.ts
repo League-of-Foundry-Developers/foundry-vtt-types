e544869355297ba3665c6b89252f229f804ddebf
Joe Nosie <joenosie@protonmail.com>
2021-03-05 22:12:10 -0600

    Adds ControlIcon for 0.7.9

--- foundry/pixi/containers/controlIcon.d.ts
+++ foundry/pixi/containers/controlIcon.d.ts
@@ -2,5 +2,54 @@
  * A generic helper for drawing a standard Control Icon
  */
 declare class ControlIcon extends PIXI.Container {
-  // @TODO: Declare
+  /**
+   * @param size        - (default: `40`)
+   * @param borderColor - (default: `0xFF5500`)
+   * @param tint        - (default: `null`)
+   */
+  constructor({
+    texture,
+    size,
+    borderColor,
+    tint
+  }: {
+    texture: string;
+    size?: number;
+    borderColor?: number;
+    tint?: number | null;
+  });
+
+  iconSrc: string;
+
+  size: number;
+
+  rect: [number, number, number, number];
+
+  borderColor: number;
+
+  tintColor: number | null;
+
+  /**
+   * @defaultValue `true`
+   */
+  interactive: boolean;
+
+  /**
+   * @defaultValue `false`
+   */
+  interactiveChildren: boolean;
+
+  hitArea: PIXI.Rectangle;
+
+  bg: PIXI.Graphics;
+
+  icon: PIXI.Sprite;
+
+  border: PIXI.Graphics;
+
+  draw(): Promise<this>;
+
+  protected _onHoverIn(event: PIXI.InteractionEvent): void;
+
+  protected _onHoverOut(event: PIXI.InteractionEvent): void;
 }
