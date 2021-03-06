3531bcc18aebcb2940963c4e9997ac571e5a6749
Joe Nosie <joenosie@protonmail.com>
2021-03-06 00:24:27 -0600

    Update TileHUD to 0.7.9

--- foundry/applications/basePlaceableHUDs/tileHUD.d.ts
+++ foundry/applications/basePlaceableHUDs/tileHUD.d.ts
@@ -1,4 +1,25 @@
 /**
  * An implementation of the PlaceableHUD base class which renders a heads-up-display interface for Tile objects.
  */
-declare class TileHUD extends BasePlaceableHUD<Tile> {}
+declare class TileHUD extends BasePlaceableHUD<Tile> {
+  /**
+   * @override
+   * @defaultValue
+   * ```
+   * mergeObject(super.defaultOptions, {
+   *   id: "tile-hud",
+   *   template: "templates/hud/drawing-hud.html"
+   * })
+   * ```
+   */
+  static get defaultOptions(): Application.Options;
+
+  /** @override */
+  getData(): ReturnType<BasePlaceableHUD<Tile>['getData']> & {
+    lockedClass: string;
+    visibilityClass: string;
+  };
+
+  /** @override */
+  setPosition(): void;
+}
