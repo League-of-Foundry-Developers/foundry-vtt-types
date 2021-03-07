f28919ee13bafae32f3df3dccd7ff6b9463387b3
Dean Vigoren <vigorator@gmail.com>
2021-03-06 21:40:30 -0700

    Added the InvitationLinks class.

---
+++ foundry/applications/invitationLinks.d.ts
@@ -0,0 +1,25 @@
+/**
+ * Game Invitation Links Reference
+ */
+
+declare class InvitationLinks extends Application {
+  /**
+   * @defaultValue
+   * ```typescript
+   * {
+   *   ...super.defaultOptions,
+   *   id: 'invitation-links',
+   *   template: 'templates/sidebar/apps/invitation-links.html',
+   *   title: game.i18n.localize("INVITATIONS.Title"),
+   *   width: 400,
+   * }
+   * ```
+   */
+  static get defaultOptions(): Application.Options;
+
+  /** @override */
+  getData(): { local?: string; remote?: string } | Promise<{ local?: string; remote?: string }>;
+
+  /** @override */
+  activateListeners(html: JQuery): void;
+}
