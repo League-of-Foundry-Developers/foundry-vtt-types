d9663858c57d7ab8f0f7bca27efdd08d9f0f097f
Kai Moschcau <mail@kmoschcau.de>
2021-03-07 10:18:51 +0100

    Update TileConfig to 0.7.9

--- foundry/applications/formApplications/tileConfig.d.ts
+++ foundry/applications/formApplications/tileConfig.d.ts
@@ -5,6 +5,63 @@
  * @param options - Additional application rendering options
  * @param preview - Configure a preview version of a tile which is not yet saved
  */
-declare class TileConfig extends FormApplication {
-  _updateObject(event?: Event, formData?: object): Promise<any>; // TODO: Add correct return type
+declare class TileConfig extends FormApplication<TileConfig.Data, Tile> {
+  constructor(tile: Tile, options?: Partial<TileConfig.Options>);
+
+  options: TileConfig.Options;
+
+  /**
+   * @override
+   * @defaultValue
+   * ```typescript
+   * mergeObject(super.defaultOptions, {
+   *   id: "tile-config",
+   *   classes: ["sheet", "tile-sheet"],
+   *   title: "Tile Configuration",
+   *   template: "templates/scene/tile-config.html",
+   *   width: 400,
+   *   submitOnChange: true
+   * });
+   * ```
+   */
+  static get defaultOptions(): TileConfig.Options;
+
+  /**
+   * @param options - (unused)
+   * @override
+   */
+  getData(options?: Application.RenderOptions): TileConfig.Data;
+
+  /** @override */
+  protected _onChangeInput(event: Event): void;
+
+  /**
+   * @param event - (unused)
+   * @override
+   */
+  protected _updateObject(event: Event, formData: TileConfig.FormData): Promise<Tile>;
+
+  /** @override */
+  close(options: Application.CloseOptions): Promise<void>;
+}
+
+declare namespace TileConfig {
+  interface Data {
+    object: Duplicated<TileConfig['object']['data']>;
+    options: TileConfig['options'];
+    submitText: 'Create' | 'Update';
+  }
+
+  interface FormData {
+    height: Tile.Data['height'] | null;
+    img: Tile.Data['img'];
+    rotation: Tile.Data['rotation'] | null;
+    width: Tile.Data['width'] | null;
+    x: Tile.Data['x'] | null;
+    y: Tile.Data['y'] | null;
+  }
+
+  interface Options extends FormApplication.Options {
+    preview?: boolean;
+  }
 }
