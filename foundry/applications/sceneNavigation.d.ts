407286382e24a588ee82559b5d72382befe1f1e0
Dean Vigoren <vigorator@gmail.com>
2021-03-07 20:58:20 -0700

    Finished updating the SceneNavigation class for foundry 0.7.9

--- foundry/applications/sceneNavigation.d.ts
+++ foundry/applications/sceneNavigation.d.ts
@@ -23,7 +23,7 @@ declare class SceneNavigation extends Application {
   get scenes(): Scene[];
 
   /** @override */
-  render(force?: boolean, options?: Application.RenderOptions): Application;
+  render(force?: boolean, options?: Application.RenderOptions): this;
 
   /** @override */
   protected _render(force?: boolean, options?: Application.RenderOptions): Promise<void>;
@@ -50,6 +50,27 @@ declare class SceneNavigation extends Application {
    * Get the set of ContextMenu options which should be applied for Scenes in the menu
    */
   private _getContextMenuOptions(): ContextMenu.Item[];
+
+  /**
+   * Handle left-click events on the scenes in the navigation menu
+   */
+  private _onClickScene(event: Event): void;
+
+  /** @override */
+  protected _onDragStart(event: DragEvent): void;
+
+  /** @override */
+  protected _onDrop(event: DragEvent): Promise<boolean | undefined | void>;
+
+  /** @override */
+  private _onToggleNav(event: Event): void;
+
+  /**
+   * Updated the loading progress bar
+   * @param context- The message to display in the progress back
+   * @param pct- The percentage the progress bar has completed
+   */
+  static _onLoadProgress(context: string, pct: number): void;
 }
 
 declare namespace SceneNavigation {
