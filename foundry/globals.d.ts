f680ef70e7d0d13a83a0256b45d323db290aa040
Johannes Loher <johannes.loher@fg4f.de>
2021-03-21 22:06:31 +0100

    Add global _token

---
+++ foundry/globals.d.ts
@@ -0,0 +1,5 @@
+/**
+ * A "secret" global to help debug attributes of the currently controlled Token.
+ * This is only for debugging, and may be removed in the future, so it's not safe to use.
+ */
+declare let _token: Token | null;
