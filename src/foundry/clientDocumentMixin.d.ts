ac621f3e0549300290f3de3cdacb0d4ba2e76651
Johannes Loher <johannes.loher@fg4f.de>
2021-05-29 13:18:32 +0200

    Add missing blank lines between properties in Macro and ClientDocumentMixin

--- src/foundry/clientDocumentMixin.d.ts
+++ src/foundry/clientDocumentMixin.d.ts
@@ -19,6 +19,7 @@ type ClientDocumentConstructor<T extends ConstructorOf<foundry.abstract.Document
 
 declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>> {
   constructor(data?: DeepPartial<SourceDataType<T>>, context?: ContextType<T>);
+
   /**
    * A collection of Application instances which should be re-rendered whenever this document is updated.
    * The keys of this object are the application ids and the values are Application instances. Each
@@ -26,100 +27,129 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
    * @see {@link Document#render}
    */
   apps: Record<string, Application>;
+
   /**
    * A cached reference to the FormApplication instance used to configure this Document.
    * @defaultValue `null`
    */
   protected _sheet: FormApplication | null; // TODO: FormApplication<this> | null
+
   /**
    * @see abstract.Document#_initialize
    */
   protected _initialize(): void;
+
   /**
    * Return a reference to the parent Collection instance which contains this Document.
    */
   get collection(): Collection<this>;
+
   /**
    * A reference to the Compendium Collection which contains this Document, if any, otherwise undefined.
    */
   get compendium(): any; // TODO: CompendiumCollection<this>
+
   /**
    * Return a reference to the Folder to which this Document belongs, if any.
    *
    * @example <caption>A Document may belong to a Folder</caption>
    * ```typescript
    * let folder = game.folders.entities[0];
+   *
    * let actor = await Actor.create({name: "New Actor", folder: folder.id});
-   * console.log(actor.data.folder); // folder.id;
-   * console.log(actor.folder); // folder;
+   *
+   * console.log(actor.data.folder);
+   *  // folder.id;
+   *
+   * console.log(actor.folder);
+   *  // folder;
+   *
    * ```
    */
   get folder(): Folder | null;
+
   /**
    * A boolean indicator for whether or not the current game User has ownership rights for this Document.
    * Different Document types may have more specialized rules for what constitutes ownership.
    */
   get isOwner(): boolean;
+
   /**
    * Test whether this Document is owned by any non-Gamemaster User.
    */
   get hasPlayerOwner(): boolean;
+
   /**
    * A boolean indicator for whether the current game User has exactly LIMITED visibility (and no greater).
    */
   get limited(): boolean;
+
   /**
    * Return a string which creates a dynamic link to this Document instance.
    */
   get link(): string;
+
   /**
    * Return the permission level that the current game User has over this Document.
    * See the CONST.ENTITY_PERMISSIONS object for an enumeration of these levels.
    *
    * @example
    * ```typescript
-   * game.user.id; // "dkasjkkj23kjf"
-   * actor.data.permission; // {default: 1, "dkasjkkj23kjf": 2};
-   * actor.permission; // 2
+   * game.user.id;
+   *  // "dkasjkkj23kjf"
+   * actor.data.permission;
+   *  // {default: 1, "dkasjkkj23kjf": 2};
+   *
+   * actor.permission;
+   *  // 2
    * ```
    */
   get permission(): ValueOf<typeof CONST.ENTITY_PERMISSIONS>;
+
   /**
    * Lazily obtain a FormApplication instance used to configure this Document, or null if no sheet is available.
    */
   get sheet(): FormApplication | null; // TODO: FormApplication<this> | null;
+
   /**
    * A Universally Unique Identifier (uuid) for this Document instance.
    */
   get uuid(): string;
+
   /**
    * A boolean indicator for whether or not the current game User has at least limited visibility for this Document.
    * Different Document types may have more specialized rules for what determines visibility.
    */
   get visible(): boolean;
+
   /**
    * Obtain the FormApplication class constructor which should be used to configure this Document.
    */
   protected _getSheetClass(): ConstructorOf<FormApplication> | null; // TODO: ConstructorOf<FormApplication<this>> | null
+
   /**
    * Prepare data for the Document.
    * Begin by resetting the prepared data back to its source state.
    * Next prepare any embedded Documents and compute any derived data elements.
    */
   prepareData(): void;
+
   /**
    * Prepare data related to this Document itself, before any embedded Documents or derived data is computed.
    */
   prepareBaseData(): void;
+
   /**
    * Prepare all embedded Document instances which exist within this primary Document.
    */
   prepareEmbeddedEntities(): void;
+
   /**
    * Apply transformations or derivations to the values of the source data object.
    * Compute data fields whose values are not stored to the database.
    */
   prepareDerivedData(): void;
+
   /**
    * Render all of the Application instances which are connected to this document by calling their respective
    * @see Application#render
@@ -129,6 +159,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
    *                  (default: `{}`)
    */
   render(force?: boolean, context?: Application.RenderOptions): void;
+
   /**
    * Determine the sort order for this Document by positioning it relative a target sibling.
    * See SortingHelper.performIntegerSort for more details
@@ -136,6 +167,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
    * @returns The Document after it has been re-sorted
    */
   sortRelative(options: SortOptions<this>): Promise<this>;
+
   /**
    * @see abstract.Document#_onCreate
    */
@@ -144,6 +176,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     options: DocumentModificationOptions,
     userId: string
   ): void;
+
   /**
    * @see abstract.Document#_onUpdate
    */
@@ -152,10 +185,12 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     options: DocumentModificationOptions,
     userId: string
   ): void;
+
   /**
    * @see abstract.Document#_onDelete
    */
   protected _onDelete(options: DocumentModificationOptions, userId: string): void;
+
   /**
    * Preliminary actions taken before a set of embedded Documents in this parent Document are created.
    * @param embeddedName - The name of the embedded Document type
@@ -169,6 +204,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     options: DocumentModificationOptions,
     userId: string
   ): void;
+
   /**
    * Follow-up actions taken after a set of embedded Documents in this parent Document are created.
    * @param embeddedName - The name of the embedded Document type
@@ -184,6 +220,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     options: DocumentModificationOptions,
     userId: string
   ): void;
+
   /**
    * Preliminary actions taken before a set of embedded Documents in this parent Document are updated.
    * @param embeddedName - The name of the embedded Document type
@@ -197,6 +234,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     options: DocumentModificationOptions,
     userId: string
   ): void;
+
   /**
    * Follow-up actions taken after a set of embedded Documents in this parent Document are updated.
    * @param embeddedName - The name of the embedded Document type
@@ -212,6 +250,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     options: DocumentModificationContext,
     userId: string
   ): void;
+
   /**
    * Preliminary actions taken before a set of embedded Documents in this parent Document are deleted.
    * @param embeddedName - The name of the embedded Document type
@@ -225,6 +264,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     options: DocumentModificationContext,
     userId: string
   ): void;
+
   /**
    * Follow-up actions taken after a set of embedded Documents in this parent Document are deleted.
    * @param embeddedName - The name of the embedded Document type
@@ -240,6 +280,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     options: DocumentModificationContext,
     userId: string
   ): void;
+
   /**
    * Present a Dialog form to create a new Document of this type.
    * Choose a name and a type from a select menu of types.
@@ -254,6 +295,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     data?: DeepPartial<SourceDataType<InstanceType<T>>> & Record<string, unknown>,
     options?: Dialog.Options
   ): Promise<InstanceType<ConfiguredDocumentClass<T>>>;
+
   /**
    * Present a Dialog form to confirm deletion of this Document.
    * @param options - Positioning and sizing options for the resulting dialog
@@ -261,10 +303,12 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
    * @returns A Promise which resolves to the deleted Document
    */
   deleteDialog(options?: Dialog.Options): Promise<this>;
+
   /**
    * Export entity data to a JSON file which can be saved by the client and later imported into a different session.
    */
   exportToJSON(): void;
+
   /**
    * A helper function to handle obtaining the relevant Document from dropped data provided via a DataTransfer event.
    * The dropped data could have:
@@ -280,16 +324,19 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     data: DropData<InstanceType<T>>,
     options?: FromDropDataOptions
   ): Promise<InstanceType<ConfiguredDocumentClass<T>>>;
+
   /**
    * Update this Document using a provided JSON string.
    * @param json - JSON data string
    * @returns The updated Document
    */
   importFromJSON(json: string): Promise<this>;
+
   /**
    * Render an import dialog for updating the data related to this Document through an exported JSON file
    */
   importFromJSONDialog(): Promise<void>;
+
   /**
    * Transform the Document data to be stored in a Compendium pack.
    * Remove any features of the data which are world-specific.
@@ -302,22 +349,27 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
   ): Omit<ReturnType<T['toObject']>, '_id' | 'folder' | 'permission'> & {
     permission?: ReturnType<T['toObject']>['permission']; // TODO: Whether or not this property exists depends on `pack`, improve when `pack` is typed
   };
+
   /**
    * @deprecated since 0.8.0
    */
   get _id(): T['id'];
+
   /**
    * @deprecated since 0.8.0
    */
   static get config(): any;
+
   /**
    * @deprecated since 0.8.0
    */
   get entity(): T['documentName'];
+
   /**
    * @deprecated since 0.8.0
    */
   get owner(): this['isOwner'];
+
   /**
    * @deprecated since 0.8.0
    */
@@ -326,6 +378,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     permission: keyof typeof foundry.CONST.ENTITY_PERMISSIONS | foundry.CONST.EntityPermission,
     exact?: boolean
   ): ReturnType<T['testUserPermission']>;
+
   /**
    * @deprecated since 0.8.0
    */
@@ -336,6 +389,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
       | (DeepPartial<SourceDataType<InstanceType<T>>> & { _id: string } & Record<string, unknown>),
     options?: DocumentModificationContext
   ): Promise<InstanceType<ConfiguredDocumentClass<T>>[]>;
+
   /**
    * @deprecated since 0.8.0
    */
@@ -344,10 +398,12 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     ids?: string[],
     options?: DocumentModificationContext
   ): Promise<InstanceType<ConfiguredDocumentClass<T>>[]>;
+
   /**
    * @deprecated since 0.8.0
    */
   getEmbeddedEntity(...args: Parameters<T['getEmbeddedDocument']>): ReturnType<T['getEmbeddedDocument']>;
+
   /**
    * @deprecated since 0.8.0
    */
@@ -356,6 +412,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     data?: Record<string, unknown> | Array<Record<string, unknown>>,
     options?: DocumentModificationContext
   ): ReturnType<T['createEmbeddedDocuments']>;
+
   /**
    * @deprecated since 0.8.0
    */
@@ -364,6 +421,7 @@ declare class ClientDocumentMixin<T extends foundry.abstract.Document<any, any>>
     data?: Array<Record<string, unknown>> | Record<string, unknown>,
     options?: DocumentModificationContext
   ): ReturnType<T['updateEmbeddedDocuments']>;
+
   /**
    * @deprecated since 0.8.0
    */
