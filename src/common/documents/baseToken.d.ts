0bf1e4e325c1c53544376858165975ef9158038b
Kai Moschcau <mail@kmoschcau.de>
2021-06-03 18:25:01 +0200

    Fix the schema for BaseToken

--- src/common/documents/baseToken.d.ts
+++ src/common/documents/baseToken.d.ts
@@ -1,5 +1,6 @@
 import { DocumentMetadata } from '../abstract/document';
 import { Document } from '../abstract/module';
+import { BaseUser } from './baseUser';
 
 /**
  * The Token document model.
@@ -8,10 +9,14 @@ export declare class BaseToken extends Document<any, any> {
   static get metadata(): Merge<
     DocumentMetadata,
     {
-      name: 'Tile';
-      collection: 'tiles';
-      label: 'DOCUMENT.Tile';
+      name: 'Token';
+      collection: 'tokens';
+      label: 'DOCUMENT.Token';
       isEmbedded: true;
+      permissions: {
+        create: 'TOKEN_CREATE';
+        update: (user: BaseUser, doc: any, data: any) => boolean;
+      };
     }
   >;
 }
