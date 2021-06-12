9ab79262d71d1e3a86e27341ae98f427794ca037
FloRad <florad92@gmail.com>
2021-06-12 21:11:06 +0200

    Added ActorData and adjusted BaseActor

---
+++ src/foundry/common/data/data.mjs/actorData.d.ts
@@ -0,0 +1,130 @@
+import EmbeddedCollection from '../../abstract/embedded-collection.mjs';
+import {
+  ConfiguredData,
+  ConfiguredDocumentClass,
+  ConfiguredSource,
+  FieldReturnType,
+  PropertiesToSource
+} from '../../../../types/helperTypes';
+import { DocumentData } from '../../abstract/module.mjs';
+import * as documents from '../../documents.mjs';
+import * as fields from '../fields.mjs';
+import { PrototypeTokenData } from '../data.mjs';
+
+interface ActorDataSchema extends DocumentSchema {
+  _id: typeof fields.DOCUMENT_ID;
+  name: typeof fields.REQUIRED_STRING;
+  type: DocumentField<string> & {
+    type: typeof String;
+    required: true;
+    validate: (t: unknown) => boolean;
+    validationError: 'The provided Actor type must be in the array of types defined by the game system';
+  };
+  img: FieldReturnType<typeof fields.IMAGE_FIELD, { default: () => string }>;
+  data: FieldReturnType<typeof fields.OBJECT_FIELD, { default: (data: { type: string }) => any }>; // TODO
+  token: {
+    type: typeof PrototypeTokenData;
+    required: true;
+    default: (data: unknown) => { name: string; img: string };
+  };
+  items: fields.EmbeddedCollectionField<typeof documents.BaseItem>;
+  effects: fields.EmbeddedCollectionField<typeof documents.BaseActiveEffect>;
+  folder: fields.ForeignDocumentField<{ type: typeof documents.BaseFolder }>;
+  sort: typeof fields.INTEGER_SORT_FIELD;
+  permission: typeof fields.DOCUMENT_PERMISSIONS;
+  flags: typeof fields.OBJECT_FIELD;
+}
+
+interface ActorDataBaseProperties {
+  /**
+   * The _id which uniquely identifies this Actor document
+   */
+  _id: string | null;
+
+  /**
+   * The name of this Actor
+   */
+  name: string;
+
+  /**
+   * An Actor subtype which configures the system data model applied
+   */
+  type: string;
+
+  /**
+   * An image file path which provides the artwork for this Actor
+   */
+  img?: string | null;
+
+  /**
+   * The system data object which is defined by the system template.json model
+   */
+  data: object;
+
+  /**
+   * Default Token settings which are used for Tokens created from this Actor
+   */
+  token: PrototypeTokenData;
+
+  /**
+   * A Collection of Item embedded Documents
+   */
+  items: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseItem>, ActorData>;
+
+  /**
+   * A collection of ActiveEffect embedded Documents
+   */
+  effects: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseActiveEffect>, ActorData>;
+
+  /**
+   * The _id of a Folder which contains this Actor
+   * @defaultValue `null`
+   */
+  folder: string | null;
+
+  /**
+   * The numeric sort value which orders this Actor relative to its siblings
+   * @defaultValue `0`
+   */
+  sort: number;
+
+  /**
+   * An object which configures user permissions to this Actor
+   * @defaultValue `{ default: CONST.ENTITY_PERMISSIONS.NONE }`
+   */
+  permission: Partial<Record<string, ValueOf<typeof CONST.ENTITY_PERMISSIONS>>>;
+
+  /**
+   * An object of optional key/value flags
+   * @defaultValue `{}`
+   */
+  flags: Record<string, unknown>;
+}
+
+type ActorDataBaseSource = PropertiesToSource<ActorDataBaseProperties>;
+type ActorDataProperties = ActorDataBaseProperties & ConfiguredData<'Actor'>;
+type ActorDataSource = ActorDataBaseSource & ConfiguredSource<'Actor'>;
+
+type DocumentDataConstructor = typeof DocumentData;
+
+interface ActorDataConstructor extends DocumentDataConstructor {
+  new (data?: DeepPartial<ActorDataSource>, document?: documents.BaseActor | null): ActorData;
+
+  defineSchema(): ActorDataSchema;
+
+  /**
+   * The default icon used for newly created Actor documents
+   * @defaultValue `"icons/svg/mystery-man.svg"`
+   */
+  DEFAULT_ICON: string;
+}
+
+/**
+ * The data schema for a Actor document.
+ * @see BaseActor
+ */
+export type ActorData = DocumentData<ActorDataSchema, ActorDataProperties, ActorDataSource, documents.BaseActor> &
+  ActorDataProperties & {
+    _initializeSource(data: DeepPartial<ActorDataSource>): ActorDataSource;
+  };
+export declare const ActorData: ActorDataConstructor;
--- src/foundry/common/documents.mjs/baseActor.d.ts
+++ src/foundry/common/documents.mjs/baseActor.d.ts
@@ -2,8 +2,11 @@ import { DocumentMetadata } from '../abstract/document.mjs';
 import { Document } from '../abstract/module.mjs';
 import { BaseActiveEffect } from './baseActiveEffect';
 import { BaseItem } from './baseItem';
+import * as data from '../data/data.mjs';
 
-export declare class BaseActor extends Document<any, null> {
+export declare class BaseActor extends Document<data.ActorData, null> {
+  static get schema(): typeof data.ActorData;
+
   static get metadata(): Merge<
     DocumentMetadata,
     {
@@ -22,4 +25,41 @@ export declare class BaseActor extends Document<any, null> {
       types: string[];
     }
   >;
+  /*
+   * A reference to the Collection of embedded ActiveEffect instances in the Actor document, indexed by _id.
+   */
+  get effects(): this['data']['effects'];
+
+  /**
+   * A reference to the Collection of embedded Item instances in the Actor document, indexed by _id.
+   */
+  get items(): this['data']['items'];
+
+  /**
+   * Migrate the system data object to conform to data model defined by the current system version.
+   * @see mergeObject
+   * @param options - Options which customize how the system data is migrated.
+   * @returns The migrated system data object, not yet saved to the database
+   */
+  migrateSystemData(options?: MigrateSystemDataOptions): object;
+}
+
+interface MigrateSystemDataOptions {
+  /**
+   * Retain keys which exist in the current data, but not the model
+   * @defaultValue `false`
+   */
+  insertKeys?: boolean;
+
+  /**
+   * Retain inner-object values which exist in the current data, but not the model
+   * @defaultValue `true`
+   */
+  insertValues?: boolean;
+
+  /**
+   * Require that data types match the model exactly to be retained
+   * @defaultValue `false`
+   */
+  enforceTypes?: boolean;
 }
