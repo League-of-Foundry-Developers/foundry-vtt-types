ae5a65daead9b295a572e89d77fea1cc60ecb353
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-06-13 14:39:09 +0200

    Fix type

--- src/foundry/foundry.js/pixi/containers/placeableObject.d.ts
+++ src/foundry/foundry.js/pixi/containers/placeableObject.d.ts
@@ -436,13 +436,13 @@ declare global {
 interface Vision {
   /**
    * @remarks
-   * This is required but has ben set to optional because of pointsource
+   * This is required but has been set to optional because of pointsource
    */
   fov?: PIXI.Polygon | null;
 
   /**
    * @remarks
-   * This is required but has ben set to optional because of pointsource
+   * This is required but has been set to optional because of pointsource
    */
   los?: PIXI.Polygon | null;
 }
