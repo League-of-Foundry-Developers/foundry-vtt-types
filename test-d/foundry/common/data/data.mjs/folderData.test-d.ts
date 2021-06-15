eef1a9c000290fa4e5093f32a2f5c415f1a5a00e
Kai Moschcau <mail@kmoschcau.de>
2021-06-15 21:20:49 +0200

    Add tests for some DocumentData children

---
+++ test-d/foundry/common/data/data.mjs/folderData.test-d.ts
@@ -0,0 +1,8 @@
+import { expectError, expectType } from 'tsd';
+
+expectError(new foundry.data.FolderData());
+expectError(new foundry.data.FolderData({}));
+expectError(new foundry.data.FolderData({ name: 'foo', type: 'foo' }));
+expectType<foundry.data.FolderData>(
+  new foundry.data.FolderData({ name: 'foo', type: foundry.CONST.FOLDER_ENTITY_TYPES[0] })
+);
