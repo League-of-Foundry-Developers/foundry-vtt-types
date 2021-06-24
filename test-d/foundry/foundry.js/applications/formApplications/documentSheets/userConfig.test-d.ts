c32c9f1f2a0c2230c3f4920c6ae87042a909b00f
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-06-24 09:11:59 +0200

    Rebase and adjust tests of UserConfig

--- test-d/foundry/foundry.js/applications/formApplications/documentSheets/userConfig.test-d.ts
+++ test-d/foundry/foundry.js/applications/formApplications/documentSheets/userConfig.test-d.ts
@@ -1,19 +1,18 @@
 import { expectError, expectType } from 'tsd';
 
-expectType<UserConfig>(new UserConfig(new User()));
-expectType<UserConfig>(new UserConfig());
-expectType<UserConfig>(new UserConfig(undefined, { width: 100 }));
+expectError<UserConfig>(new UserConfig());
+expectError<UserConfig>(new UserConfig(undefined, { width: 100 }));
 expectError(new UserConfig(new foundry.documents.BaseUser()));
 
-const config = new UserConfig();
+const config = new UserConfig(new User());
 expectType<User>(config.object);
 expectType<User>(config.getData().user);
 expectType<Actor[]>(config.getData().actors);
 expectType<DocumentSheet.Options>(config.getData().options);
 
-const withCustomOptions = new UserConfig<DocumentSheet.Options & { custom: true }>();
+const withCustomOptions = new UserConfig<DocumentSheet.Options & { custom: true }>(new User());
 expectType<DocumentSheet.Options & { custom: true }>(withCustomOptions.options);
 expectType<DocumentSheet.Options & { custom: true }>(withCustomOptions.getData().options);
 
-const withCustomData = new UserConfig<DocumentSheet.Options, { me: User }>();
+const withCustomData = new UserConfig<DocumentSheet.Options, { me: User }>(new User());
 expectType<{ me: User }>(withCustomData.getData());
