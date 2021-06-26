5a4d6c24c49f64db291a26f4dfb4bae411da414c
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-06-26 16:32:52 +0200

    Update _token

--- src/foundry/foundry.js/globals.d.ts
+++ src/foundry/foundry.js/globals.d.ts
@@ -2,4 +2,4 @@
  * A "secret" global to help debug attributes of the currently controlled Token.
  * This is only for debugging, and may be removed in the future, so it's not safe to use.
  */
-declare let _token: Token | null;
+declare let _token: InstanceType<CONFIG['Token']['objectClass']> | null;
