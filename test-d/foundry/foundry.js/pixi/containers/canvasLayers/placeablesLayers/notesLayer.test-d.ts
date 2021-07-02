d70766b6a30b2b16d9524275cd3a88ac292a098b
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-07-02 17:34:40 +0200

    Test NotesLayer

---
+++ test-d/foundry/foundry.js/pixi/containers/canvasLayers/placeablesLayers/notesLayer.test-d.ts
@@ -0,0 +1,20 @@
+import { expectType } from 'tsd';
+
+expectType<'Note'>(NotesLayer.documentName);
+expectType<NotesLayer>(NotesLayer.instance);
+expectType<NotesLayer.LayerOptions>(NotesLayer.layerOptions);
+expectType<'notes'>(NotesLayer.layerOptions.name);
+expectType<ConstructorOf<Note>>(NotesLayer.layerOptions.objectClass);
+
+expectType<'notesDisplayToggle'>(NotesLayer.TOGGLE_SETTING);
+
+expectType<void>(NotesLayer.registerSettings());
+
+const layer = new NotesLayer();
+expectType<ConstructorOf<Note>>(layer.options.objectClass);
+expectType<NotesLayer.LayerOptions>(layer.options);
+expectType<'notes'>(layer.options.name);
+
+expectType<NotesLayer>(layer.activate());
+
+expectType<NotesLayer>(layer.deactivate());
