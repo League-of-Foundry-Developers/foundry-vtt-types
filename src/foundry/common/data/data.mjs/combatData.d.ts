0ebab613b5cbc471b3b85e2adf32d54fe440b035
FloRad <5250409+FloRad@users.noreply.github.com>
2021-07-02 13:17:50 +0200

    Update src/foundry/common/data/data.mjs/combatData.d.ts
    Co-authored-by: Johannes Loher <johannes.loher@fg4f.de>

--- src/foundry/common/data/data.mjs/combatData.d.ts
+++ src/foundry/common/data/data.mjs/combatData.d.ts
@@ -64,7 +64,7 @@ interface CombatDataConstructorData {
   scene?: string | null;
 
   /** A Collection of Combatant embedded Documents */
-  combatants?: Parameters<CombatantData['_initializeSource']>[0][] | null;
+  combatants?: Parameters<foundry.data.CombatantData['_initializeSource']>[0][] | null;
 
   /**
    * Is the Combat encounter currently active?
