ed249f17f6c6da62b95d27867491af57d9a78c2e
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-07-02 09:21:10 +0200

    Test name of EffectsLayer

--- test-d/foundry/foundry.js/pixi/containers/canvasLayers/effectsLayer.test-d.ts
+++ test-d/foundry/foundry.js/pixi/containers/canvasLayers/effectsLayer.test-d.ts
@@ -2,8 +2,10 @@ import { expectType } from 'tsd';
 
 expectType<EffectsLayer>(EffectsLayer.instance);
 expectType<EffectsLayer.LayerOptions>(EffectsLayer.layerOptions);
+expectType<'effects'>(EffectsLayer.layerOptions.name);
 
 const layer = new EffectsLayer();
+expectType<'effects'>(layer.options.name);
 expectType<PIXI.Container | undefined>(layer.weather);
 expectType<SpecialEffect | undefined>(layer.weatherEffect);
 expectType<unknown[]>(layer.emitters);
