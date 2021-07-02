d61aa6e8d16fb30387e318b0cebbadfa9a5a65f1
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-07-02 18:08:50 +0200

    Test SoundsLayer

---
+++ test-d/foundry/foundry.js/pixi/containers/canvasLayers/placeablesLayers/soundsLayer.test-d.ts
@@ -0,0 +1,26 @@
+import { expectType } from 'tsd';
+
+expectType<'AmbientSound'>(SoundsLayer.documentName);
+expectType<SoundsLayer>(SoundsLayer.instance);
+expectType<SoundsLayer.LayerOptions>(SoundsLayer.layerOptions);
+expectType<'sounds'>(SoundsLayer.layerOptions.name);
+expectType<ConstructorOf<AmbientSound>>(SoundsLayer.layerOptions.objectClass);
+
+const layer = new SoundsLayer();
+expectType<ConstructorOf<AmbientSound>>(layer.options.objectClass);
+expectType<SoundsLayer.LayerOptions>(layer.options);
+expectType<'sounds'>(layer.options.name);
+
+expectType<boolean>(layer.livePreview);
+
+expectType<Promise<SoundsLayer>>(layer.tearDown());
+
+expectType<void>(layer.initializeSources());
+
+expectType<number | void>(layer.refresh());
+expectType<number | void>(layer.refresh({}));
+expectType<number | void>(layer.refresh({ fade: true }));
+
+expectType<void>(layer.previewSound({ x: 100, y: 0 }));
+
+expectType<void>(layer.stopAll());
