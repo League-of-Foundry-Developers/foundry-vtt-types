3d2ed04cead32e27c693c850539a377d6cd648ea
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-07-10 07:43:41 +0200

    Fix erroneous FogExploration

--- src/foundry/foundry.js/clientDocuments/fogExploration.d.ts
+++ src/foundry/foundry.js/clientDocuments/fogExploration.d.ts
@@ -1,15 +1,12 @@
 //TODO: This is just a stub
-import { ClientDocumentMixin } from '../clientDocumentMixin';
-
 declare global {
   /**
-
    * The client-side FogExploration document which extends the common BaseFogExploration model.
    * Each FogExploration document contains FogExplorationData which defines its data schema.
    *
    * @see {@link data.FogExplorationData}              The FogExploration data schema
    */
-  class FogExploration extends ClientDocumentMixin<foundry.documents.BaseFogExploration> {}
+  class FogExploration extends ClientDocumentMixin(foundry.documents.BaseFogExploration) {}
 }
 
 export {};
