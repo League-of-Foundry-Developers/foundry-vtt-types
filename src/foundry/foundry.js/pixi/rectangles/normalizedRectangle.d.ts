5b5b1955b95b54371a0fa7576d68596f4a163ab7
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-07-10 20:35:38 +0200

    Update NormalizedRectangle

--- src/foundry/foundry.js/pixi/rectangles/normalizedRectangle.d.ts
+++ src/foundry/foundry.js/pixi/rectangles/normalizedRectangle.d.ts
@@ -3,4 +3,22 @@
  */
 declare class NormalizedRectangle extends PIXI.Rectangle {
   constructor(x: number, y: number, width: number, height: number);
+
+  /**
+   * Generate a new rectangle by rotating this one clockwise about its center by a certain number of radians
+   * @param radians - The angle of rotation
+   * @returns A new rotated rectangle
+   */
+  rotate(radians: number): NormalizedRectangle;
+
+  /**
+   * Create normalized rectangular bounds given a rectangle shape and an angle of central rotation.
+   * @param x       - The top-left x-coordinate of the un-rotated rectangle
+   * @param y       - The top-left y-coordinate of the un-rotated rectangle
+   * @param width   - The width of the un-rotated rectangle
+   * @param height  - The height of the un-rotated rectangle
+   * @param radians - The angle of rotation about the center
+   * @returns The constructed rotated rectangle bounds
+   */
+  static fromRotation(x: number, y: number, width: number, height: number, radians: number): NormalizedRectangle;
 }
