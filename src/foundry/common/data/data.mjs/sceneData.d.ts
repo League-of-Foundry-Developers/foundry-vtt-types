cc6c23656be0c3e7055f0dd031296656b8603560
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-07-12 20:41:44 +0200

    Update SceneDataProperties

--- src/foundry/common/data/data.mjs/sceneData.d.ts
+++ src/foundry/common/data/data.mjs/sceneData.d.ts
@@ -1,4 +1,10 @@
-import { FieldReturnType, PropertiesToSource } from '../../../../types/helperTypes';
+import {
+  ConfiguredDocumentClass,
+  ConfiguredFlags,
+  FieldReturnType,
+  PropertiesToSource
+} from '../../../../types/helperTypes';
+import EmbeddedCollection from '../../abstract/embedded-collection.mjs';
 import { DocumentData } from '../../abstract/module.mjs';
 import * as fields from '../fields.mjs';
 import * as documents from '../../documents.mjs';
@@ -85,13 +91,519 @@ interface SceneDataSchema extends DocumentSchema {
 }
 
 interface SceneDataProperties {
+  /**
+   * The _id which uniquely identifies this Scene document
+   */
   _id: string | null;
+
+  /**
+   * The name of this scene
+   */
+  name: string;
+
+  /**
+   * Is this scene currently active? Only one scene may be active at a given time.
+   * @defaultValue `false`
+   */
+  active: boolean;
+
+  /**
+   * Is this scene displayed in the top navigation bar?
+   * @defaultValue `true`
+   */
+  navigation: boolean;
+
+  /**
+   * The integer sorting order of this Scene in the navigation bar relative to others
+   */
+  navOrder: number;
+
+  /**
+   * A string which overrides the canonical Scene name which is displayed in the navigation bar
+   * @defaultValue `''`
+   */
+  navName: string;
+
+  /**
+   * An image or video file path which provides the background media for the scene
+   * @defaultValue `undefined`
+   */
+  img: string | undefined | null;
+
+  /**
+   * @defaultValue `undefined`
+   */
+  foreground: string | undefined | null;
+
+  /**
+   * A thumbnail image (base64) or file path which visually summarizes the scene
+   * @defaultValue `undefined`
+   */
+  thumb: string | undefined | null;
+
+  /**
+   * The width of the scene canvas, this should normally be the width of the background media
+   * @defaultValue `4000`
+   */
+  width: number;
+
+  /**
+   * The height of the scene canvas, this should normally be the height of the background media
+   * @defaultValue `3000`
+   */
+  height: number;
+
+  /**
+   * The proportion of canvas padding applied around the outside of the scene
+   * dimensions to provide additional buffer space
+   * @defaultValue `0.25`
+   */
+  padding: number;
+
+  /**
+   * The initial view coordinates for the scene, or null
+   * @defaultValue `null`
+   */
+  initial: { x: number; y: number; scale: number } | null;
+
+  /**
+   * The color of the canvas which is displayed behind the scene background
+   * @defaultValue `#999999`
+   */
+  backgroundColor: string | null;
+
+  /**
+   * The type of grid used in this scene, a number from CONST.GRID_TYPES
+   * @defaultValue `CONST.GRID_TYPES.SQUARE`
+   */
+  gridType: CONST.GridType;
+
+  /**
+   * The grid size which represents the width (or height) of a single grid space
+   * @defaultValue `100`
+   */
+  grid: number;
+
+  /**
+   * A number of offset pixels that the background image is shifted horizontally relative to the grid
+   * @defaultValue `0`
+   */
+  shiftX: number;
+
+  /**
+   * A number of offset pixels that the background image is shifted vertically relative to the grid
+   * @defaultValue `0`
+   */
+  shiftY: number;
+
+  /**
+   * A string representing the color used to render the grid lines
+   * @defaultValue `#000000`
+   */
+  gridColor: string | null;
+
+  /**
+   * A number between 0 and 1 for the opacity of the grid lines
+   * @defaultValue `0.2`
+   */
+  gridAlpha: number;
+
+  /**
+   * The number of distance units which are represented by a single grid space.
+   */
+  gridDistance: number;
+
+  /**
+   * A label for the units of measure which are used for grid distance.
+   */
+  gridUnits: string;
+
+  /**
+   * Do Tokens require vision in order to see the Scene environment?
+   * @defaultValue `true`
+   */
+  tokenVision: boolean;
+
+  /**
+   * Should fog exploration progress be tracked for this Scene?
+   * @defaultValue `true`
+   */
+  fogExploration: boolean;
+
+  /**
+   * The timestamp at which fog of war was last reset for this Scene.
+   */
+  fogReset: number;
+
+  /**
+   * Does this Scene benefit from global illumination which provides bright light everywhere?
+   * @defaultValue `false`
+   */
+  globalLight: boolean;
+
+  /**
+   * A darkness level between 0 and 1, beyond which point global illumination is
+   * temporarily disabled if globalLight is true.
+   * @defaultValue `null`
+   */
+  globalLightThreshold: number | null;
+
+  /**
+   * The ambient darkness level in this Scene, where 0 represents mid-day
+   * (maximum illumination) and 1 represents mid-night (maximum darkness)
+   * @defaultValue `0`
+   */
+  darkness: number;
+
+  /**
+   * A collection of embedded Drawing objects.
+   */
+  drawings: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseDrawing>, SceneData>;
+
+  /**
+   * A collection of embedded Token objects.
+   */
+  tokens: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseToken>, SceneData>;
+
+  /**
+   *
+   * A collection of embedded AmbientLight objects.
+   */
+  lights: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseAmbientLight>, SceneData>;
+
+  /**
+   * A collection of embedded Note objects.
+   */
+  notes: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseNote>, SceneData>;
+
+  /**
+   * A collection of embedded AmbientSound objects.
+   */
+  sounds: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseAmbientSound>, SceneData>;
+
+  /**
+   * A collection of embedded MeasuredTemplate objects.
+   */
+  templates: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseMeasuredTemplate>, SceneData>;
+
+  /**
+   * A collection of embedded Tile objects.
+   */
+  tiles: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseTile>, SceneData>;
+
+  /**
+   * A collection of embedded Wall objects
+   */
+  walls: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseWall>, SceneData>;
+
+  /**
+   * A linked Playlist document which should begin automatically playing when this
+   * Scene becomes active.
+   * @defaultValue `null`
+   */
+  playlist: string | null;
+
+  /**
+   * @defaultValue `null`
+   */
+  playlistSound: string | null;
+
+  /**
+   * A linked JournalEntry document which provides narrative details about this Scene.
+   * @defaultValue `null`
+   */
+  journal: string | null;
+
+  /**
+   * A named weather effect which should be rendered in this Scene.
+   * @defaultValue `''`
+   */
+  weather: string;
+
+  /**
+   * The _id of a Folder which contains this Actor
+   * @defaultValue `null`
+   */
+  folder: string | null;
+
+  /**
+   * The numeric sort value which orders this Actor relative to its siblings
+   * @defaultValue `0`
+   */
+  sort: number;
+
+  /**
+   * An object which configures user permissions to this Actor
+   * @defaultValue `{ default: CONST.ENTITY_PERMISSIONS.NONE }`
+   */
+  permission: Partial<Record<string, foundry.CONST.EntityPermission>>;
+
+  /**
+   * An object of optional key/value flags
+   */
+  flags: ConfiguredFlags<'Scene'>;
+
+  size: typeof NaN;
 }
 
 interface SceneDataConstructorData {
+  /**
+   * The _id which uniquely identifies this Scene document
+   */
   _id?: string | null;
+
+  /**
+   * The name of this scene
+   */
+  name: string;
+
+  /**
+   * Is this scene currently active? Only one scene may be active at a given time.
+   * @defaultValue `false`
+   */
+  active?: boolean;
+
+  /**
+   * Is this scene displayed in the top navigation bar?
+   * @defaultValue `true`
+   */
+  navigation?: boolean;
+
+  /**
+   * The integer sorting order of this Scene in the navigation bar relative to others
+   */
+  navOrder?: number;
+
+  /**
+   * A string which overrides the canonical Scene name which is displayed in the navigation bar
+   * @defaultValue `''`
+   */
+  navName?: string;
+
+  /**
+   * An image or video file path which provides the background media for the scene
+   * @defaultValue `undefined`
+   */
+  img?: string | undefined | null;
+
+  /**
+   * @defaultValue `undefined`
+   */
+  foreground?: string | undefined | null;
+
+  /**
+   * A thumbnail image (base64) or file path which visually summarizes the scene
+   * @defaultValue `undefined`
+   */
+  thumb?: string | undefined | null;
+
+  /**
+   * The width of the scene canvas, this should normally be the width of the background media
+   * @defaultValue `4000`
+   */
+  width?: number;
+
+  /**
+   * The height of the scene canvas, this should normally be the height of the background media
+   * @defaultValue `3000`
+   */
+  height?: number;
+
+  /**
+   * The proportion of canvas padding applied around the outside of the scene
+   * dimensions to provide additional buffer space
+   * @defaultValue `0.25`
+   */
+  padding?: number;
+
+  /**
+   * The initial view coordinates for the scene, or null
+   * @defaultValue `null`
+   */
+  initial?: { x: number; y: number; scale: number } | null;
+
+  /**
+   * The color of the canvas which is displayed behind the scene background
+   * @defaultValue `#999999`
+   */
+  backgroundColor?: string | null;
+
+  /**
+   * The type of grid used in this scene, a number from CONST.GRID_TYPES
+   * @defaultValue `CONST.GRID_TYPES.SQUARE`
+   */
+  gridType?: CONST.GridType;
+
+  /**
+   * The grid size which represents the width (or height) of a single grid space
+   * @defaultValue `100`
+   */
+  grid?: number;
+
+  /**
+   * A number of offset pixels that the background image is shifted horizontally relative to the grid
+   * @defaultValue `0`
+   */
+  shiftX?: number;
+
+  /**
+   * A number of offset pixels that the background image is shifted vertically relative to the grid
+   * @defaultValue `0`
+   */
+  shiftY?: number;
+
+  /**
+   * A string representing the color used to render the grid lines
+   * @defaultValue `#000000`
+   */
+  gridColor?: string | null;
+
+  /**
+   * A number between 0 and 1 for the opacity of the grid lines
+   * @defaultValue `0.2`
+   */
+  gridAlpha?: number;
+
+  /**
+   * The number of distance units which are represented by a single grid space.
+   */
+  gridDistance?: number;
+
+  /**
+   * A label for the units of measure which are used for grid distance.
+   */
+  gridUnits?: string;
+
+  /**
+   * Do Tokens require vision in order to see the Scene environment?
+   * @defaultValue `true`
+   */
+  tokenVision?: boolean;
+
+  /**
+   * Should fog exploration progress be tracked for this Scene?
+   * @defaultValue `true`
+   */
+  fogExploration?: boolean;
+
+  /**
+   * The timestamp at which fog of war was last reset for this Scene.
+   */
+  fogReset?: number;
+
+  /**
+   * Does this Scene benefit from global illumination which provides bright light everywhere?
+   * @defaultValue `false`
+   */
+  globalLight?: boolean;
+
+  /**
+   * A darkness level between 0 and 1, beyond which point global illumination is
+   * temporarily disabled if globalLight is true.
+   * @defaultValue `null`
+   */
+  globalLightThreshold?: number | null;
+
+  /**
+   * The ambient darkness level in this Scene, where 0 represents mid-day
+   * (maximum illumination) and 1 represents mid-night (maximum darkness)
+   * @defaultValue `0`
+   */
+  darkness?: number;
+
+  /**
+   * A collection of embedded Drawing objects.
+   */
+  drawings?: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseDrawing>, SceneData>;
+
+  /**
+   * A collection of embedded Token objects.
+   */
+  tokens?: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseToken>, SceneData>;
+
+  /**
+   *
+   * A collection of embedded AmbientLight objects.
+   */
+  lights?: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseAmbientLight>, SceneData>;
+
+  /**
+   * A collection of embedded Note objects.
+   */
+  notes?: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseNote>, SceneData>;
+
+  /**
+   * A collection of embedded AmbientSound objects.
+   */
+  sounds?: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseAmbientSound>, SceneData>;
+
+  /**
+   * A collection of embedded MeasuredTemplate objects.
+   */
+  templates?: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseMeasuredTemplate>, SceneData>;
+
+  /**
+   * A collection of embedded Tile objects.
+   */
+  tiles?: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseTile>, SceneData>;
+
+  /**
+   * A collection of embedded Wall objects
+   */
+  walls?: EmbeddedCollection<ConfiguredDocumentClass<typeof documents.BaseWall>, SceneData>;
+
+  /**
+   * A linked Playlist document which should begin automatically playing when this
+   * Scene becomes active.
+   * @defaultValue `null`
+   */
+  playlist?: string | null;
+
+  /**
+   * @defaultValue `null`
+   */
+  playlistSound?: string | null;
+
+  /**
+   * A linked JournalEntry document which provides narrative details about this Scene.
+   * @defaultValue `null`
+   */
+  journal?: string | null;
+
+  /**
+   * A named weather effect which should be rendered in this Scene.
+   * @defaultValue `''`
+   */
+  weather?: string;
+
+  /**
+   * The _id of a Folder which contains this Actor
+   * @defaultValue `null`
+   */
+  folder?: string | null;
+
+  /**
+   * The numeric sort value which orders this Actor relative to its siblings
+   * @defaultValue `0`
+   */
+  sort?: number;
+
+  /**
+   * An object which configures user permissions to this Actor
+   * @defaultValue `{ default: CONST.ENTITY_PERMISSIONS.NONE }`
+   */
+  permission?: Partial<Record<string, foundry.CONST.EntityPermission>>;
+
+  /**
+   * An object of optional key/value flags
+   */
+  flags?: ConfiguredFlags<'Scene'>;
 }
 
+/**
+ * The data schema for a Scene document.
+ * @see BaseScene
+ */
 export declare class SceneData extends DocumentData<
   SceneDataSchema,
   SceneDataProperties,
