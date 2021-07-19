0944337cd48399e7a1c404e7e0b285e53636838d
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-07-19 22:30:58 +0200

    Update CanvasAnimation

--- src/foundry/foundry.js/canvasAnimation.d.ts
+++ src/foundry/foundry.js/canvasAnimation.d.ts
@@ -5,13 +5,20 @@ declare class CanvasAnimation {
   static get ticker(): PIXI.Ticker;
 
   /**
+   * Track an object of active animations by name, context, and function
+   * This allows a currently playing animation to be referenced and terminated
+   */
+  static animations: Record<
+    string,
+    { fn: (dt: number) => void; context: PIXI.Container; resolve: (value: boolean) => void }
+  >;
+
+  /**
    * Apply a linear animation from the current value of some attribute to a new value
    * Resolve a Promise once the animation has concluded and the attributes have reached their new target
    * @param attributes - An array of attributes to animate. Structure of the Array is shown in the example
-   * @param context    - An animation context to use which defines scope
-   * @param name       - Provide a unique animation name which may be referenced later (default: `null`)
-   * @param duration   - The duration in milliseconds over which the animation should occur (default: `1000`)
-   * @param ontick     - A function which defines additional behaviors to apply every animation frame
+   * @param options    - Additional options which customize the animation
+   *                     (default: `{}`)
    * @returns A Promise which resolves once the linear animation has concluded
    *
    * @example
@@ -33,18 +40,8 @@ declare class CanvasAnimation {
    */
   static animateLinear(
     attributes: CanvasAnimation.Attribute[],
-    {
-      context,
-      name,
-      duration,
-      ontick
-    }: {
-      context: PIXI.Container;
-      name?: string | null;
-      duration?: number;
-      ontick: (dt: number, attributes: CanvasAnimation.Attribute[]) => void;
-    }
-  ): Promise<void>;
+    options?: Partial<LinearAnimationOptions>
+  ): Promise<boolean>;
 
   /**
    * If an animation using a certain name already exists, terminate it
@@ -54,18 +51,22 @@ declare class CanvasAnimation {
 
   /**
    * Asynchronously animate a transition function and resolve a Promise once the animation has completed
-   * @param fn      - A suitable transition function. See PIXI.Ticker for details
-   * @param context - The Canvas container providing scope for the transition
-   * @param name    - Provide a unique animation name which may be referenced later
-   * @param args    - Variable argument passed to the transition function each frame
+   * @param fn         - A suitable transition function. See PIXI.Ticker for details
+   * @param context    - The Canvas container providing scope for the transition
+   * @param name       -  Provide a unique animation name which may be referenced later
+   * @param attributes - The attributes being animated by the function
+   * @param duration   - The duration in milliseconds over which the animation should occur
+   * @param ontick     - A function which defines additional behaviors to apply every animation frame
    * @returns A Promise which resolves once the animation has completed
    */
   protected static _animatePromise(
-    fn: (dt: number, resolve: () => void, reject: (reason?: any) => void, ...args: any[]) => void,
+    fn: TransitionFunction,
     context: PIXI.Container,
     name: string,
-    ...args: any[]
-  ): Promise<void>;
+    attributes: CanvasAnimation.Attribute[],
+    duration: number,
+    ontick: TickFunction | undefined
+  ): Promise<boolean>;
 
   /**
    * Generic ticker function to implement the animation.
@@ -75,18 +76,12 @@ declare class CanvasAnimation {
    */
   protected static _animateFrame(
     deltaTime: number,
-    resolve: () => void,
+    resolve: (value: boolean) => void,
     reject: (reason?: any) => void,
     attributes: CanvasAnimation.Attribute[],
     duration: number,
-    ontick: (dt: number, attributes: CanvasAnimation.Attribute[]) => void
+    ontick: TickFunction | undefined
   ): void;
-
-  /**
-   * Track an object of active animations by name, context, and function
-   * This allows a currently playing animation to be referenced and terminated
-   */
-  static animations: Record<string, [(dt: number) => unknown, PIXI.Container]>;
 }
 
 declare namespace CanvasAnimation {
@@ -96,3 +91,36 @@ declare namespace CanvasAnimation {
     to: number;
   }
 }
+
+interface LinearAnimationOptions {
+  /**
+   * An animation context to use which defines scope
+   */
+  context: PIXI.Container;
+
+  /**
+   * Provide a unique animation name which may be referenced later
+   */
+  name: string;
+
+  /**
+   * The duration in milliseconds over which the animation should occur
+   * @defaultValue `1000`
+   */
+  duration: number;
+
+  /**
+   * A function which defines additional behaviors to apply every animation frame
+   */
+  ontick: TickFunction;
+}
+
+type TickFunction = (dt: number, attributes: CanvasAnimation.Attribute[]) => void;
+type TransitionFunction = (
+  dt: number,
+  resolve: (value: boolean) => void,
+  reject: (reason?: any) => void,
+  attributes: CanvasAnimation.Attribute[],
+  duration: number,
+  ontick?: TickFunction
+) => void;
---
+++ test-d/foundry/foundry.js/canvasAnimation.test-d.ts
@@ -0,0 +1,20 @@
+import { expectType } from 'tsd';
+
+expectType<PIXI.Ticker>(CanvasAnimation.ticker);
+expectType<Record<string, { fn: Function; context: PIXI.Container; resolve: (value: boolean) => void }>>(
+  CanvasAnimation.animations
+);
+expectType<Promise<boolean>>(CanvasAnimation.animateLinear([{ parent: canvas?.stage.pivot, attribute: 'x', to: 200 }]));
+expectType<Promise<boolean>>(
+  CanvasAnimation.animateLinear([{ parent: canvas?.stage.pivot, attribute: 'x', to: 200 }], {})
+);
+expectType<Promise<boolean>>(
+  CanvasAnimation.animateLinear([{ parent: canvas?.stage.pivot, attribute: 'x', to: 200 }], {
+    context: canvas?.stage,
+    name: 'my-animation',
+    duration: 1000,
+    ontick: () => console.log('ticking')
+  })
+);
+
+expectType<void>(CanvasAnimation.terminateAnimation('my-animation'));
