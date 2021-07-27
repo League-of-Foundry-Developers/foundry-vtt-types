572b69a2e83d550179ab2b5a611e2e29f1deda9d
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-07-27 19:36:25 +0200

    Fix declaration

--- src/foundry/foundry.js/applications/playerList.d.ts
+++ src/foundry/foundry.js/applications/playerList.d.ts
@@ -1,56 +1,58 @@
-/**
- * The UI element which displays the list of Users who are currently playing within the active World.
- *
- * @typeParam Options - the type of the options object
- */
 import { ConfiguredDocumentClass } from '../../../types/helperTypes';
 
-declare class PlayerList<Options extends Application.Options = Application.Options> extends Application<Options> {
-  constructor(options?: Partial<Options>);
-
+declare global {
   /**
-   * An internal toggle for whether or not to show offline players or hide them
-   * @defaultValue `false`
+   * The UI element which displays the list of Users who are currently playing within the active World.
+   *
+   * @typeParam Options - the type of the options object
    */
-  protected _showOffline: boolean;
+  class PlayerList<Options extends Application.Options = Application.Options> extends Application<Options> {
+    constructor(options?: Partial<Options>);
 
-  /**
-   * @defaultValue
-   * ```typescript
-   * mergeObject(super.defaultOptions, {
-   *   id: "players",
-   *   template: "templates/user/players.html",
-   *   popOut: false
-   * })
-   * ```
-   */
-  static get defaultOptions(): Application.Options;
+    /**
+     * An internal toggle for whether or not to show offline players or hide them
+     * @defaultValue `false`
+     */
+    protected _showOffline: boolean;
 
-  /** @override */
-  render(force?: boolean, options?: Application.RenderOptions): this;
+    /**
+     * @defaultValue
+     * ```typescript
+     * mergeObject(super.defaultOptions, {
+     *   id: "players",
+     *   template: "templates/user/players.html",
+     *   popOut: false
+     * })
+     * ```
+     */
+    static get defaultOptions(): Application.Options;
 
-  /** @override */
-  getData(options?: Application.RenderOptions): PlayerList.Data | Promise<PlayerList.Data>;
+    /** @override */
+    render(force?: boolean, options?: Application.RenderOptions): this;
 
-  /** @override */
-  activateListeners(html: JQuery): void;
+    /** @override */
+    getData(options?: Application.RenderOptions): PlayerList.Data | Promise<PlayerList.Data>;
 
-  /**
-   * Return the default context options available for the Players application
-   */
-  protected _getUserContextOptions(): ContextMenu.Item[];
+    /** @override */
+    activateListeners(html: JQuery): void;
 
-  /**
-   * Toggle display of the Players hud setting for whether or not to display offline players
-   * @param event - The originating click event
-   */
-  protected _onToggleOfflinePlayers(event: JQuery.ClickEvent): void;
-}
+    /**
+     * Return the default context options available for the Players application
+     */
+    protected _getUserContextOptions(): ContextMenu.Item[];
 
-declare namespace PlayerList {
-  interface Data {
-    users: StoredDocument<InstanceType<ConfiguredDocumentClass<typeof User>>>[];
-    showOffline: boolean;
-    hide: boolean;
+    /**
+     * Toggle display of the Players hud setting for whether or not to display offline players
+     * @param event - The originating click event
+     */
+    protected _onToggleOfflinePlayers(event: JQuery.ClickEvent): void;
+  }
+
+  namespace PlayerList {
+    interface Data {
+      users: StoredDocument<InstanceType<ConfiguredDocumentClass<typeof User>>>[];
+      showOffline: boolean;
+      hide: boolean;
+    }
   }
 }
