43c78f032a71303f9581c4189b6c896cf3f89815
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-07-29 14:39:39 +0200

    Update other shaders

--- src/foundry/foundry.js/pixi/shaders/abstractBaseShaders/standardColorationShaders/chromaColorationShader.d.ts
+++ src/foundry/foundry.js/pixi/shaders/abstractBaseShaders/standardColorationShaders/chromaColorationShader.d.ts
@@ -3,4 +3,16 @@
  */
 declare class ChromaColorationShader extends StandardColorationShader {
   static fragmentShader: string;
+
+  /**
+   * @override
+   * @defaultValue
+   * ```typescript
+   * Object.assign({}, super.defaultUniforms, {
+   *   pulse: 0,
+   *   color: PIXI.utils.hex2rgb(0xCEECEE)
+   * })
+   * ```
+   */
+  static defaultUniforms: AbstractBaseShader.Uniforms;
 }
