6c47bd3ce7c1c357e804ef5d33346bb0851f1635
Kyle Filz <Kfilz@live.ca>
2021-08-02 00:41:37 -0500

    Add basic tests for RollTable.

--- test-d/foundry/foundry.js/collections/documentCollections/worldCollections/rollTables.test-d.ts
+++ test-d/foundry/foundry.js/collections/documentCollections/worldCollections/rollTables.test-d.ts
@@ -1,6 +1,8 @@
 import { expectType } from 'tsd';
+import type { RollTableDataProperties } from '../../../../../../src/foundry/common/data/data.mjs/rollTableData';
+import type { PropertiesToSource } from '../../../../../../src/types/helperTypes';
 
 const rollTables = new RollTables();
 expectType<StoredDocument<RollTable>>(rollTables.get('', { strict: true }));
-expectType<any[]>(rollTables.toJSON()); // TODO Adjust as soon as BaseRollTable and RollTableData have been typed
+expectType<(PropertiesToSource<RollTableDataProperties> & { _id: string })[]>(rollTables.toJSON());
 expectType<RollTableDirectory | undefined>(rollTables.directory);
