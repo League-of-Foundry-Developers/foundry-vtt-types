cab2b3456db286f5e624d1c56a9a4df5ba105b8d
Johannes Loher <johannes.loher@fg4f.de>
2021-08-19 23:00:05 +0200

    Update Actors for 0.8.9

--- test-d/foundry/foundry.js/collections/documentCollections/worldCollections/actors.test-d.ts
+++ test-d/foundry/foundry.js/collections/documentCollections/worldCollections/actors.test-d.ts
@@ -4,3 +4,4 @@ const actors = new Actors();
 expectType<StoredDocument<Actor>>(actors.get('', { strict: true }));
 expectType<StoredDocument<Actor>['data']['_source'][]>(actors.toJSON());
 expectType<ActorDirectory | undefined>(actors.directory);
+expectType<Actor | undefined>(actors.tokens['foo']);
