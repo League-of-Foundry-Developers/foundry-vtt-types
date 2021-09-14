063f8f000ede130d61106c6044592f7ae1a7cf21
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-09-14 17:21:46 +0200

    Add failing test

--- test-d/foundry/foundry.js/collections/documentCollections/compendiumCollection.test-d.ts
+++ test-d/foundry/foundry.js/collections/documentCollections/compendiumCollection.test-d.ts
@@ -12,3 +12,21 @@ const metadata = {
 const compendiumCollection = new CompendiumCollection(metadata);
 expectType<StoredDocument<JournalEntry>>(compendiumCollection.get('', { strict: true }));
 expectType<Array<StoredDocument<foundry.documents.BaseJournalEntry>['data']['_source']>>(compendiumCollection.toJSON());
+
+expectType<{
+  _id: string;
+  name: string;
+  img: string | null | undefined;
+}>((await compendiumCollection.getIndex()).get('some id', { strict: true }));
+
+const itemCollection = new CompendiumCollection({
+  entity: 'Item',
+  label: 'Important items',
+  name: 'items',
+  package: 'other-package',
+  path: 'path/to/items',
+  private: false
+});
+expectType<{ _id: string; name: string; img: string | null; type: 'armor' | 'weapon' }>(
+  (await itemCollection.getIndex()).get('some id', { strict: true })
+);
