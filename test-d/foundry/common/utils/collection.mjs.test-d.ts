61bc5a2024b8a3c6b804172f0ebbc667d844930f
Oskar Pfeifer-Bley <oskar@programming-php.net>
2021-09-15 14:38:40 +0200

    Support type guards within Collection.{filter,find}

--- test-d/foundry/common/utils/collection.mjs.test-d.ts
+++ test-d/foundry/common/utils/collection.mjs.test-d.ts
@@ -10,3 +10,14 @@ expectType<string>(c.get('', { strict: true }));
 expectType<string | undefined>(c.getName(''));
 expectType<string | undefined>(c.getName('', { strict: false }));
 expectType<string>(c.getName('', { strict: true }));
+
+function isString(e: string | null): e is string {
+  return typeof e === 'string';
+}
+
+const cn = new Collection<string | null>();
+expectType<Array<string | null>>(cn.filter((each) => typeof each === 'string'));
+expectType<string | null | undefined>(cn.find((each) => typeof each === 'string'));
+
+expectType<string[]>(cn.filter(isString));
+expectType<string | undefined>(cn.find(isString));
