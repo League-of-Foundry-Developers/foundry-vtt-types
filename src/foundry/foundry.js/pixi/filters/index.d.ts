848ded611a8e3fb46a0ae7341e10df2aa8230dbb
Johannes Loher <johannes.loher@fg4f.de>
2021-09-19 00:11:41 +0200

    Add InverseOcclusionMaskFilter

--- src/foundry/foundry.js/pixi/filters/index.d.ts
+++ src/foundry/foundry.js/pixi/filters/index.d.ts
@@ -1,1 +1,2 @@
 import './abstractBaseMaskFilter';
+import './abstractBaseMaskFilters';
---
+++ test-d/foundry/foundry.js/pixi/filter/abstractBaseMaskFilters/inverseOcclusionMaskFilter.test-d.ts
@@ -0,0 +1,3 @@
+import { expectType } from 'tsd';
+
+expectType<InverseOcclusionMaskFilter>(InverseOcclusionMaskFilter.create());
