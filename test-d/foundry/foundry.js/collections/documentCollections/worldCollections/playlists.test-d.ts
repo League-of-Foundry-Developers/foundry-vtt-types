99efb251b911e0706af7190a608a58d37a087e50
Johannes Loher <johannes.loher@fg4f.de>
2021-10-05 12:21:42 +0200

    Update Playlist for 0.8.9
    Closes #1136

--- test-d/foundry/foundry.js/collections/documentCollections/worldCollections/playlists.test-d.ts
+++ test-d/foundry/foundry.js/collections/documentCollections/worldCollections/playlists.test-d.ts
@@ -1,6 +1,8 @@
 import { expectType } from 'tsd';
+import type { PlaylistDataProperties } from '../../../../../../src/foundry/common/data/data.mjs/playlistData';
+import type { PropertiesToSource } from '../../../../../../src/types/helperTypes';
 
 const playlists = new Playlists();
 expectType<StoredDocument<Playlist>>(playlists.get('', { strict: true }));
-expectType<any[]>(playlists.toJSON()); // TODO Adjust as soon as BasePlaylist and PlaylistData has been typed
+expectType<(PropertiesToSource<PlaylistDataProperties> & { _id: string })[]>(playlists.toJSON());
 expectType<PlaylistDirectory | undefined>(playlists.directory);
