103a284d62ab47b4b6216aebe11dd5fdc4162c09
Johannes Loher <johannes.loher@fg4f.de>
2021-11-07 14:16:57 +0100

    Update DrawingConfig for 0.8.x

--- src/foundry/foundry.js/applications/formApplications/drawingConfig.d.ts
+++ src/foundry/foundry.js/applications/formApplications/drawingConfig.d.ts
@@ -1,112 +1,110 @@
-/**
- * Configuration sheet for the Drawing object
- * @typeParam P - the type of the options object
- */
-declare class DrawingConfig<P extends DrawingConfig.Options = DrawingConfig.Options> extends FormApplication<
-  P,
-  DrawingConfig.Data,
-  Drawing
-> {
+import type { ConfiguredDocumentClassForName } from '../../../../types/helperTypes';
+
+declare global {
   /**
-   * @param drawing - The Drawing object being configured
-   * @param options - Additional application rendering options
+   * The Application responsible for configuring a single Drawing document within a parent Scene.
+   * @typeParam Options - the type of the options object
+   * @typeParam Data    - The data structure used to render the handlebars template.
    */
-  constructor(drawing: Drawing, options?: Partial<P>);
+  class DrawingConfig<
+    Options extends DrawingConfig.Options = DrawingConfig.Options,
+    Data extends object = DrawingConfig.Data<Options>
+  > extends FormApplication<Options, Data, InstanceType<ConfiguredDocumentClassForName<'Drawing'>>> {
+    /**
+     * @defaultValue
+     * ```typescript
+     * foundry.utils.mergeObject(super.defaultOptions, {
+     *   id: "drawing-config",
+     *   classes: ["sheet"],
+     *   template: "templates/scene/drawing-config.html",
+     *   width: 480,
+     *   height: 360,
+     *   configureDefault: false,
+     *   tabs: [{ navSelector: ".tabs", contentSelector: "form", initial: "position" }],
+     * });
+     * ```
+     */
+    static get defaultOptions(): DrawingConfig.Options;
 
-  /**
-   * @defaultValue
-   * ```typescript
-   * mergeObject(super.defaultOptions, {
-   *   id: "drawing-config",
-   *   classes: ["sheet"],
-   *   template: "templates/scene/drawing-config.html",
-   *   width: 480,
-   *   height: 360,
-   *   configureDefault: false,
-   *   tabs: [{navSelector: ".tabs", contentSelector: "form", initial: "position"}]
-   * });
-   * ```
-   */
-  static get defaultOptions(): DrawingConfig.Options;
+    /** @override */
+    get title(): string;
 
-  /** @override */
-  get title(): string;
-
-  /**
-   * @param options - (unused)
-   * @override
-   */
-  getData(options?: Partial<P>): DrawingConfig.Data;
-
-  /**
-   * Get the names and labels of fill type choices which can be applied
-   */
-  protected static _getFillTypes(): DrawingConfig.FillTypes;
-
-  /**
-   * @param event - (unused)
-   * @override
-   */
-  protected _updateObject<F extends DrawingConfig.FormData>(
-    event: Event,
-    formData: F
-  ): Promise<this['options']['configureDefault'] extends true ? F : Drawing>;
-
-  /** @override */
-  close(options?: FormApplication.CloseOptions): Promise<void>;
-
-  /** @override */
-  activateListeners(html: JQuery): void;
-
-  /**
-   * Reset the user Drawing configuration settings to their default values
-   */
-  protected _onResetDefaults(event: JQuery.ClickEvent): void;
-}
-
-declare namespace DrawingConfig {
-  interface Data {
-    author: string;
-    isDefault: Options['configureDefault'];
-    fillTypes: ReturnType<typeof DrawingConfig['_getFillTypes']>;
-    fontFamilies: Record<typeof CONFIG['fontFamilies'][number], typeof CONFIG['fontFamilies'][number]>;
-    object: DrawingConfig['object']['data'];
-    options: DrawingConfig['options'];
-    submitText: string;
-  }
-
-  type FillTypes = {
-    [Key in keyof typeof foundry.CONST['DRAWING_FILL_TYPES'] as typeof foundry.CONST['DRAWING_FILL_TYPES'][Key]]: `DRAWING.FillType${Titlecase<Key>}`;
-  };
-
-  interface FormData {
-    bezierFactor: foundry.data.DrawingData['_source']['bezierFactor'];
-    fillAlpha: foundry.data.DrawingData['_source']['fillAlpha'];
-    fillColor: foundry.data.DrawingData['_source']['fillColor'];
-    fillType: foundry.data.DrawingData['_source']['fillType'];
-    fontFamily: foundry.data.DrawingData['_source']['fontFamily'];
-    fontSize: foundry.data.DrawingData['_source']['fontSize'] | null;
-    height: foundry.data.DrawingData['_source']['height'] | null;
-    rotation: foundry.data.DrawingData['_source']['rotation'] | null;
-    strokeAlpha: foundry.data.DrawingData['_source']['strokeAlpha'];
-    strokeColor: foundry.data.DrawingData['_source']['strokeColor'];
-    strokeWidth: foundry.data.DrawingData['_source']['strokeWidth'] | null;
-    text: string;
-    textAlpha: foundry.data.DrawingData['_source']['textAlpha'];
-    textColor: foundry.data.DrawingData['_source']['textColor'];
-    texture: string;
-    width: foundry.data.DrawingData['_source']['width'] | null;
-    x: foundry.data.DrawingData['_source']['x'] | null;
-    y: foundry.data.DrawingData['_source']['y'] | null;
-    z: foundry.data.DrawingData['_source']['z'] | null;
-  }
-
-  interface Options extends FormApplication.Options {
-    configureDefault: boolean;
+    /** @override */
+    getData(options?: Partial<Options>): Data | Promise<Data>;
 
     /**
-     * Configure a preview version of the Drawing which is not yet saved
+     * Get the names and labels of fill type choices which can be applied
+     * @internal
      */
-    preview?: boolean;
+    protected static _getFillTypes(): DrawingConfig.FillTypes;
+
+    /** @override */
+    protected _updateObject(
+      event: Event,
+      formData: DrawingConfig.FormData
+    ): Promise<foundry.data.DrawingData['_source'] | ConfiguredDocumentClassForName<'Drawing'> | undefined>;
+
+    /** @override */
+    close(options?: FormApplication.CloseOptions): Promise<void>;
+
+    /** @override */
+    activateListeners(html: JQuery): void;
+
+    /**
+     * Reset the user Drawing configuration settings to their default values
+     * @param event - The originating mouse-click event
+     */
+    protected _onResetDefaults(event: JQuery.ClickEvent): void;
+  }
+
+  namespace DrawingConfig {
+    interface Data<Options extends DrawingConfig.Options = DrawingConfig.Options> {
+      author: string;
+      isDefault: boolean;
+      fillTypes: DrawingConfig.FillTypes;
+      fontFamilies: Record<string, string>;
+      object: foundry.data.DrawingData['_source'];
+      options: Options;
+      submitText: string;
+    }
+
+    type FillTypes = {
+      [Key in keyof typeof foundry.CONST['DRAWING_FILL_TYPES'] as typeof foundry.CONST['DRAWING_FILL_TYPES'][Key]]: `DRAWING.FillType${Titlecase<Key>}`;
+    };
+
+    interface FormData {
+      bezierFactor: number;
+      fillAlpha: number;
+      fillColor: string;
+      fillType: foundry.CONST.DrawingFillType;
+      fontFamily: string;
+      fontSize: number | null;
+      height: number | null;
+      rotation: number | null;
+      strokeAlpha: number;
+      strokeColor: string;
+      strokeWidth: number | null;
+      text: string;
+      textAlpha: number;
+      textColor: string;
+      texture: string;
+      width: number | null;
+      x: number | null;
+      y: number | null;
+      z: number | null;
+    }
+
+    interface Options extends FormApplication.Options {
+      /**
+       * Configure the default drawing settings, instead of a specific Drawing
+       * @defaultValue `false`
+       */
+      configureDefault: boolean;
+
+      /**
+       * Configure a preview version of the Drawing which is not yet saved
+       */
+      preview?: boolean;
+    }
   }
 }
