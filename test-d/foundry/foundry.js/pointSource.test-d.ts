e45c131e48e3dce7a1386eabb0aece4e5b113328
Kai Moschcau <mail@kmoschcau.de>
2021-12-31 15:22:21 +0100

    Fix type tests related to PointSources

--- test-d/foundry/foundry.js/pointSource.test-d.ts
+++
@@ -1,6 +0,0 @@
-import { expectType } from 'tsd';
-
-const pointSource = new PointSource(new Token(new TokenDocument()), 'sight');
-expectType<[number, number, number] | undefined>(pointSource.colorRGB);
-const initializedPointSource = pointSource.initialize();
-expectType<[number, number, number]>(initializedPointSource.colorRGB);
