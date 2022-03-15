f611034a87bd62a7cb3e20720d0cf1154b2d4f7d
Johannes Loher <johannes.loher@fg4f.de>
2022-03-15 21:29:20 +0100

    Add http.mjs for v9
    Closes #1283

---
+++ src/foundry/common/utils/http.mjs.d.ts
@@ -0,0 +1,52 @@
+/**
+ * A wrapper method around `fetch` that attaches an AbortController signal to the `fetch` call for clean timeouts
+ * @see https://www.npmjs.com/package/node-fetch#request-cancellation-with-abortsignal
+ * @param url            - The URL to make the Request to
+ * @param data           - The data of the Request
+ *                         (defalt: `{}`)
+ * @param timeoutOptions - (default: `{}`)
+ * @throws {@link HttpError}
+ */
+export function fetchWithTimeout(
+  url: string,
+  data?: RequestInit | undefined,
+  { timeoutMs, onTimeout }?: TimeoutOptions | undefined
+): Promise<Response>;
+
+/**
+ * A small wrapper that automatically asks for JSON
+ * @param url            - The URL to make the Request to
+ * @param data           - The data of the Request
+ *                         (defalt: `{}`)
+ * @param timeoutOptions - (default: `{}`)
+ */
+export function fetchJsonWithTimeout(
+  url: string,
+  data?: RequestInit | undefined,
+  { timeoutMs, onTimeout }?: TimeoutOptions | undefined
+): Promise<unknown>;
+
+/**
+ * Represents an HTTP Error when a non-OK response is returned by Fetch
+ */
+export class HttpError extends Error {
+  constructor(statusText: string, code: number, displayMessage?: string | undefined);
+
+  code: number;
+
+  displayMessage: string;
+}
+
+interface TimeoutOptions {
+  /**
+   * How long to wait for a Response before cleanly aborting
+   * @defaultValue `30000`
+   */
+  timeoutMs?: number | undefined;
+
+  /**
+   * A method to invoke if and when the timeout is reached
+   * @defaultValue `() => {}`
+   */
+  onTimeout?: (() => void) | undefined;
+}
--- src/foundry/common/utils/module.mjs.d.ts
+++ src/foundry/common/utils/module.mjs.d.ts
@@ -1,4 +1,5 @@
 export * from './geometry.mjs';
 export * from './helpers.mjs';
+export * from './http.mjs';
 export { default as Collection } from './collection.mjs';
 export { default as Semaphore } from './semaphore.mjs';
