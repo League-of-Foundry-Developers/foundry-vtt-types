6f035160df54c0d11c787174176f7d19b8369a6e
Johannes Loher <johannes.loher@fg4f.de>
2022-03-20 18:52:03 +0100

    Add ObjectHUD for v9
    Closes #1043

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -136,3 +136,8 @@ type StoredDocument<D extends foundry.abstract.Document<any, any>> = D & {
 type TemporaryDocument<D> = D extends StoredDocument<infer U> ? U : D;
 
 type PropertyTypeOrFallback<T, Key extends string, Fallback> = Key extends keyof T ? T[Key] : Fallback;
+
+/**
+ * Makes the given keys `K` of the type `T` required
+ */
+type RequiredProps<T, K extends keyof T> = Required<Pick<T, K>> & Omit<T, K>;
