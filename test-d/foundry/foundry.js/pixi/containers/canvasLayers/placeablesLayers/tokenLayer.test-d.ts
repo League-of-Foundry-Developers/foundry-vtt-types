04f4e65de31edf18f829a471c82db713bec064c4
Kai Moschcau <mail@kmoschcau.de>
2022-04-09 11:55:14 +0200

    Remove trivial TokenLayer tests and fix docs

--- test-d/foundry/foundry.js/pixi/containers/canvasLayers/placeablesLayers/tokenLayer.test-d.ts
+++ test-d/foundry/foundry.js/pixi/containers/canvasLayers/placeablesLayers/tokenLayer.test-d.ts
@@ -1,52 +1,9 @@
 import { expectType } from 'tsd';
 
-expectType<'Token'>(TokenLayer.documentName);
 expectType<TokenLayer | undefined>(TokenLayer.instance);
-expectType<TokenLayer.LayerOptions>(TokenLayer.layerOptions);
-expectType<'tokens'>(TokenLayer.layerOptions.name);
 expectType<typeof Token>(TokenLayer.layerOptions.objectClass);
 
 const layer = new TokenLayer();
 expectType<typeof Token>(layer.options.objectClass);
 expectType<TokenLayer.LayerOptions>(layer.options);
 expectType<'tokens'>(layer.options.name);
-
-expectType<1>(layer.gridPrecision);
-
-expectType<TokenHUD>(layer.hud);
-
-expectType<Token[]>(layer.ownedTokens);
-
-expectType<Promise<TokenLayer>>(layer.tearDown());
-
-expectType<TokenLayer>(layer.activate());
-
-expectType<TokenLayer>(layer.deactivate());
-
-expectType<boolean>(layer.selectObjects());
-expectType<boolean>(layer.selectObjects({}));
-expectType<boolean>(
-  layer.selectObjects({
-    x: 10,
-    y: 10,
-    width: 100,
-    height: 200,
-    releaseOptions: { trigger: true },
-    controlOptions: { releaseOthers: true }
-  })
-);
-
-expectType<number>(layer.targetObjects({ x: 100, y: 100, width: 800, height: 600 }));
-expectType<number>(layer.targetObjects({ x: 100, y: 100, width: 800, height: 600 }, {}));
-expectType<number>(layer.targetObjects({ x: 100, y: 100, width: 800, height: 600 }, { releaseOthers: true }));
-
-expectType<Token | null>(layer.cycleTokens(true, true));
-
-expectType<Promise<Combatant[]>>(layer.toggleCombat());
-expectType<Promise<Combatant[]>>(layer.toggleCombat(true));
-expectType<Promise<Combatant[]>>(layer.toggleCombat(true, null));
-expectType<Promise<Combatant[]>>(layer.toggleCombat(true, new Combat()));
-expectType<Promise<Combatant[]>>(layer.toggleCombat(true, new Combat(), {}));
-expectType<Promise<Combatant[]>>(layer.toggleCombat(true, new Combat(), { token: new Token(new TokenDocument()) }));
-
-expectType<void>(layer.concludeAnimation());
