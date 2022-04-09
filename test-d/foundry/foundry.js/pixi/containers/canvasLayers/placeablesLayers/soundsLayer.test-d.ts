90af81d3714b3da79f4abdab6a110979ddf1c89a
Kai Moschcau <mail@kmoschcau.de>
2022-04-09 11:34:31 +0200

    Remove trivial tests from SoundsLayer

--- test-d/foundry/foundry.js/pixi/containers/canvasLayers/placeablesLayers/soundsLayer.test-d.ts
+++ test-d/foundry/foundry.js/pixi/containers/canvasLayers/placeablesLayers/soundsLayer.test-d.ts
@@ -1,26 +1,9 @@
 import { expectType } from 'tsd';
 
-expectType<'AmbientSound'>(SoundsLayer.documentName);
 expectType<SoundsLayer | undefined>(SoundsLayer.instance);
-expectType<SoundsLayer.LayerOptions>(SoundsLayer.layerOptions);
-expectType<'sounds'>(SoundsLayer.layerOptions.name);
 expectType<typeof AmbientSound>(SoundsLayer.layerOptions.objectClass);
 
 const layer = new SoundsLayer();
 expectType<typeof AmbientSound>(layer.options.objectClass);
 expectType<SoundsLayer.LayerOptions>(layer.options);
 expectType<'sounds'>(layer.options.name);
-
-expectType<boolean>(layer.livePreview);
-
-expectType<Promise<SoundsLayer>>(layer.tearDown());
-
-expectType<void>(layer.initializeSources());
-
-expectType<number | void>(layer.refresh());
-expectType<number | void>(layer.refresh({}));
-expectType<number | void>(layer.refresh({ fade: 50 }));
-
-expectType<void>(layer.previewSound({ x: 100, y: 0 }));
-
-expectType<void>(layer.stopAll());
