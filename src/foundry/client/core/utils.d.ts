2f5236568da49a491af8d410e73f9f05165452bc
Kyle Filz <Kfilz@live.ca>
2022-07-30 17:47:19 -0500

    Update client/core/utils.d.ts to v10.276

--- src/foundry/client/core/utils.d.ts
+++ src/foundry/client/core/utils.d.ts
@@ -1,3 +1,5 @@
+import { ClientDocumentMixin } from '../data/abstract/client-document';
+
 /**
  * Export data content to be saved to a local file
  * @param data     - Data content converted to a string
@@ -15,9 +17,77 @@ declare function readTextFromFile(file: File): Promise<string>;
 
 /**
  * Retrieve an Entity or Embedded Entity by its Universally Unique Identifier (uuid).
- * @param uuid - The uuid of the Entity or Embedded Entity to retrieve
+ * @param uuid     - The uuid of the Entity or Embedded Entity to retrieve
+ * @param relative - A document to resolve relative UUIDs against.
  */
-declare function fromUuid(uuid: string): Promise<foundry.abstract.Document<any, any> | null>;
+declare function fromUuid(
+  uuid: string,
+  relative?: ClientDocumentMixin<foundry.abstract.Document<any, any>>
+): Promise<foundry.abstract.Document<any, any> | null>;
+
+/**
+ * Retrieve a Document by its Universally Unique Identifier (uuid) synchronously. If the uuid resolves to a compendium
+ * document, that document's index entry will be returned instead.
+ * @param uuid     - The uuid of the Document to retrieve.
+ * @param relative - A document to resolve relative UUIDs against.
+ * @returns The Document or its index entry if it resides in a Compendium, otherwise null.
+ * @throws If the uuid resolves to a Document that cannot be retrieved synchronously.
+ */
+declare function fromUuidSync(
+  uuid: string,
+  relative?: ClientDocumentMixin<foundry.abstract.Document<any, any>>
+): foundry.abstract.Document<any, any> | Record<string, unknown> | null;
+
+export interface ResolvedUUID {
+  /** The parent collection. */
+  collection?: DocumentCollection<any, any>;
+  /** The parent document. */
+  documentId?: string;
+  /** An already-resolved document. */
+  doc?: ClientDocumentMixin<foundry.abstract.Document<any, any>>;
+  /** Any remaining Embedded Document parts. */
+  embedded: string;
+}
+
+/**
+ * Parse a UUID into its constituent parts.
+ * @param uuid     - The UUID to parse.
+ * @param relative - A document to resolve relative UUIDs against.
+ * @returns Returns the Collection and the Document ID to resolve the parent document,
+ * as well as the remaining Embedded Document parts, if any.
+ * @internal
+ */
+declare function _parseUuid(
+  uuid: string,
+  relative?: ClientDocumentMixin<foundry.abstract.Document<any, any>>
+): ResolvedUUID;
+
+/**
+ * Resolve a series of embedded document UUID parts against a parent Document.
+ * @param parent - The parent Document.
+ * @param parts  - A series of Embedded Document UUID parts.
+ * @returns The resolved Embedded Document.
+ * @internal
+ */
+declare function _resolveEmbedded(
+  parent: foundry.abstract.Document<any, any>,
+  parts: string[]
+): foundry.abstract.Document<any, any>;
+
+/**
+ * Resolve a UUID relative to another document.
+ * The general-purpose algorithm for resolving relative UUIDs is as follows:
+ * 1. If the number of parts is odd, remove the first part and resolve it against the current document and update the
+ *    current document.
+ * 2. If the number of parts is even, resolve embedded documents against the current document.
+ * @param uuid     - The UUID to resolve.
+ * @param relative - The document to resolve against.
+ * @internal
+ */
+declare function _resolveRelativeUuid(
+  uuid: string,
+  relative: ClientDocumentMixin<foundry.abstract.Document<any, any>>
+): ResolvedUUID;
 
 /**
  * Return a reference to the Document class implementation which is configured for use.
