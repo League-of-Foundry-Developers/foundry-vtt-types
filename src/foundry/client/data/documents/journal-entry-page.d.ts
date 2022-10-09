adc6e816407536754f056f697c848ad243dbe307
rsek <hello@rsek.ca>
2022-10-09 00:14:06 -0700

    update methods + getters

--- src/foundry/client/data/documents/journal-entry-page.d.ts
+++ src/foundry/client/data/documents/journal-entry-page.d.ts
@@ -1,4 +1,4 @@
-import './utils/primitives.mjs';
+import "./utils/primitives.mjs";
 
 declare global {
   /**
@@ -10,10 +10,81 @@ declare global {
    */
   class JournalEntryPage extends ClientDocumentMixin(foundry.documents.BaseJournalEntryPage) {
     /**
+     * The table of contents for this JournalEntryPage.
+     */
+    get toc(): JournalEntryPage.Heading;
+    /**
+     * Return a reference to the Note instance for this Journal Entry Page in the current Scene, if any.
+     * If multiple notes are placed for this Journal Entry, only the first will be returned.
+     */
+    get sceneNote(): Note | null;
+
+    /**
      * Convert a heading into slug suitable for use as an identifier.
      * @param heading - The heading element or some text content.
      */
-    static slugifyHeading(heading: HTMLHeadingElement | string): ReturnType<String['slugify']>;
+    static slugifyHeading(heading: HTMLHeadingElement | string): string;
+
+    /**
+     * Build a table of contents for the given HTML content.
+     * @param html - The HTML content to generate a ToC outline for.
+     * @param options - Additional options to configure ToC generation.
+     */
+    static buildTOC(
+      html: HTMLElement[],
+      {
+        includeElement
+      }?:
+        | {
+            includeElement?: boolean | undefined;
+          }
+        | undefined
+    ): JournalEntryPage.Heading;
+    /**
+     * Flatten the tree structure into a single object with each node's slug as the key.
+     * @param nodes - The root ToC nodes.
+     */
+    static _flattenTOC(nodes: JournalEntryPage.Heading[]): JournalEntryPage.Heading;
+    /**
+     * Construct a table of contents node from a heading element.
+     * @param heading - The heading element.
+     * @param options - Additional options to configure the returned node.
+     */
+    static _makeHeadingNode(
+      heading: HTMLHeadingElement,
+      {
+        includeElement
+      }?:
+        | {
+            includeElement?: boolean | undefined;
+          }
+        | undefined
+    ): JournalEntryPage.Heading;
+  }
+  namespace JournalEntryPage {
+    interface Heading {
+      /**
+       * The heading level, 1-6.
+       */
+      level: number;
+      /**
+       * The heading level, 1-6.
+       */
+      text: string;
+      /**
+       * The generated slug for this heading.
+       */
+      slug: string;
+      /**
+       * The currently rendered element for this heading, if it exists.
+       */
+      element?: HTMLHeadingElement | undefined;
+      /**
+       * Any child headings of this one.
+       */
+      children: string[];
+    }
   }
 }
+
 export {};
