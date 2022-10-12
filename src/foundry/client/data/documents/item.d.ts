5f9a85324fb02685aeb7d123f56aeb67231df836
rsek <hello@rsek.ca>
2022-10-12 00:30:02 -0700

    update Item

--- src/foundry/client/data/documents/item.d.ts
+++ src/foundry/client/data/documents/item.d.ts
@@ -2,26 +2,16 @@ import { ConfiguredDocumentClass } from "../../../../types/helperTypes";
 
 declare global {
   /**
-   * The client-side Item document which extends the common BaseItem abstraction.
-   * Each Item document contains ItemData which defines its data schema.
+   * The client-side Item document which extends the common BaseItem model.
    *
-   * @see {@link data.ItemData}              The Item data schema
-   * @see {@link documents.Items}            The world-level collection of Item documents
-   * @see {@link applications.ItemSheet}     The Item configuration application
-   *
-   * @param data    - Initial data provided to construct the Item document
-   * @param context - The document context, see {@link foundry.abstract.Document}
+   * @see {@link documents.Items}        - The world-level collection of Item documents
+   * @see {@link applications.ItemSheet} - The Item configuration application
    */
   class Item extends ClientDocumentMixin(foundry.documents.BaseItem) {
     /**
      * A convenience alias of Item#parent which is more semantically intuitive
      */
     get actor(): this["parent"];
-
-    /**
-     * A convenience reference to the image path (data.img) used to represent this Item
-     */
-    get img(): this["data"]["img"];
 
     /**
      * Provide a thumbnail image path used to represent this document.
@@ -44,7 +34,7 @@ declare global {
      */
     getRollData(): object;
 
-    // @ts-expect-error For some reason, proctected static methods from Document are lost, so ts complains that this isn't actually an override
+    // @ts-expect-error For some reason, protected static methods from Document are lost, so ts complains that this isn't actually an override
     protected static override _onCreateDocuments(
       items: Array<InstanceType<ConfiguredDocumentClass<typeof Item>>>,
       context: DocumentModificationContext
