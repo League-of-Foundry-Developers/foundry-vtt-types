5949c2c687c94f0ed8e99555269fe56a8a282c63
rsek <hello@rsek.ca>
2022-10-12 18:00:42 -0700

    missing BaseJournalEntryPage members

--- src/foundry/common/documents.mjs/baseJournalEntryPage.d.ts
+++ src/foundry/common/documents.mjs/baseJournalEntryPage.d.ts
@@ -3,6 +3,8 @@ import { Document } from "../abstract/module.mjs";
 import { DocumentMetadata } from "../abstract/document.mjs";
 import type { BaseJournalEntry } from "./baseJournalEntry.js";
 import type { ConfiguredDocumentClass } from "../../../types/helperTypes.js";
+import type { BaseUser } from "./baseUser.js";
+import type { CONST } from "../module.mjs.js";
 
 type JournalEntryPageMetadata = Merge<
   DocumentMetadata,
@@ -28,4 +30,15 @@ export declare class BaseJournalEntryPage extends Document<
   static override get schema(): typeof data.JournalEntryPageData;
 
   static override get metadata(): JournalEntryPageMetadata;
+
+  // FIXME: this should be a journal entry page schema, but that's outside the scope of this PR.
+  static defineSchema(): DocumentSchema;
+
+  /**
+   * The allowed set of JournalEntryPageData types which may exist.
+   */
+  // FIXME: this should be typed as Game['documentTypes']['JournalEntryPage'], but this doesn't exist yet.
+  static get TYPES(): string[];
+
+  getUserLevel(user: BaseUser): ValueOf<typeof CONST.DOCUMENT_OWNERSHIP_LEVELS> | null;
 }
