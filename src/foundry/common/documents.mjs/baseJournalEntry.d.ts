18b532a2d3b00aa34a0fff4c5ce0d059c5cae625
rsek <hello@rsek.ca>
2022-10-15 22:24:27 -0700

    adding placeholders for BaseJournalEntry methods

--- src/foundry/common/documents.mjs/baseJournalEntry.d.ts
+++ src/foundry/common/documents.mjs/baseJournalEntry.d.ts
@@ -23,4 +23,24 @@ export declare class BaseJournalEntry extends Document<data.JournalEntryData, nu
   static override get schema(): typeof data.JournalEntryData;
 
   static override get metadata(): JournalEntryMetadata;
+
+  // FIXME when DataModel is updated for v10
+  // @ts-expect-error inherit from DataModel
+  static migrateData(data);
+
+  // FIXME when DataModel is updated for v10
+  // @ts-expect-error inherit from DataModel
+  static shimData(data, options);
+
+  // FIXME when DataModel is updated for v10
+  // @ts-expect-error inherit from DataModel
+  _initializeSource(source, options);
+
+  /**
+   * Migrate old content and img field to individual pages.
+   * @param source - Old source data which will be mutated in-place
+   * @returns Page data that should be added to the document
+   * @deprecated since v10
+   */
+  static migrateContentToPages(source: object): data.JournalEntryPageData[];
 }
