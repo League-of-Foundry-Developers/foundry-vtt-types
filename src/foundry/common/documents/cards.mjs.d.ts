44a15965f34c34d1431787d5f5bf1e8a3856eb67
Kai Moschcau <mail@kmoschcau.de>
2023-03-19 17:05:35 +0100

    Add BaseCards
    Closes  #2143

---
+++ src/foundry/common/documents/cards.mjs.d.ts
@@ -0,0 +1,192 @@
+import type Document from "../abstract/document.mjs";
+import type { DocumentMetadata } from "../abstract/document.mjs";
+import type * as fields from "../data/fields.mjs";
+import type * as documents from "./module.mjs";
+
+declare global {
+  type CardsData<TypeName extends BaseCards.TypeNames = BaseCards.TypeNames> = BaseCards.Properties<TypeName>;
+}
+
+/**
+ * The Document definition for Cards.
+ * Defines the DataSchema and common behaviors for Cards which are shared between both client and server.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface BaseCards<TypeName extends BaseCards.TypeNames = BaseCards.TypeNames>
+  extends BaseCards.Properties<TypeName> {}
+declare class BaseCards<TypeName extends BaseCards.TypeNames = BaseCards.TypeNames> extends Document<
+  BaseCards.SchemaField<TypeName>,
+  BaseCards.Metadata
+> {
+  /**
+   * @param data    - Initial data from which to construct the Cards
+   * @param context - Construction context options
+   */
+  constructor(data: BaseCards.ConstructorData<TypeName>, context?: DocumentConstructionContext);
+
+  static override metadata: Readonly<BaseCards.Metadata>;
+
+  static override defineSchema(): BaseCards.Schema;
+
+  /**
+   * The default icon used for a cards stack that does not have a custom image set
+   * @defaultValue `"icons/svg/card-hand.svg"`
+   */
+  static DEFAULT_ICON: string;
+
+  /**
+   * The allowed set of CardsData types which may exist
+   */
+  static get TYPES(): BaseCards.TypeNames[];
+
+  static override migrateData(source: object): object;
+
+  static override shimData(
+    data: object,
+    {
+      embedded
+    }?: {
+      /**
+       * Apply shims to embedded models?
+       * @defaultValue `true`
+       */
+      embedded?: boolean;
+    }
+  ): object;
+}
+export default BaseCards;
+
+declare namespace BaseCards {
+  type TypeNames = fields.SystemDataField.TypeNames<typeof BaseCards>;
+
+  type Metadata = Merge<
+    DocumentMetadata,
+    {
+      name: "Cards";
+      collection: "cards";
+      indexed: true;
+      compendiumIndexFields: ["_id", "name", "img", "type", "sort"];
+      embedded: { Card: "cards" };
+      label: "DOCUMENT.Cards";
+      labelPlural: "DOCUMENT.CardsPlural";
+      coreTypes: ["deck", "hand", "pile"];
+    }
+  >;
+
+  type SchemaField<TypeName extends TypeNames> = fields.SchemaField<Schema<TypeName>>;
+  type ConstructorData<TypeName extends TypeNames> = UpdateData<TypeName> &
+    Required<Pick<UpdateData<TypeName>, "name">>;
+  type UpdateData<TypeName extends TypeNames> = fields.SchemaField.InnerAssignmentType<Schema<TypeName>>;
+  type Properties<TypeName extends TypeNames> = fields.SchemaField.InnerInitializedType<Schema<TypeName>>;
+  type Source<TypeName extends TypeNames> = fields.SchemaField.InnerPersistedType<Schema<TypeName>>;
+
+  interface Schema<TypeName extends TypeNames = TypeNames> extends DataSchema {
+    /**
+     * The _id which uniquely identifies this stack of Cards document
+     * @defaultValue `null`
+     */
+    _id: fields.DocumentIdField;
+
+    /** The text name of this stack */
+    name: fields.StringField<{ required: true; blank: false; label: "CARDS.Name" }>;
+
+    /**
+     * The type of this stack, in BaseCards.metadata.types
+     * @defaultValue `BaseCards.TYPES[0]`
+     */
+    type: fields.StringField<
+      {
+        required: true;
+        label: "CARDS.Type";
+        choices: () => typeof BaseCards.TYPES;
+        initial: () => typeof BaseCards.TYPES[0];
+        validationError: "The Cards type must be in the array of types supported by the game system";
+      },
+      TypeName,
+      TypeName,
+      TypeName
+    >;
+
+    /**
+     * A text description of this stack
+     * @defaultValue `""`
+     */
+    description: fields.HTMLField<{ label: "CARDS.Description" }>;
+
+    /**
+     * An image or video which is used to represent the stack of cards
+     * @defaultValue `BaseCards.DEFAULT_ICON`
+     */
+    img: fields.FilePathField<{
+      categories: ["IMAGE", "VIDEO"];
+      initial: () => typeof BaseCards.DEFAULT_ICON;
+      label: "CARDS.Image";
+    }>;
+
+    /**
+     * Game system data which is defined by the system template.json model
+     * @defaultValue `{}`
+     */
+    system: fields.SystemDataField<typeof BaseCards, TypeName>;
+
+    /**
+     * A collection of Card documents which currently belong to this stack
+     * @defaultValue `[]`
+     */
+    cards: fields.EmbeddedCollectionField<typeof documents.BaseCard>;
+
+    /**
+     * The visible width of this stack
+     * @defaultValue `null`
+     */
+    width: fields.NumberField<{ integer: true; positive: true; label: "Width" }>;
+
+    /**
+     * The visible height of this stack
+     * @defaultValue `null`
+     */
+    height: fields.NumberField<{ integer: true; positive: true; label: "Height" }>;
+
+    /**
+     * The angle of rotation of this stack
+     * @defaultValue `0`
+     */
+    rotation: fields.AngleField<{ label: "Rotation" }>;
+
+    /**
+     * Whether or not to publicly display the number of cards in this stack
+     * @defaultValue `false`
+     */
+    displayCount: fields.BooleanField;
+
+    /**
+     * The _id of a Folder which contains this document
+     * @defaultValue `null`
+     */
+    folder: fields.ForeignDocumentField<typeof documents.BaseFolder>;
+
+    /**
+     * The sort order of this stack relative to others in its parent collection
+     * @defaultValue `0`
+     */
+    sort: fields.IntegerSortField;
+
+    /**
+     * An object which configures ownership of this Cards
+     * @defaultValue see {@link fields.DocumentOwnershipField}
+     */
+    ownership: fields.DocumentOwnershipField;
+
+    /**
+     * An object of optional key/value flags
+     * @defaultValue `{}`
+     */
+    flags: fields.ObjectField.FlagsField<"Cards">;
+
+    /**
+     * An object of creation and access information
+     * @defaultValue see {@link fields.DocumentStatsField}
+     */
+    _stats: fields.DocumentStatsField;
+  }
+}
---
+++ test-d/foundry/common/documents/cards.mjs.test-d.ts
@@ -0,0 +1,137 @@
+import { expectError, expectType } from "tsd";
+import type { BaseCards } from "../../../../src/foundry/common/documents/module.mjs";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface GermanDeckModel
+  extends foundry.data.fields.SchemaField.InnerInitializedType<ReturnType<typeof GermanDeckModel["defineSchema"]>> {}
+class GermanDeckModel extends foundry.abstract.DataModel<
+  foundry.data.fields.SchemaField<ReturnType<typeof GermanDeckModel["defineSchema"]>>
+> {
+  static override defineSchema() {
+    return {
+      /** The game these cards are most often used in */
+      mostUsedGame: new foundry.data.fields.StringField()
+    };
+  }
+
+  /** The kind of players these cards are most often used by */
+  get mostUsedBy(): string {
+    return "older players";
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface FrenchDeckModel
+  extends foundry.data.fields.SchemaField.InnerInitializedType<ReturnType<typeof FrenchDeckModel["defineSchema"]>> {}
+class FrenchDeckModel extends foundry.abstract.DataModel<
+  foundry.data.fields.SchemaField<ReturnType<typeof FrenchDeckModel["defineSchema"]>>
+> {
+  static override defineSchema() {
+    return {
+      /** A cool use for this kind of cards */
+      coolUse: new foundry.data.fields.StringField()
+    };
+  }
+
+  /** Possible injuries sustained when using these cards */
+  get possibleInjuries(): string {
+    return "card stuck in eye";
+  }
+}
+
+declare global {
+  interface SystemConfig {
+    Cards: {
+      german: GermanDeckModel;
+      french: FrenchDeckModel;
+    };
+  }
+}
+
+expectType<foundry.documents.BaseCards<"german">>(
+  new foundry.documents.BaseCards({ name: "Schellen 4", type: "german" })
+);
+expectType<foundry.documents.BaseCards<"french">>(
+  new foundry.documents.BaseCards({ name: "King of Hearts", type: "french" })
+);
+expectError(new foundry.documents.BaseCards({ name: "Kittyfluff", type: "foo" }));
+
+const oldCardModel = new GermanDeckModel();
+const unoCardModel = new FrenchDeckModel();
+
+expectType<foundry.documents.BaseCards<"german">>(
+  new foundry.documents.BaseCards({ name: "Schellen 4", type: "german", system: { mostUsedGame: "Skat" } })
+);
+expectType<foundry.documents.BaseCards<"german">>(
+  new foundry.documents.BaseCards({ name: "Schellen 4", type: "german", system: oldCardModel })
+);
+expectType<foundry.documents.BaseCards<"french">>(
+  new foundry.documents.BaseCards({ name: "King of Hearts", type: "french", system: { coolUse: "throwing cards" } })
+);
+expectType<foundry.documents.BaseCards<"french">>(
+  new foundry.documents.BaseCards({ name: "King of Hearts", type: "french", system: unoCardModel })
+);
+expectError(
+  new foundry.documents.BaseCards({ name: "King of Hearts", type: "french", system: { mostUsedBy: "older players" } })
+);
+expectError(new foundry.documents.BaseCards({ name: "King of Hearts", type: "french", system: { foo: "bar" } }));
+
+const german = new foundry.documents.BaseCards({ name: "Schellen 4", type: "german" });
+const french = new foundry.documents.BaseCards({ name: "King of Hearts", type: "french" });
+
+expectType<Promise<foundry.documents.BaseCards<"german"> | undefined>>(
+  german.update({ system: { mostUsedGame: "Skat" } })
+);
+expectType<Promise<foundry.documents.BaseCards<"german"> | undefined>>(german.update({ system: oldCardModel }));
+expectType<Promise<foundry.documents.BaseCards<"french"> | undefined>>(
+  french.update({ system: { coolUse: "throwing cards" } })
+);
+expectType<Promise<foundry.documents.BaseCards<"french"> | undefined>>(french.update({ system: unoCardModel }));
+expectError(french.update({ system: { foo: "bar" } }));
+
+expectType<object>(german.updateSource({ system: { mostUsedGame: "Skat" } }));
+expectType<object>(german.updateSource({ system: oldCardModel }));
+expectType<object>(french.updateSource({ system: { coolUse: "throwing cards" } }));
+expectType<object>(french.updateSource({ system: unoCardModel }));
+expectError(french.updateSource({ system: { foo: "bar" } }));
+
+expectType<"german">(german.type);
+expectType<GermanDeckModel>(german.system);
+expectType<string | undefined>(german._source.system.mostUsedGame);
+expectType<string | undefined>(german.system._source.mostUsedGame);
+expectError(german._source.system.mostUsedBy);
+expectType<string | undefined>(german.system.mostUsedGame);
+expectType<string | undefined>(german.system.mostUsedBy);
+
+expectType<"french">(french.type);
+expectType<FrenchDeckModel>(french.system);
+expectType<string | undefined>(french._source.system.coolUse);
+expectType<string | undefined>(french.system._source.coolUse);
+expectError(french._source.system.possibleInjuries);
+expectType<string | undefined>(french.system.coolUse);
+expectType<string | undefined>(french.system.possibleInjuries);
+
+declare const unknownActor: foundry.documents.BaseCards;
+expectType<"german" | "french">(unknownActor.type);
+expectType<GermanDeckModel | FrenchDeckModel>(unknownActor.system);
+
+// Flags for Actor, Item, Card, and Cards documents can be configured via the FlagConfig. This is tested here.
+declare global {
+  interface FlagConfig {
+    Cards: {
+      "my-system": {
+        vintage: boolean;
+      };
+    };
+  }
+}
+expectType<{ vintage: boolean }>(german.flags["my-system"]);
+
+expectType<boolean>(german.getFlag("my-system", "vintage"));
+expectType<never>(german.getFlag("my-system", "unknown-key"));
+expectType<unknown>(german.getFlag("another-system", "value"));
+
+expectType<Promise<BaseCards>>(french.setFlag("my-system", "vintage", true));
+expectError(french.setFlag("my-system", "vintage", 300));
+expectError(french.setFlag("my-system", "unknown-key", 2));
+expectType<Promise<BaseCards>>(french.setFlag("another-system", "value", true));
