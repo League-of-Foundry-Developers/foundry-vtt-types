88ca850311d13c65f039254bd18ce4abcd2ab330
Kai Moschcau <mail@kmoschcau.de>
2023-06-09 09:40:47 +0200

    Update BaseFogExploration to v10.291
    Closes #2148

--- test-d/foundry/common/data/data.mjs/folderData.test-d.ts
+++
@@ -1,8 +0,0 @@
-import { expectError, expectType } from "tsd";
-
-expectError(new foundry.data.FolderData());
-expectError(new foundry.data.FolderData({}));
-expectError(new foundry.data.FolderData({ name: "foo", type: "foo" }));
-expectType<foundry.data.FolderData>(
-  new foundry.data.FolderData({ name: "foo", type: foundry.CONST.FOLDER_DOCUMENT_TYPES[0] })
-);
