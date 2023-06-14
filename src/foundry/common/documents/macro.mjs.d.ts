5924f78596d424fa16515244f3d8e3708fdf4998
Kevin Garner <kevin.garner@ssigroup.com>
2023-06-14 12:04:13 -0500

    Added required fields typing to ConstructorData for BaseMacro.

--- src/foundry/common/documents/macro.mjs.d.ts
+++ src/foundry/common/documents/macro.mjs.d.ts
@@ -84,7 +84,7 @@ declare namespace BaseMacro {
   >;
 
   type SchemaField = fields.SchemaField<Schema>;
-  type ConstructorData = UpdateData;
+  type ConstructorData = UpdateData & Required<Pick<UpdateData, "name" | "type" | "scope" | "command">>;
   type UpdateData = fields.SchemaField.InnerAssignmentType<Schema>;
   type Properties = fields.SchemaField.InnerInitializedType<Schema>;
   type Source = fields.SchemaField.InnerPersistedType<Schema>;
