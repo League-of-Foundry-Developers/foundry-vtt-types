c9ed1a69eae0574c3790cf66cf1a5a7a6e352f64
Kevin Garner <kevin.garner@ssigroup.com>
2023-06-20 14:21:29 -0500

    Updated Setting for v10.291.

--- src/foundry/client/data/documents/setting.d.ts
+++ src/foundry/client/data/documents/setting.d.ts
@@ -1,35 +1,25 @@
+// FOUNDRY_VERSION: 10.291
+
 import { DocumentModificationOptions } from "../../../common/abstract/document.mjs";
+import type BaseSetting from "../../../common/documents/setting.mjs";
+
 declare global {
   /**
    * The client-side Setting document which extends the common BaseSetting model.
-   * Each Setting document contains SettingData which defines its data schema.
-
-   * @see {@link data.SettingData}              The Setting data schema
-   * @see {@link documents.WorldSettings}       The world-level collection of Setting documents
+   * @see {@link SettingData}         The Setting data schema
+   * @see {@link WorldSettings}       The world-level collection of Setting documents
    */
-  class Setting extends ClientDocumentMixin(foundry.documents.BaseSetting) {
-    /**
-     * A convenient alias to the key attribute of the setting data
-     */
-    get key(): string;
-
-    /**
-     * A convenient alias to the parsed value attribute of the setting data.
-     */
-    get value(): unknown;
-
+  class Setting extends ClientDocumentMixin(BaseSetting) {
     protected override _onCreate(
-      data: foundry.data.SettingData["_source"],
+      data: SettingData["_source"],
       options: DocumentModificationOptions,
       userId: string
     ): void;
 
-    protected _onUpdate(
-      changed: DeepPartial<foundry.data.SettingData["_source"]>,
+    protected override _onUpdate(
+      changed: DeepPartial<SettingData["_source"]>,
       options: DocumentModificationOptions,
       userId: string
     ): void;
   }
 }
-
-export {};
