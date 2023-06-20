96769230b9624d6cf83c7e3d268e6f7316877128
Kevin Garner <kevin.garner@ssigroup.com>
2023-06-20 15:01:36 -0500

    Updated RollTable for v10.291.

--- src/foundry/client/data/documents/table.d.ts
+++ src/foundry/client/data/documents/table.d.ts
@@ -1,56 +1,56 @@
+// FOUNDRY_VERSION: 10.291
+
 import { ConfiguredDocumentClass } from "../../../../types/helperTypes";
 import { DocumentModificationOptions } from "../../../common/abstract/document.mjs";
+import type BaseChatMessage from "../../../common/documents/chat-message.mjs";
+import type BaseRollTable from "../../../common/documents/roll-table.mjs";
+import type BaseTableResult from "../../../common/documents/table-result.mjs";
 
 declare global {
   /**
    * The client-side RollTable document which extends the common BaseRollTable model.
-   * Each RollTable document contains RollTableData which defines its data schema.
    *
-   * @see {@link data.RollTableData}              The RollTable data schema
-   * @see {@link documents.RollTables}            The world-level collection of RollTable documents
-   * @see {@link applications.RollTableConfig}    The RollTable configuration application
+   * @see {@link RollTables}         The world-level collection of RollTable documents
+   * @see {@link TableResult}        The embedded TableResult document
+   * @see {@link RollTableConfig}    The RollTable configuration application
    */
-  class RollTable extends ClientDocumentMixin(foundry.documents.BaseRollTable) {
-    /**
-     * @param data    - Initial data provided to construct the RollTable document
-     * @param context - The document context, see {@link foundry.abstract.Document}
-     */
-    constructor(
-      data: ConstructorParameters<typeof foundry.documents.BaseRollTable>[0],
-      context?: ConstructorParameters<typeof foundry.documents.BaseRollTable>[1]
-    );
-
+  class RollTable extends ClientDocumentMixin(BaseRollTable) {
     /**
      * Provide a thumbnail image path used to represent this document.
      */
-    get thumbnail(): this["data"]["img"];
+    get thumbnail(): string;
 
     /**
      * Display a result drawn from a RollTable in the Chat Log along.
      * Optionally also display the Roll which produced the result and configure aspects of the displayed messages.
      *
-     * @param results - An Array of one or more TableResult Documents which were drawn and should be displayed
-     * @param options - Additional options which modify message creation
+     * @param results   - An Array of one or more TableResult Documents which were drawn and should
+     *                                        be displayed.
+     * @param options   - Additional options which modify message creation
+     *                  (default: `{}`)
      */
     toMessage(
-      results: InstanceType<ConfiguredDocumentClass<typeof foundry.documents.BaseTableResult>>[],
+      results: InstanceType<ConfiguredDocumentClass<typeof BaseTableResult>>[],
       options?: Partial<RollTable.ToMessageOptions>
-    ): Promise<InstanceType<ConfiguredDocumentClass<typeof foundry.documents.BaseChatMessage>> | undefined>;
+    ): Promise<InstanceType<ConfiguredDocumentClass<typeof BaseChatMessage>> | undefined>;
 
     /**
      * Draw a result from the RollTable based on the table formula or a provided Roll instance
-     * @param options - Optional arguments which customize the draw behavior
-     * @returns A Promise which resolves to an object containing the executed roll and the produced results
+     * @param options   - Optional arguments which customize the draw behavior
+     *                  (default: `{}`)
+     * @returns A Promise which resolves to an object containing the executed roll and the
+     *          produced results.
      */
     draw(options?: RollTable.DrawOptions): Promise<RollTableDraw>;
 
     /**
      * Draw multiple results from a RollTable, constructing a final synthetic Roll as a dice pool of inner rolls.
-     * @param number  - The number of results to draw
-     * @param options - Optional arguments which customize the draw
+     * @param number    - The number of results to draw
+     * @param options   - Optional arguments which customize the draw
+     *                  (default: `{}`)
      * @returns The drawn results
      */
-    drawMany(number: number, options?: Partial<RollTable.DrawOptions>): Promise<RollTableDraw>;
+    drawMany(number: number, options?: RollTable.DrawOptions): Promise<RollTableDraw>;
 
     /**
      * Normalize the probabilities of rolling each item in the RollTable based on their assigned weights
@@ -59,10 +59,8 @@ declare global {
 
     /**
      * Reset the state of the RollTable to return any drawn items to the table
-     * @remarks Actually, returns list of TableEntries updated, not the RollTable.
-     * As written, it force updates all records, not just the ones already drawn.
      */
-    reset(): Promise<InstanceType<ConfiguredDocumentClass<typeof foundry.documents.BaseTableResult>>[]>;
+    resetResults(): Promise<InstanceType<ConfiguredDocumentClass<typeof BaseTableResult>>[]>;
 
     /**
      * Evaluate a RollTable by rolling its formula and retrieving a drawn result.
@@ -70,29 +68,32 @@ declare global {
      * Note that this function only performs the roll and identifies the result, the RollTable#draw function should be
      * called to formalize the draw from the table.
      *
-     * @param options - (default: `{}`)
+     * @param options   - Options which modify rolling behavior
+     *                  (default: `{}`)
      * @returns The Roll and results drawn by that Roll
      *
-     * @example
-     * ```typescript
-     * // Draw results using the default table formula
+     * @example Draw results using the default table formula
+     * ```ts
      * const defaultResults = await table.roll();
+     * ```
      *
-     * // Draw results using a custom roll formula
-     * const roll = new Roll("1d20 + @abilities.wis.mod", actor.getRollData());
+     * @example Draw results using a custom roll formula
+     * ```ts
+     * const roll = new Roll("1d20 + \@abilities.wis.mod", actor.getRollData());
      * const customResults = await table.roll({roll});
      * ```
      */
-    roll(options?: RollTable.RollOptions | undefined): Promise<RollTableDraw>;
+    roll(options?: RollTable.RollOptions): Promise<RollTableDraw>;
 
     /**
      * Get an Array of valid results for a given rolled total
-     * @param value - The rolled value
-     * @returns An Array of results
+     * @param {number} value    The rolled value
+     * @returns {TableResult[]} An Array of results
      */
     getResultsForRoll(value: number): InstanceType<ConfiguredDocumentClass<typeof TableResult>>[];
 
-    protected override _onCreateEmbeddedDocuments(
+    /** @internal */
+    protected _onCreateEmbeddedDocuments(
       embeddedName: string,
       documents: foundry.abstract.Document<any, any>[],
       result: Record<string, unknown>[],
@@ -100,7 +101,8 @@ declare global {
       userId: string
     ): void;
 
-    override _onDeleteEmbeddedDocuments(
+    /** @internal */
+    protected override _onDeleteEmbeddedDocuments(
       embeddedName: string,
       documents: InstanceType<ConfiguredDocumentClass<typeof TableResult>>[],
       result: string[],
@@ -108,17 +110,18 @@ declare global {
       userId: string
     ): void;
 
-    override toCompendium(
+    /** @override */
+    toCompendium(
       pack?: CompendiumCollection<CompendiumCollection.Metadata> | null | undefined,
       options?: ClientDocumentMixin.CompendiumExportOptions | undefined
-    ): Omit<foundry.data.RollTableData["_source"], "_id" | "folder" | "permission"> & {
-      permission?: foundry.data.RollTableData extends { toObject(): infer U } ? U : never;
+    ): Omit<RollTableData["_source"], "_id" | "folder" | "permission"> & {
+      permission?: RollTableData extends { toObject(): infer U } ? U : never;
     };
 
     /**
      * Create a new RollTable document using all of the Documents from a specific Folder as new results.
-     * @param folder  - The Folder document from which to create a roll table
-     * @param options - Additional options passed to the RollTable.create method
+     * @param folder    - The Folder document from which to create a roll table
+     * @param options   - Additional options passed to the RollTable.create method
      */
     static fromFolder(
       folder: InstanceType<ConfiguredDocumentClass<typeof foundry.documents.BaseFolder>>,
@@ -146,7 +149,7 @@ declare global {
        * One or more table results which have been drawn
        * @defaultValue `[]`
        */
-      results: foundry.data.TableResultData[];
+      results: TableResultData[];
 
       /**
        * Whether to automatically display the results in chat
@@ -214,7 +217,6 @@ declare global {
     /**
      * An array of drawn TableResult documents
      */
-    results: InstanceType<ConfiguredDocumentClass<typeof foundry.documents.BaseTableResult>>[];
+    results: InstanceType<ConfiguredDocumentClass<typeof BaseTableResult>>[];
   }
 }
-export {};
