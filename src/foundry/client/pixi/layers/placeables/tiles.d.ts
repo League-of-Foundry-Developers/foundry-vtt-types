69d55b4239de0d232d8040658f825060c4c8bd63
JPMeehan <jpmeehan5@gmail.com>
2023-12-28 14:28:22 -0800

    Added v11 TilesLayer

--- src/foundry/client/pixi/layers/placeables/tiles.d.ts
+++ src/foundry/client/pixi/layers/placeables/tiles.d.ts
@@ -1,8 +1,97 @@
+import type { DropData } from "../../../data/abstract/client-document";
+
 export {};
 
 declare global {
   /**
    * A PlaceablesLayer designed for rendering the visual Scene for a specific vertical cross-section.
    */
-  class TilesLayer extends PlaceablesLayer<"Tile"> {}
+  class TilesLayer extends PlaceablesLayer<"Tile"> {
+    /**
+     * @remarks This is not overridden in foundry but reflects the real behavior.
+     */
+    static get instance(): Canvas["tiles"];
+
+    static override documentName: "Tile";
+
+    override options: PlaceablesLayer.LayerOptions<any>;
+
+    override get hookName(): string;
+
+    override get hud(): Canvas["hud"]["tile"];
+
+    /**
+     * An array of Tile objects which are rendered within the objects container
+     */
+    get tiles(): Tile[];
+
+    /**
+     * Get an array of overhead Tile objects which are roofs
+     */
+    get roofs(): Tile[];
+
+    /**
+     * Determine whether to display roofs
+     */
+    get displayRoofs(): boolean;
+
+    /**
+     * A convenience reference to the tile occlusion mask on the primary canvas group.
+     */
+    get depthMask(): CachedContainer;
+
+    override controllableObjects(): Generator<Tile>;
+
+    override _activate(): void;
+
+    override _deactivate(): void;
+
+    /**
+     * Activate a sublayer of the tiles layer, which controls interactivity of placeables and release controlled objects.
+     * @param foreground - Which sublayer need to be activated? Foreground or background?
+     *                     (default: `false`)
+     */
+    protected _activateSubLayer(foreground?: boolean): void;
+
+    _tearDown(options?: Record<string, unknown> | undefined): Promise<void>;
+
+    protected _onDragLeftStart(event: PIXI.FederatedEvent): Promise<unknown>;
+
+    protected _onDragLeftMove(event: PIXI.FederatedEvent): void;
+
+    protected _onDragLeftDrop(event: PIXI.FederatedEvent): Promise<void>;
+
+    protected _onDragLeftCancel(event: PointerEvent): void;
+
+    /**
+     * Handle drop events for Tile data on the Tiles Layer
+     * @param event - The concluding drag event
+     * @param data  - The extracted Tile data
+     */
+    protected _onDropData(event: DragEvent, data: DropData<any>): Promise<void>;
+
+    /**
+     * Prepare the data object when a new Tile is dropped onto the canvas
+     * @param event - The concluding drag event
+     * @param data  - The extracted Tile data
+     * @returns The prepared data to create
+     */
+    _getDropData(event: DragEvent, data: DropData<any>): Promise<DropData<any>>;
+
+    /**
+     * @deprecated since v11, will be removed in v13
+     * @remarks "TilesLayer#textureDataMap has moved to TextureLoader.textureBufferDataMap"
+     */
+    get textureDataMap(): (typeof TextureLoader)["textureBufferDataMap"];
+  }
+
+  namespace TilesLayer {
+    interface LayerOptions extends PlaceablesLayer.LayerOptions<"Tile"> {
+      name: "tiles";
+      zIndex: 0;
+      controllableObjects: true;
+      rotatableObjects: true;
+      elevationSorting: true;
+    }
+  }
 }
