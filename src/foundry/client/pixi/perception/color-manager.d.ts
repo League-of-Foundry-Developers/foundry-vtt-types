abf807ffea13b5f48bec9de5ffcf06c0fce0a2ff
JPMeehan <jpmeehan5@gmail.com>
2024-01-02 14:24:58 -0800

    Resolved some TODOs

--- src/foundry/client/pixi/perception/color-manager.d.ts
+++ src/foundry/client/pixi/perception/color-manager.d.ts
@@ -1,7 +1,7 @@
+import Color from "../../../common/utils/color.mjs";
+
 export {};
 
-// TODO: Define Color in common/utils/color.mjs
-type Color = number;
 declare global {
   /**
    * A singleton class dedicated to manage the color spaces associated with the scene and the canvas.
