0da6c3c8d05795fc666e462e5179c77e078be402
JPMeehan <jpmeehan5@gmail.com>
2024-01-08 11:06:29 -0800

    Initialized Canvas Test Update

--- tests/foundry/client/pixi/core/shapes/normalized-rectangle.test-d.ts
+++
@@ -1,7 +0,0 @@
-import { assertType, expectTypeOf } from "vitest";
-
-const rectangle = new NormalizedRectangle(100, 300, 500, 400);
-assertType<PIXI.Rectangle>(rectangle);
-expectTypeOf(rectangle.rotate(0.5)).toEqualTypeOf<NormalizedRectangle>();
-
-expectTypeOf(NormalizedRectangle.fromRotation(100, 300, 200, 100, 0.5)).toEqualTypeOf<NormalizedRectangle>();
