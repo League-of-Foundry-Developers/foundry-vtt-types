b124f8983db8c30a901d3b11312d7ffb8c125051
JPMeehan <jpmeehan5@gmail.com>
2024-03-21 07:43:47 -0700

    PointSourceMesh type fix

--- tests/foundry/client/pixi/core/containers/point-source-mesh.test-d.ts
+++ tests/foundry/client/pixi/core/containers/point-source-mesh.test-d.ts
@@ -5,7 +5,6 @@ const myGeometry = new PIXI.Geometry();
 const myShader = AdaptiveLightingShader.create();
 
 // Matches a call made in RenderedPointSource##createMesh
-// @ts-expect-error This is a bonafide type error in the Foundry source
 const myPointSourceMesh = new PointSourceMesh(myGeometry, myShader);
 
 expectTypeOf(myPointSourceMesh.addChild()).toEqualTypeOf<never>();
