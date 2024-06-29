fcb71ddbcf64fd58bc4bbdb62e5b668c2e504a3b
LinkedFluuuush <linkedfluuuush@gmail.com>
2024-06-29 14:28:30 +0200

    Created Journal Entry Page test file

---
+++ tests/foundry/client/data/documents/journal-entry-page.test-d.ts
@@ -0,0 +1,17 @@
+import { expectTypeOf } from "vitest";
+
+// @ts-expect-error data is required
+new JournalEntryPage();
+
+const myJournalEntryPage = new JournalEntryPage({});
+
+expectTypeOf(myJournalEntryPage.toc).toEqualTypeOf<Record<string, JournalEntryPage.JournalEntryPageHeading>>();
+expectTypeOf(myJournalEntryPage.sceneNote).toEqualTypeOf<Note | null>();
+
+const headingElement = new HTMLHeadingElement();
+expectTypeOf(JournalEntryPage.slugifyHeading(headingElement)).toEqualTypeOf<string>();
+expectTypeOf(JournalEntryPage.slugifyHeading("Test string")).toEqualTypeOf<string>();
+
+expectTypeOf(JournalEntryPage.buildTOC([new HTMLElement(), new HTMLElement()], {})).toEqualTypeOf<
+  Record<string, JournalEntryPage.JournalEntryPageHeading>
+>();
