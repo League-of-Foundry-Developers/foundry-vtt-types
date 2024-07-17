f9c40c86a7111eaa74384595db19cab37346235c
Joseph <JPMeehan5@Gmail.com>
2024-07-17 10:05:24 -0700

    Backend tests

---
+++ tests/foundry/client-esm/data/client-backend.mjs.test-d.ts
@@ -0,0 +1,15 @@
+import { expectTypeOf } from "vitest";
+
+const myBackend = new foundry.data.ClientDatabaseBackend();
+
+expectTypeOf(myBackend.create(Item, { data: [{ name: "foo" }], broadcast: true, pack: null })).toEqualTypeOf<
+  Promise<Item[]>
+>();
+
+expectTypeOf(myBackend.update(Actor, { updates: [{ name: "foo" }], broadcast: false, pack: null })).toEqualTypeOf<
+  Promise<Actor[]>
+>();
+
+expectTypeOf(myBackend.delete(Scene, { ids: [], broadcast: true, pack: "some.pack" })).toEqualTypeOf<
+  Promise<Scene[]>
+>();
