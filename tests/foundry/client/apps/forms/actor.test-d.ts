6ad99d0f74e300885f55463b54e7a54391f5ba19
Luke Abby <luke@lukeabby.dev>
2024-08-05 09:23:30 +0200

    Revert test change

--- tests/foundry/client/apps/forms/actor.test-d.ts
+++ tests/foundry/client/apps/forms/actor.test-d.ts
@@ -1,6 +1,6 @@
 import { expectTypeOf } from "vitest";
 
 const actorSheet = new ActorSheet(new Actor({ name: "Some dude", type: "character" }));
-expectTypeOf(actorSheet.objectA).toEqualTypeOf<Actor>();
+expectTypeOf(actorSheet.object).toEqualTypeOf<Actor>();
 expectTypeOf(actorSheet.actor).toEqualTypeOf<Actor>();
 expectTypeOf(actorSheet.token).toEqualTypeOf<TokenDocument | null>();
