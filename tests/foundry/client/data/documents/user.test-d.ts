c68ca8b84ef6dfbc14c9f716f8a3b1bd4765a859
Luke Abby <luke@lukeabby.dev>
2024-11-02 08:41:06 -0700

    Fix issue with User configuration

--- tests/foundry/client/data/documents/user.test-d.ts
+++ tests/foundry/client/data/documents/user.test-d.ts
@@ -14,9 +14,7 @@ expectTypeOf(user.id).toEqualTypeOf<string | null>();
 expectTypeOf(user.viewedScene).toEqualTypeOf<string | null>();
 assertType<Partial<Record<string, boolean>>>(user.permissions);
 expectTypeOf(user.getHotbarMacros().map((each) => each.macro)).toEqualTypeOf<Array<Macro | null>>();
-expectTypeOf(user.getHotbarMacros().map((each) => each.macro)).toEqualTypeOf<
-  Array<Macro.ConfiguredInstance | null>
->();
+expectTypeOf(user.getHotbarMacros().map((each) => each.macro)).toEqualTypeOf<Array<Macro.ConfiguredInstance | null>>();
 
 user.assignHotbarMacro(new Macro({ name: "Foo" }), 1);
 
@@ -26,3 +24,22 @@ expectTypeOf(user.avatar).toEqualTypeOf<string | null | undefined>();
 expectTypeOf(user.sheet).toEqualTypeOf<FormApplication | foundry.applications.api.ApplicationV2 | null>();
 
 expectTypeOf(user.color).toEqualTypeOf<string>();
+
+declare class ConfiguredUser extends User {}
+
+declare global {
+  interface DocumentClassConfig {
+    User: typeof ConfiguredUser;
+  }
+
+  interface FlagConfig {
+    User: {
+      someScope: {
+        someFlag: number;
+      };
+    };
+  }
+}
+
+expectTypeOf<User.ConfiguredClass>().toEqualTypeOf<typeof ConfiguredUser>();
+expectTypeOf<User.ConfiguredInstance>().toEqualTypeOf<ConfiguredUser>();
