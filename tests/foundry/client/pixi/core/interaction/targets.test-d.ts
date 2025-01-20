e77ca56d0e7d6cc7c066f6430e99213b49fa624f
esheyw <esheyw@gmail.com>
2025-01-19 20:58:01 -0800

    UserTargets done

--- tests/foundry/client/pixi/core/interaction/targets.test-d.ts
+++ tests/foundry/client/pixi/core/interaction/targets.test-d.ts
@@ -1,10 +1,10 @@
 import { expectTypeOf } from "vitest";
 
-declare const user: User;
-declare const token: Token;
+declare const user: User.ConfiguredInstance;
+declare const token: Token.ConfiguredInstance;
 
 const targets = new UserTargets(user);
-expectTypeOf(targets.user).toEqualTypeOf<User>();
+expectTypeOf(targets.user).toEqualTypeOf<User.ConfiguredInstance>();
 expectTypeOf(targets.ids).toEqualTypeOf<string[]>();
 expectTypeOf(targets.add(token)).toEqualTypeOf<void>();
 expectTypeOf(targets.clear()).toEqualTypeOf<void>();
