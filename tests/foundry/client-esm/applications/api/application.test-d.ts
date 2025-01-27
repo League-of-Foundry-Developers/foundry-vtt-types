f86eee320de13736c6a863b69f38481a80bb67ba
Dov Rosenberg <dov@rosenbergmail.com>
2025-01-27 17:47:44 -0500

    api done

--- tests/foundry/client-esm/applications/api/application.test-d.ts
+++ tests/foundry/client-esm/applications/api/application.test-d.ts
@@ -1,3 +1,6 @@
+import { expectTypeOf } from "vitest";
+import type { default as AppV2Namespace } from "../../../../../src/foundry/client-esm/applications/api/application.d.mts";
+
 const { ApplicationV2 } = foundry.applications.api;
 
 // Regression test for issue where synchronous actions were not being allowed.
@@ -33,3 +36,56 @@ declare const x: DeeplyOptional;
 x.render({
   parts: ["part-name"],
 });
+
+declare const applicationV2: InstanceType<typeof ApplicationV2>;
+
+expectTypeOf(applicationV2.options).toEqualTypeOf<Readonly<AppV2Namespace.Configuration>>();
+expectTypeOf(applicationV2.window).toEqualTypeOf<{
+  header?: HTMLElement | undefined;
+  resize?: HTMLElement | undefined;
+  title: HTMLHeadingElement | undefined;
+  icon: HTMLElement | undefined;
+  close: HTMLButtonElement | undefined;
+  controls: HTMLButtonElement | undefined;
+  controlsDropdown: HTMLDivElement | undefined;
+  onDrag: (event: PointerEvent) => void;
+  onResize: (event: PointerEvent) => void;
+  pointerStartPosition: AppV2Namespace.Position | undefined;
+  pointerMoveThrottle: boolean;
+}>();
+expectTypeOf(applicationV2.tabGroups).toEqualTypeOf<Record<string, string>>();
+expectTypeOf(applicationV2.classList).toEqualTypeOf<DOMTokenList>();
+expectTypeOf(applicationV2.id).toEqualTypeOf<string>();
+expectTypeOf(applicationV2.title).toEqualTypeOf<string>();
+expectTypeOf(applicationV2.element).toEqualTypeOf<HTMLElement>();
+expectTypeOf(applicationV2.minimized).toEqualTypeOf<boolean>();
+expectTypeOf(applicationV2.position).toEqualTypeOf<AppV2Namespace.Position>();
+expectTypeOf(applicationV2.rendered).toEqualTypeOf<boolean>();
+expectTypeOf(applicationV2.state).toEqualTypeOf<typeof ApplicationV2.RENDER_STATES>();
+expectTypeOf(applicationV2.render()).toEqualTypeOf<Promise<InstanceType<typeof ApplicationV2>>>();
+expectTypeOf(applicationV2.close()).toEqualTypeOf<Promise<InstanceType<typeof ApplicationV2>>>();
+
+declare const position: AppV2Namespace.Position;
+expectTypeOf(applicationV2.setPosition(position)).toEqualTypeOf<AppV2Namespace.Position>();
+expectTypeOf(applicationV2.toggleControls()).toEqualTypeOf<void>();
+expectTypeOf(applicationV2.minimize()).toEqualTypeOf<Promise<void>>();
+expectTypeOf(applicationV2.maximize()).toEqualTypeOf<Promise<void>>();
+expectTypeOf(applicationV2.bringToFront()).toEqualTypeOf<void>();
+expectTypeOf(applicationV2.changeTab("", "")).toEqualTypeOf<void>();
+
+declare const formConfig: AppV2Namespace.FormConfiguration;
+declare const event: Event;
+expectTypeOf(applicationV2._onChangeForm(formConfig, event)).toEqualTypeOf<void>();
+
+expectTypeOf(ApplicationV2.BASE_APPLICATION).toEqualTypeOf<typeof ApplicationV2>();
+expectTypeOf(ApplicationV2.RENDER_STATES).toEqualTypeOf<{
+  ERROR: -3;
+  CLOSING: -2;
+  CLOSED: -1;
+  NONE: 0;
+  RENDERING: 1;
+  RENDERED: 2;
+}>();
+expectTypeOf(ApplicationV2.emittedEvents).toEqualTypeOf<["render", "close", "position"]>();
+expectTypeOf(ApplicationV2.inheritanceChain()).toEqualTypeOf<Generator<AppV2Namespace.AnyConstructor>>();
+expectTypeOf(ApplicationV2.parseCSSDimensions("", 1)).toEqualTypeOf<number>();
--- tests/foundry/client-esm/applications/api/handlebars-application.test-d.ts
+++ tests/foundry/client-esm/applications/api/handlebars-application.test-d.ts
@@ -1,5 +1,16 @@
+import { expectTypeOf } from "vitest";
+import type { default as HBMixinNamespace } from "../../../../../src/foundry/client-esm/applications/api/handlebars-application.d.mts";
+
 const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api;
 
 // Regression test for "Class static side 'typeof EnabledCompendiumsSettings' incorrectly extends base class static side 'typeof HandlebarsApplication & typeof ApplicationV2'."
 // Reported by @denwav, see https://discord.com/channels/732325252788387980/803646399014109205/1259600593714937978
 export class EnabledCompendiumsSettings extends HandlebarsApplicationMixin(ApplicationV2) {}
+
+class HBMixinTest extends HandlebarsApplicationMixin(ApplicationV2) {}
+const hbMixinTest = new HBMixinTest();
+
+expectTypeOf(hbMixinTest.parts).toEqualTypeOf<Record<string, HTMLElement>>();
+expectTypeOf(hbMixinTest.title).toEqualTypeOf<string>();
+
+expectTypeOf(HBMixinTest.PARTS).toEqualTypeOf<Record<string, HBMixinNamespace.HandlebarsTemplatePart>>();
