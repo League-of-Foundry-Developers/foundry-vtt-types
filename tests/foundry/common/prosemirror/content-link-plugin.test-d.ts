6e7a405ac9c3913786b5051bb6358e4a312c25b5
Dov Rosenberg <dov@rosenbergmail.com>
2025-02-02 18:07:04 -0500

    fix

--- tests/foundry/common/prosemirror/content-link-plugin.test-d.ts
+++ tests/foundry/common/prosemirror/content-link-plugin.test-d.ts
@@ -1,10 +1,9 @@
 import { expectTypeOf } from "vitest";
-import type { AnyDocument } from "../../../../src/foundry/client/data/abstract/client-document.d.mts";
 
 declare const schema: foundry.prosemirror.Schema;
 
 const proseMirrorContentLinkPlugin = new foundry.prosemirror.ProseMirrorContentLinkPlugin(schema, {});
-expectTypeOf(proseMirrorContentLinkPlugin.document).toEqualTypeOf<Readonly<AnyDocument>>();
+expectTypeOf(proseMirrorContentLinkPlugin.document).toEqualTypeOf<foundry.abstract.Document.Any>();
 expectTypeOf(proseMirrorContentLinkPlugin.relativeLinks).toEqualTypeOf<Readonly<boolean>>();
 
 expectTypeOf(
