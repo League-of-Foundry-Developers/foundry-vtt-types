e65dc6f8b27c99b3159309a1446cb55056475390
Dov Rosenberg <dov@rosenbergmail.com>
2025-02-02 07:24:44 -0500

    schema

---
+++ tests/foundry/common/prosemirror/schema/lists.test-d.ts
@@ -0,0 +1,34 @@
+/* eslint-disable import/extensions */
+import type { Node } from "prosemirror-model";
+import { expectTypeOf } from "vitest";
+import { ol, ul, li, liText } from "../../../../../src/foundry/common/prosemirror/schema/lists.mjs";
+
+expectTypeOf(ol).toEqualTypeOf<{
+  content: string;
+  managed: Record<string, unknown>;
+  group: string;
+  attrs: Record<string, unknown>;
+  parseDOM: Record<string, unknown>[];
+  toDOM: (node: Node) => [string, number] | [string, Record<string, unknown>, number];
+}>();
+
+expectTypeOf(ul).toEqualTypeOf<{
+  content: string;
+  group: string;
+  parseDOM: Record<string, unknown>[];
+  toDOM: () => [string, number];
+}>();
+
+expectTypeOf(li).toEqualTypeOf<{
+  content: string;
+  defining: boolean;
+  parseDOM: Record<string, unknown>[];
+  toDOM: () => [string, number];
+}>();
+
+expectTypeOf(liText).toEqualTypeOf<{
+  content: string;
+  defining: boolean;
+  parseDOM: Record<string, unknown>[];
+  toDOM: () => [string, number];
+}>();
