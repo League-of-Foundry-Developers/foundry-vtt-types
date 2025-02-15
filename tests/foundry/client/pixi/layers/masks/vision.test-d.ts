126b113a354c1ba23b7d8555bb5e803b65a268ac
esheyw <esheyw@gmail.com>
2025-02-15 11:22:45 -0800

    copy work from perc/layers branch

--- tests/foundry/client/pixi/layers/masks/vision.test-d.ts
+++ tests/foundry/client/pixi/layers/masks/vision.test-d.ts
@@ -1,13 +1,35 @@
 import { expectTypeOf } from "vitest";
 
-const mySprite = new SpriteMesh();
+expectTypeOf(CanvasVisionMask.textureConfiguration).toEqualTypeOf<CachedContainer.TextureConfiguration>();
 
+const mySprite = new SpriteMesh();
 const myVisionMask = new CanvasVisionMask(mySprite);
 
-const myVisionContainer = new PIXI.Container();
+expectTypeOf(myVisionMask.clearColor).toEqualTypeOf<Color.RGBAColorVector>();
+expectTypeOf(myVisionMask.autoRender).toBeBoolean();
+expectTypeOf(myVisionMask.vision).toEqualTypeOf<CanvasVisionMask.CanvasVisionContainer | undefined>();
+const vis = myVisionMask.vision;
+if (vis) {
+  expectTypeOf(vis.containmentFilter).toEqualTypeOf<VoidFilter>();
+  expectTypeOf(vis.sight.preview).toEqualTypeOf<PIXI.LegacyGraphics>();
+  expectTypeOf(vis.darkness.darkness).toEqualTypeOf<PIXI.LegacyGraphics>();
+  expectTypeOf(vis.light.global.meshes).toEqualTypeOf<PIXI.Container>();
+  //deprecated since v12 until v14
+  expectTypeOf(vis.light.tokens.tokens.tokens.tokens).toEqualTypeOf<typeof vis.light>();
+  expectTypeOf(vis.base).toEqualTypeOf<typeof vis.light.preview>();
+  expectTypeOf(vis.fov).toEqualTypeOf<typeof vis.light>();
+  expectTypeOf(vis.los).toEqualTypeOf<typeof vis.light.mask>();
+}
 
-expectTypeOf(myVisionMask.createRenderTexture()).toEqualTypeOf<PIXI.RenderTexture>();
+expectTypeOf(myVisionMask.blurFilter).toEqualTypeOf<AlphaBlurFilter | undefined>();
 
-expectTypeOf(myVisionMask.clearColor[3]).toEqualTypeOf<number>();
+expectTypeOf(myVisionMask.draw()).toEqualTypeOf<Promise<void>>();
 
-expectTypeOf(myVisionMask.attachVision(myVisionContainer)).toEqualTypeOf<CanvasVisionContainer>();
+declare const someVisionContainer: CanvasVisionMask.CanvasVisionContainer;
+expectTypeOf(myVisionMask.attachVision(someVisionContainer)).toEqualTypeOf<CanvasVisionMask.CanvasVisionContainer>();
+expectTypeOf(myVisionMask.detachVision()).toEqualTypeOf<CanvasVisionMask.CanvasVisionContainer>();
+
+//deprecated since v11, until v13
+expectTypeOf(myVisionMask.filter).toEqualTypeOf<AlphaBlurFilter | undefined>();
+declare const someBlurFilter: AlphaBlurFilter;
+myVisionMask.filter = someBlurFilter;
