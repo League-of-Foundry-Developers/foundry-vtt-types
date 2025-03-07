e422bfd71b282948b72eef397ce37ee3d6c7b4f3
Joseph <JPMeehan5@Gmail.com>
2025-03-06 17:04:58 -0800

    FilePicker to 13.337

---
+++ tests/foundry/client-esm/applications/apps/file-picker.test-d.ts
@@ -0,0 +1,13 @@
+import { expectTypeOf } from "vitest";
+
+import FilePicker = foundry.applications.apps.FilePicker;
+
+declare const sourceType: FilePicker.SourceType;
+
+expectTypeOf(sourceType).toEqualTypeOf<"data" | "public" | "s3">();
+
+declare const myFile: File;
+
+const response = await FilePicker.upload("foo", "bar", myFile);
+
+expectTypeOf(response.ok).toBeBoolean();
