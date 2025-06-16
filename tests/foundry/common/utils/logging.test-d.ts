03750e6e412ba2f358b6c70b3c62669719f56f79
esheyw <esheyw@gmail.com>
2025-06-15 21:31:11 -0700

    logging and semaphore tests

--- tests/foundry/common/utils/logging.test-d.ts
+++ tests/foundry/common/utils/logging.test-d.ts
@@ -5,3 +5,26 @@ expectTypeOf(foundry.utils.logCompatibilityWarning)
   .parameter(1)
   .toEqualTypeOf<foundry.utils.LogCompatibilityWarningOptions | undefined>();
 expectTypeOf(foundry.utils.logCompatibilityWarning).returns.toEqualTypeOf<void>();
+
+expectTypeOf(foundry.utils.logCompatibilityWarning("hi there")).toBeVoid();
+expectTypeOf(foundry.utils.logCompatibilityWarning("hi there", {})).toBeVoid();
+expectTypeOf(
+  foundry.utils.logCompatibilityWarning("hi there", {
+    mode: CONST.COMPATIBILITY_MODES.FAILURE,
+    details: "really biffed it",
+    once: true,
+    since: 42,
+    until: 373,
+    stack: false,
+  }),
+).toBeVoid();
+expectTypeOf(
+  foundry.utils.logCompatibilityWarning("hi there", {
+    mode: undefined,
+    details: undefined,
+    once: undefined,
+    since: undefined,
+    until: undefined,
+    stack: undefined,
+  }),
+).toBeVoid();
--- tests/foundry/common/utils/semaphore.test-d.ts
+++ tests/foundry/common/utils/semaphore.test-d.ts
@@ -5,8 +5,22 @@ new Semaphore();
 
 const lock = new Semaphore(4);
 
+expectTypeOf(lock.max).toBeNumber();
+lock.max = 1; // writeable
+
+expectTypeOf(lock.remaining).toEqualTypeOf<number>();
+// @ts-expect-error This getter has no setter
+lock.remaining = 7;
+
+expectTypeOf(lock.active).toEqualTypeOf<number>();
+// @ts-expect-error This getter has no setter
+lock.active = 42;
+
 expectTypeOf(lock.add(async (s: string) => !s, "test")).toEqualTypeOf<Promise<boolean>>();
 expectTypeOf(lock.add(async () => false)).toEqualTypeOf<Promise<boolean>>();
+
+declare function foo(): Promise<string | number | null>;
+expectTypeOf(await lock.add(foo)).toEqualTypeOf<string | number | null>();
 
 // @ts-expect-error - The callback needs one argument.
 lock.add(async (s: string) => !s);
@@ -14,6 +28,4 @@ lock.add(async (s: string) => !s);
 // @ts-expect-error - The callback doesn't need any arguments.
 lock.add(async () => false, "");
 
-expectTypeOf(lock.remaining).toEqualTypeOf<number>();
-expectTypeOf(lock.active).toEqualTypeOf<number>();
 expectTypeOf(lock.clear()).toEqualTypeOf<void>();
