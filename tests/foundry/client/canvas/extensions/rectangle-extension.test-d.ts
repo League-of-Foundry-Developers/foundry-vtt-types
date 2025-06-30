3c2d680169ddeb7268aa92fec55da262df4aff4d
esheyw <esheyw@gmail.com>
2025-06-29 20:58:54 -0700

    rectangle tests

--- tests/foundry/client/canvas/extensions/rectangle-extension.test-d.ts
+++ tests/foundry/client/canvas/extensions/rectangle-extension.test-d.ts
@@ -3,6 +3,14 @@ import { expectTypeOf } from "vitest";
 import Canvas = foundry.canvas.Canvas;
 import WeilerAthertonClipper = foundry.canvas.geometry.WeilerAthertonClipper;
 
+expectTypeOf(PIXI.Rectangle.CS_ZONES).toExtend<Record<keyof PIXI.Rectangle.CS_Zones, PIXI.Rectangle.CS_ZONES>>();
+
+expectTypeOf(PIXI.Rectangle.fromRotation(50, 50, 500, 500, Math.PI / 2)).toEqualTypeOf<PIXI.Rectangle>();
+expectTypeOf(PIXI.Rectangle.fromRotation(50, 50, 500, 500, Math.PI / 2, somePIXIPoint)).toEqualTypeOf<PIXI.Rectangle>();
+expectTypeOf(
+  PIXI.Rectangle.fromRotation(50, 50, 500, 500, Math.PI / 2, somePIXIPoint, otherRect),
+).toEqualTypeOf<PIXI.Rectangle>();
+
 const rect = new PIXI.Rectangle(64, 64, 100, 100);
 const clipperPoints = [
   { X: 50, Y: 50 },
@@ -11,23 +19,27 @@ const clipperPoints = [
   { X: 100, Y: 50 },
 ];
 
-declare const somePoint: PIXI.Point;
+const somePoint = { x: 5000, y: 12345 };
+declare const somePIXIPoint: PIXI.Point;
 declare const otherRect: PIXI.Rectangle;
 declare const somePolygon: PIXI.Polygon;
 
-expectTypeOf(PIXI.Rectangle.fromRotation(50, 50, 500, 500, Math.PI / 2, somePoint)).toEqualTypeOf<PIXI.Rectangle>();
-
-expectTypeOf(rect.CS_ZONES).toExtend<Record<keyof PIXI.Rectangle.CS_Zones, PIXI.Rectangle.CS_ZONES>>();
+expectTypeOf(rect.center).toEqualTypeOf<Canvas.Point>();
+expectTypeOf(rect.pointIsOn(somePoint)).toBeBoolean();
 expectTypeOf(rect._getEdgeZone(somePoint)).toEqualTypeOf<PIXI.Rectangle.CS_ZONES>();
-expectTypeOf(rect._getZone(somePoint)).toEqualTypeOf<PIXI.Rectangle.CS_ZONES>();
-expectTypeOf(rect.pointIsOn(somePoint)).toEqualTypeOf<boolean>();
 expectTypeOf(rect.pointsBetween({ x: 40, y: 30 }, { x: 200, y: 300 })).toEqualTypeOf<Canvas.Point[]>();
 expectTypeOf(rect.segmentIntersections(somePoint, { x: 400, y: 200 })).toEqualTypeOf<Canvas.Point[]>();
 expectTypeOf(rect.intersection(otherRect)).toEqualTypeOf<PIXI.Rectangle>();
 expectTypeOf(rect.toPolygon()).toEqualTypeOf<PIXI.Polygon>();
 expectTypeOf(rect.leftEdge).toEqualTypeOf<PIXI.Rectangle.Edge>();
+expectTypeOf(rect.rightEdge).toEqualTypeOf<PIXI.Rectangle.Edge>();
+expectTypeOf(rect.topEdge).toEqualTypeOf<PIXI.Rectangle.Edge>();
+expectTypeOf(rect.bottomEdge).toEqualTypeOf<PIXI.Rectangle.Edge>();
+expectTypeOf(rect._getZone(somePoint)).toEqualTypeOf<PIXI.Rectangle.CS_ZONES>();
+
 expectTypeOf(rect.lineSegmentIntersects(somePoint, { x: 40, y: 20 })).toEqualTypeOf<boolean>();
-expectTypeOf<PIXI.Rectangle.CS_ZONES>().toExtend<number>();
+expectTypeOf(rect.lineSegmentIntersects(somePoint, { x: 40, y: 20 }, { inside: true })).toEqualTypeOf<boolean>();
+expectTypeOf(rect.lineSegmentIntersects(somePoint, { x: 40, y: 20 }, { inside: undefined })).toEqualTypeOf<boolean>();
 
 // no options
 expectTypeOf(rect.intersectPolygon(somePolygon)).toEqualTypeOf<PIXI.Polygon>();
@@ -36,6 +48,13 @@ expectTypeOf(rect.intersectPolygon(somePolygon)).toEqualTypeOf<PIXI.Polygon>();
 expectTypeOf(
   rect.intersectPolygon(somePolygon, {
     weilerAtherton: true,
+    canMutate: false,
+    clipType: WeilerAthertonClipper.CLIP_TYPES.INTERSECT,
+  }),
+).toEqualTypeOf<PIXI.Polygon>();
+expectTypeOf(
+  rect.intersectPolygon(somePolygon, {
+    weilerAtherton: undefined,
     canMutate: false,
     clipType: WeilerAthertonClipper.CLIP_TYPES.INTERSECT,
   }),
@@ -60,13 +79,20 @@ expectTypeOf(
   rect.intersectPolygon(somePolygon, {
     weilerAtherton: true,
     clipType: ClipperLib.ClipType.ctIntersection,
-    canMutate: null,
+    canMutate: undefined,
   }),
 ).toEqualTypeOf<PIXI.Polygon>();
 
-// This return type (`| []`) is very probably a Foundry bug in v12.
+expectTypeOf(rect.intersectClipper(clipperPoints)).toEqualTypeOf<PIXI.Polygon.ClipperPoint[]>();
 expectTypeOf(
   rect.intersectClipper(clipperPoints, { clipType: ClipperLib.ClipType.ctDifference, scalingFactor: 3 }),
-).toEqualTypeOf<PIXI.Polygon | []>();
+).toEqualTypeOf<PIXI.Polygon.ClipperPoint[]>();
+expectTypeOf(rect.intersectClipper(clipperPoints, { clipType: undefined, scalingFactor: undefined })).toEqualTypeOf<
+  PIXI.Polygon.ClipperPoint[]
+>();
 
 expectTypeOf(rect.overlaps(otherRect)).toEqualTypeOf<boolean>();
+expectTypeOf(rect.normalize()).toEqualTypeOf<PIXI.Rectangle>();
+
+expectTypeOf(rect.rotate((4 / 5) * Math.PI)).toEqualTypeOf<PIXI.Rectangle>();
+expectTypeOf(rect.rotate((4 / 5) * Math.PI, somePIXIPoint)).toEqualTypeOf<PIXI.Rectangle>();
