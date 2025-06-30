5abcc7bc5897cc7817d366caf871a12ce6b0ff06
esheyw <esheyw@gmail.com>
2025-06-29 20:48:42 -0700

    polygon tests

--- tests/foundry/client/canvas/extensions/polygon-extension.test-d.ts
+++ tests/foundry/client/canvas/extensions/polygon-extension.test-d.ts
@@ -1,6 +1,8 @@
 import { expectTypeOf } from "vitest";
 
-declare const somePoint: PIXI.Point;
+import WeilerAthertonClipper = foundry.canvas.geometry.WeilerAthertonClipper;
+
+const somePoint = { x: 50, y: 23 };
 declare const otherPoly: PIXI.Polygon;
 declare const someCircle: PIXI.Circle;
 declare const someRect: PIXI.Rectangle;
@@ -11,28 +13,55 @@ const clipperPoints = [
   { X: 100, Y: 50 },
 ];
 
+expectTypeOf(PIXI.Polygon.fromClipperPoints(clipperPoints)).toEqualTypeOf<PIXI.Polygon>();
 expectTypeOf(PIXI.Polygon.fromClipperPoints(clipperPoints, { scalingFactor: 4 })).toEqualTypeOf<PIXI.Polygon>();
+expectTypeOf(PIXI.Polygon.fromClipperPoints(clipperPoints, { scalingFactor: undefined })).toEqualTypeOf<PIXI.Polygon>();
 
 const myPoly = new PIXI.Polygon([0, 1, 2, 3]);
 
 expectTypeOf(myPoly.isPositive).toBeBoolean();
+expectTypeOf(myPoly._isPositive).toEqualTypeOf<boolean | undefined>();
+expectTypeOf(myPoly.clearCache()).toBeVoid();
+expectTypeOf(myPoly.signedArea()).toBeNumber();
+expectTypeOf(myPoly.reverseOrientation()).toEqualTypeOf<typeof myPoly>();
 expectTypeOf(myPoly.addPoint(somePoint)).toEqualTypeOf<typeof myPoly>();
-expectTypeOf(myPoly.toClipperPoints({ scalingFactor: 1.2 })).toEqualTypeOf<PIXI.Polygon.ClipperPoint[]>();
+expectTypeOf(myPoly.getBounds()).toEqualTypeOf<PIXI.Rectangle>();
 
+expectTypeOf(myPoly.toClipperPoints()).toEqualTypeOf<PIXI.Polygon.ClipperPoint[]>();
+expectTypeOf(myPoly.toClipperPoints({ scalingFactor: 40 })).toEqualTypeOf<PIXI.Polygon.ClipperPoint[]>();
+expectTypeOf(myPoly.toClipperPoints({ scalingFactor: undefined })).toEqualTypeOf<PIXI.Polygon.ClipperPoint[]>();
+
+expectTypeOf(myPoly.isClosed).toBeBoolean();
+
+expectTypeOf(myPoly.intersectPolygon(otherPoly)).toEqualTypeOf<PIXI.Polygon>();
 expectTypeOf(
   myPoly.intersectPolygon(otherPoly, {
     clipType: ClipperLib.ClipType.ctIntersection,
     scalingFactor: 4,
   }),
 ).toEqualTypeOf<PIXI.Polygon>();
+expectTypeOf(
+  myPoly.intersectPolygon(otherPoly, {
+    clipType: undefined,
+    scalingFactor: undefined,
+  }),
+).toEqualTypeOf<PIXI.Polygon>();
 
+expectTypeOf(myPoly.intersectClipper(clipperPoints)).toEqualTypeOf<PIXI.Polygon.ClipperPoint[]>();
 expectTypeOf(
   myPoly.intersectClipper(clipperPoints, {
     clipType: ClipperLib.ClipType.ctXor,
-    scalingFactor: 1.4,
+    scalingFactor: 50,
+  }),
+).toEqualTypeOf<PIXI.Polygon.ClipperPoint[]>();
+expectTypeOf(
+  myPoly.intersectClipper(clipperPoints, {
+    clipType: undefined,
+    scalingFactor: undefined,
   }),
 ).toEqualTypeOf<PIXI.Polygon.ClipperPoint[]>();
 
+expectTypeOf(myPoly.intersectCircle(someCircle)).toEqualTypeOf<PIXI.Polygon>();
 expectTypeOf(
   myPoly.intersectCircle(someCircle, {
     clipType: ClipperLib.ClipType.ctUnion,
@@ -40,11 +69,56 @@ expectTypeOf(
     scalingFactor: 2,
   }),
 ).toEqualTypeOf<PIXI.Polygon>();
+expectTypeOf(
+  myPoly.intersectCircle(someCircle, {
+    clipType: undefined,
+    density: undefined,
+    scalingFactor: undefined,
+  }),
+).toEqualTypeOf<PIXI.Polygon>();
+
+expectTypeOf(myPoly.intersectRectangle(someRect)).toEqualTypeOf<PIXI.Polygon>();
+expectTypeOf(
+  myPoly.intersectRectangle(someRect, {
+    weilerAtherton: false,
+    clipType: ClipperLib.ClipType.ctDifference,
+    scalingFactor: undefined,
+  }),
+).toEqualTypeOf<PIXI.Polygon>();
+
+// @ts-expect-error Can't pass `canMutate` if we're not on the WAC path
+myPoly.intersectRectangle(someRect, { weilerAtherton: false, canMutate: true });
 
 expectTypeOf(
   myPoly.intersectRectangle(someRect, {
     weilerAtherton: true,
-    canMutate: false,
-    clipType: ClipperLib.ClipType.ctXor,
+    canMutate: true,
+    clipType: WeilerAthertonClipper.CLIP_TYPES.INTERSECT,
+  }),
+).toEqualTypeOf<PIXI.Polygon>();
+
+// weilerAtherton: true is default
+expectTypeOf(
+  myPoly.intersectRectangle(someRect, {
+    canMutate: true,
+    clipType: WeilerAthertonClipper.CLIP_TYPES.INTERSECT,
+  }),
+).toEqualTypeOf<PIXI.Polygon>();
+
+// @ts-expect-error Can't pass `scalingFactor` on the WAC branch
+myPoly.intersectRectangle(someRect, { weilerAtherton: true, scalingFactor: CONST.CLIPPER_SCALING_FACTOR });
+
+expectTypeOf(
+  myPoly.intersectRectangle(someRect, {
+    weilerAtherton: undefined,
+    canMutate: undefined,
+    clipType: undefined,
+  }),
+).toEqualTypeOf<PIXI.Polygon>();
+expectTypeOf(
+  myPoly.intersectRectangle(someRect, {
+    weilerAtherton: false,
+    clipType: undefined,
+    scalingFactor: undefined,
   }),
 ).toEqualTypeOf<PIXI.Polygon>();
