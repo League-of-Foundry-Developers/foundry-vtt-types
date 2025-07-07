c874e9521fb6741f2e72b2f6c22186b5d6d8ce9b
esheyw <esheyw@gmail.com>
2025-07-06 17:32:21 -0700

    arrow

--- tests/foundry/client/canvas/interaction/ping/arrow.test-d.ts
+++ tests/foundry/client/canvas/interaction/ping/arrow.test-d.ts
@@ -1,16 +1,19 @@
 import { expectTypeOf } from "vitest";
 import { ArrowPing } from "#client/canvas/interaction/_module.mjs";
 
-const myPoint = new PIXI.Point(2, 2);
+new ArrowPing({ x: 400, y: 737 });
+const myArrowPing = new ArrowPing(
+  { x: 828, y: 777 },
+  {
+    color: [0.2, 0.5, 0.6],
+    color2: [0.3, 0, 0.2],
+    name: "myArrowPing",
+    rings: 2,
+    rotation: Math.PI / 2,
+    duration: 2000,
+    size: 128,
+  },
+);
 
-const myArrowPing = new ArrowPing(myPoint, {
-  color: [0.2, 0.5, 0.6],
-  color2: [0.3, 0, 0.2],
-  name: "myArrowPing",
-  rings: 2,
-  rotation: Math.PI / 2,
-  duration: 2000,
-  size: 128,
-});
-
-expectTypeOf(myArrowPing.animate()).toEqualTypeOf<Promise<boolean>>();
+declare const someGraphics: PIXI.Graphics;
+expectTypeOf(myArrowPing["_drawShape"](someGraphics, Color.from(0xcfbdea), 0.333, 20));
