f7e6dfc882c208a35cf8b598b90388f97a1b76b6
Luke Abby <luke@lukeabby.dev>
2025-07-21 17:32:59 -0700

    Remove old comment about now not broken test

--- tests/foundry/common/abstract/type-data.mjs.test-d.ts
+++ tests/foundry/common/abstract/type-data.mjs.test-d.ts
@@ -118,7 +118,6 @@ class QuestModel extends TypeDataModel<QuestSchema, JournalEntryPage.Implementat
   method() {
     expectTypeOf(this.description).toEqualTypeOf<HTMLElement>();
     expectTypeOf(this._source.description).toEqualTypeOf<string>();
-    // Note(LukeAbby): Currently not working but should.
     expectTypeOf(this.some.deep!.baseProp).toEqualTypeOf<string>();
     expectTypeOf(this.some.deep!.derivedProp).toEqualTypeOf<number>();
     expectTypeOf(this.some.deep!.prop).toEqualTypeOf<number>();
