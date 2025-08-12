7adcfcdd0edc05dbfa105c0a83519f284479d6b0
esheyw <esheyw@gmail.com>
2025-08-12 15:40:35 -0700

    convolver

--- tests/foundry/client/audio/convolver.test-d.ts
+++ tests/foundry/client/audio/convolver.test-d.ts
@@ -1,49 +1,59 @@
-import { expectTypeOf } from "vitest";
+import { describe, expectTypeOf, test } from "vitest";
 import ConvolverEffect = foundry.audio.ConvolverEffect;
 
 declare const context: AudioContext;
 declare const buffer: AudioBuffer;
-
-// @ts-expect-error construction requires an AudioContext
-let effect = new ConvolverEffect();
-effect = new ConvolverEffect(context);
-effect = new ConvolverEffect(context, {});
-
-// all possible properties, most of which are deep magic as far as the test author is concerned
-effect = new ConvolverEffect(context, {
-  intensity: 9,
-  impulseResponsePath: "some/path/to/file.wav",
-  buffer,
-  channelCount: 7,
-  channelCountMode: "explicit",
-  channelInterpretation: "speakers",
-  disableNormalization: false,
-});
-
-// What Foundry actually expects you to pass, based on CONFIG.soundEffects
-effect = new ConvolverEffect(context, {
-  intensity: 8,
-});
-
-effect = new ConvolverEffect(context, {
-  intensity: undefined,
-  impulseResponsePath: undefined,
-  // the rest of the interface comes from a built-in that does not allow nullish values on any properties
-});
-
-expectTypeOf(effect.intensity).toEqualTypeOf<number>();
-effect.intensity = 2; // setter; exactly equivalent to the following test
-expectTypeOf(effect.update({ intensity: 2 })).toEqualTypeOf<void>();
-// non-Finite `intensity`s are ignored
-expectTypeOf(effect.update({ intensity: null })).toEqualTypeOf<void>();
-
 declare const audioNode: AudioNode;
 declare const audioParam: AudioParam;
-expectTypeOf(effect.disconnect()).toEqualTypeOf<void>();
-expectTypeOf(effect.disconnect(audioNode)).toEqualTypeOf<void>();
-expectTypeOf(effect.disconnect(audioParam)).toEqualTypeOf<void>();
-expectTypeOf(effect.connect(audioNode)).toEqualTypeOf<AudioNode>();
-// Foundry doesn't support this overload, so its deprecated
-// eslint-disable-next-line @typescript-eslint/no-deprecated
-expectTypeOf(effect.connect(audioParam)).toEqualTypeOf<AudioParam>();
-expectTypeOf(effect.onConnectFrom(audioNode)).toEqualTypeOf<void>();
+
+describe("ConvolverEffect Tests", () => {
+  test("Construction", () => {
+    // @ts-expect-error construction requires an AudioContext
+    new ConvolverEffect();
+    new ConvolverEffect(context);
+    new ConvolverEffect(context, {});
+
+    // all possible properties, most of which are deep magic as far as the test author is concerned
+    new ConvolverEffect(context, {
+      intensity: 9,
+      impulseResponsePath: "some/path/to/file.wav",
+      buffer,
+      channelCount: 7,
+      channelCountMode: "explicit",
+      channelInterpretation: "speakers",
+      disableNormalization: false,
+    });
+
+    // What Foundry actually expects you to pass, based on CONFIG.soundEffects
+    new ConvolverEffect(context, {
+      intensity: 8,
+    });
+
+    new ConvolverEffect(context, {
+      intensity: undefined,
+      impulseResponsePath: undefined,
+      // the rest of the interface comes from a built-in that does not allow nullish values on any properties
+    });
+  });
+
+  const effect = new ConvolverEffect(context, { intensity: 8 });
+
+  test("Miscellaneous", () => {
+    expectTypeOf(effect.intensity).toEqualTypeOf<number>();
+    effect.intensity = 2; // setter; exactly equivalent to the following test
+    expectTypeOf(effect.update({ intensity: 2 })).toEqualTypeOf<void>();
+    // non-Finite `intensity`s are ignored
+    expectTypeOf(effect.update({ intensity: undefined })).toEqualTypeOf<void>();
+  });
+
+  test("Connection", () => {
+    expectTypeOf(effect.disconnect()).toEqualTypeOf<void>();
+    expectTypeOf(effect.disconnect(audioNode)).toEqualTypeOf<void>();
+    expectTypeOf(effect.disconnect(audioParam)).toEqualTypeOf<void>();
+    expectTypeOf(effect.connect(audioNode)).toEqualTypeOf<AudioNode>();
+    // Foundry doesn't support this overload, so its deprecated
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
+    expectTypeOf(effect.connect(audioParam)).toEqualTypeOf<AudioParam>();
+    expectTypeOf(effect.onConnectFrom(audioNode)).toEqualTypeOf<void>();
+  });
+});
