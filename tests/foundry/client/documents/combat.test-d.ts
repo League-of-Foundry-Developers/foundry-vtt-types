cfec7003c64238685be211a22d5f47e4b5a92ea3
esheyw <esheyw@gmail.com>
2026-05-03 01:02:21 -0700

    test

--- tests/foundry/client/documents/combat.test-d.ts
+++ tests/foundry/client/documents/combat.test-d.ts
@@ -34,7 +34,7 @@ expectTypeOf(combat.rollInitiative("")).toEqualTypeOf<Promise<Combat.Implementat
 expectTypeOf(combat.rollAll()).toEqualTypeOf<Promise<Combat.Implementation>>();
 expectTypeOf(combat.rollNPC()).toEqualTypeOf<Promise<Combat.Implementation>>();
 expectTypeOf(combat.setInitiative("", 1)).toEqualTypeOf<Promise<void>>();
-expectTypeOf(combat.setupTurns()).toEqualTypeOf<Combat.Implementation["turns"]>();
+expectTypeOf(combat.setupTurns()).toEqualTypeOf<Combatant.Stored[]>();
 expectTypeOf(combat.debounceSetup()).toEqualTypeOf<void>();
 expectTypeOf(combat.updateCombatantActors()).toEqualTypeOf<void>();
 
