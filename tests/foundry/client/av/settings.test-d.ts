e6aab68c1b5ce0683d0dc896dbf5fbbf6f1a9e03
esheyw <esheyw@gmail.com>
2026-05-17 01:05:47 -0700

    av

--- tests/foundry/client/av/settings.test-d.ts
+++ tests/foundry/client/av/settings.test-d.ts
@@ -1,35 +1,18 @@
 import { expectTypeOf } from "vitest";
 
 import AVMaster = foundry.av.AVMaster;
+import AVSettings = foundry.av.AVSettings;
 
 const avSettings = new AVSettings();
 
-expectTypeOf(avSettings.client).toEqualTypeOf<AVSettings.ClientSettings>();
-expectTypeOf(avSettings.world).toEqualTypeOf<AVSettings.WorldSettings>();
+AVSettings.DEFAULT_USER_SETTINGS.blocked;
+
+expectTypeOf(avSettings.client).toEqualTypeOf<AVSettings.ClientSettingData>();
+expectTypeOf(avSettings.world).toEqualTypeOf<AVSettings.WorldSettingData>();
 expectTypeOf(avSettings.activity).toEqualTypeOf<Record<string, AVSettings.Data>>();
 expectTypeOf(avSettings.getUser("")).toEqualTypeOf<AVSettings.UserSettings | null>();
 expectTypeOf(avSettings.users).toEqualTypeOf<Record<string, AVSettings.UserSettings>>();
 expectTypeOf(avSettings.verticalDock).toEqualTypeOf<boolean>();
 
-interface CustomVoiceModes {
-  PTT: "ptt"; // TODO(LukeAbby): This appears to be required due to `Settings#mode`'s initial. Check if this is accurate.
-  SOME_CUSTOM_MODE: "custom";
-}
-
-declare global {
-  namespace AVSettings {
-    interface Overrides {
-      VoiceModes: CustomVoiceModes;
-    }
-  }
-}
-
-AVSettings.VOICE_MODES = {
-  PTT: "ptt",
-  SOME_CUSTOM_MODE: "custom",
-};
-
-expectTypeOf(AVSettings.VOICE_MODES).toEqualTypeOf<CustomVoiceModes>();
-
 const avMaster = new AVMaster();
-expectTypeOf(avMaster.settings.client.voice.mode).toEqualTypeOf<"ptt" | "custom">();
+expectTypeOf(avMaster.settings.client.voice.mode).toEqualTypeOf<AVSettings.VOICE_MODES>();
