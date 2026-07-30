23e34f8f5736350f4d0d7e7b9b66b8c3e8d3f0d4
esheyw <esheyw@gmail.com>
2026-07-30 00:33:27 -0700

    more tests, touchup other helpers

--- tests/foundry/client/utils/helpers.test-d.ts
+++ tests/foundry/client/utils/helpers.test-d.ts
@@ -2,13 +2,19 @@ import { describe, expectTypeOf, test } from "vitest";
 
 import CompendiumCollection = foundry.documents.collections.CompendiumCollection;
 
-expectTypeOf(foundry.utils.saveDataToFile("", "", "")).toEqualTypeOf<void>();
+test("cleanHTML", () => {
+  expectTypeOf(foundry.utils.cleanHTML("foo")).toBeString();
+});
+
+test("saveDataToFile", () => {
+  expectTypeOf(foundry.utils.saveDataToFile("", "", "")).toEqualTypeOf<void>();
+});
 
 declare const file: File;
-expectTypeOf(foundry.utils.readTextFromFile(file)).toEqualTypeOf<Promise<string>>();
 
-expectTypeOf(getDocumentClass("Actor")).toEqualTypeOf<Actor.ImplementationClass>();
-expectTypeOf(getDocumentClass("Item")).toEqualTypeOf<Item.ImplementationClass>();
+test("readTextFromFile", () => {
+  expectTypeOf(foundry.utils.readTextFromFile(file)).toEqualTypeOf<Promise<string>>();
+});
 
 declare const tokenDoc: TokenDocument.Stored;
 declare const scene: Scene.Stored;
@@ -145,6 +151,12 @@ describe("fromUuidSync Tests", () => {
       ActiveEffect.Stored | CompendiumCollection.IndexEntry<"ActiveEffect"> | null
     >();
 
+    // `strict` has no bearing on return type:
+
+    expectTypeOf(fromUuidSync(settingUuid, { strict: true })).toEqualTypeOf<Setting.Stored | null>();
+    expectTypeOf(fromUuidSync(settingUuid, { strict: false })).toEqualTypeOf<Setting.Stored | null>();
+    expectTypeOf(fromUuidSync(settingUuid, { strict: undefined })).toEqualTypeOf<Setting.Stored | null>();
+
     // relative:
 
     expectTypeOf(fromUuidSync(actorUuid, { relative: undefined })).toEqualTypeOf<
@@ -161,7 +173,7 @@ describe("fromUuidSync Tests", () => {
     fromUuidSync(actorUuid, { relative: scene });
     // @ts-expect-error `Setting`s cannot be found inside anything, the only valid value is `undefined`.
     fromUuidSync(settingUuid, { relative: scene });
-    // `Setting`s are never found in compendia, no index.z
+    // `Setting`s are never found in compendia, no index.
     expectTypeOf(fromUuidSync(settingUuid, { relative: undefined })).toEqualTypeOf<Setting.Stored | null>();
 
     // invalid:
@@ -242,6 +254,157 @@ describe("fromUuidSync Tests", () => {
   });
 });
 
+test("getDocumentClass", () => {
+  // getDocumentClass is a blessed global, we don't need to go via `foundry.utils`
+
+  expectTypeOf(getDocumentClass("ActiveEffect")).toEqualTypeOf<ActiveEffect.ImplementationClass>();
+  expectTypeOf(getDocumentClass("ActiveEffect").documentName).toEqualTypeOf<"ActiveEffect">();
+
+  expectTypeOf(getDocumentClass("Actor")).toEqualTypeOf<Actor.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Actor").documentName).toEqualTypeOf<"Actor">();
+
+  expectTypeOf(getDocumentClass("ActorDelta")).toEqualTypeOf<ActorDelta.ImplementationClass>();
+  expectTypeOf(getDocumentClass("ActorDelta").documentName).toEqualTypeOf<"ActorDelta">();
+
+  expectTypeOf(getDocumentClass("Adventure")).toEqualTypeOf<Adventure.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Adventure").documentName).toEqualTypeOf<"Adventure">();
+
+  expectTypeOf(getDocumentClass("AmbientLight")).toEqualTypeOf<AmbientLightDocument.ImplementationClass>();
+  expectTypeOf(getDocumentClass("AmbientLight").documentName).toEqualTypeOf<"AmbientLight">();
+
+  expectTypeOf(getDocumentClass("AmbientSound")).toEqualTypeOf<AmbientSoundDocument.ImplementationClass>();
+  expectTypeOf(getDocumentClass("AmbientSound").documentName).toEqualTypeOf<"AmbientSound">();
+
+  expectTypeOf(getDocumentClass("Card")).toEqualTypeOf<Card.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Card").documentName).toEqualTypeOf<"Card">();
+
+  expectTypeOf(getDocumentClass("Cards")).toEqualTypeOf<Cards.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Cards").documentName).toEqualTypeOf<"Cards">();
+
+  expectTypeOf(getDocumentClass("ChatMessage")).toEqualTypeOf<ChatMessage.ImplementationClass>();
+  expectTypeOf(getDocumentClass("ChatMessage").documentName).toEqualTypeOf<"ChatMessage">();
+
+  expectTypeOf(getDocumentClass("Combat")).toEqualTypeOf<Combat.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Combat").documentName).toEqualTypeOf<"Combat">();
+
+  expectTypeOf(getDocumentClass("Combatant")).toEqualTypeOf<Combatant.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Combatant").documentName).toEqualTypeOf<"Combatant">();
+
+  expectTypeOf(getDocumentClass("CombatantGroup")).toEqualTypeOf<CombatantGroup.ImplementationClass>();
+  expectTypeOf(getDocumentClass("CombatantGroup").documentName).toEqualTypeOf<"CombatantGroup">();
+
+  expectTypeOf(getDocumentClass("Drawing")).toEqualTypeOf<DrawingDocument.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Drawing").documentName).toEqualTypeOf<"Drawing">();
+
+  expectTypeOf(getDocumentClass("FogExploration")).toEqualTypeOf<FogExploration.ImplementationClass>();
+  expectTypeOf(getDocumentClass("FogExploration").documentName).toEqualTypeOf<"FogExploration">();
+
+  expectTypeOf(getDocumentClass("Folder")).toEqualTypeOf<Folder.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Folder").documentName).toEqualTypeOf<"Folder">();
+
+  expectTypeOf(getDocumentClass("Item")).toEqualTypeOf<Item.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Item").documentName).toEqualTypeOf<"Item">();
+
+  expectTypeOf(getDocumentClass("JournalEntry")).toEqualTypeOf<JournalEntry.ImplementationClass>();
+  expectTypeOf(getDocumentClass("JournalEntry").documentName).toEqualTypeOf<"JournalEntry">();
+
+  expectTypeOf(getDocumentClass("JournalEntryCategory")).toEqualTypeOf<JournalEntryCategory.ImplementationClass>();
+  expectTypeOf(getDocumentClass("JournalEntryCategory").documentName).toEqualTypeOf<"JournalEntryCategory">();
+
+  expectTypeOf(getDocumentClass("JournalEntryPage")).toEqualTypeOf<JournalEntryPage.ImplementationClass>();
+  expectTypeOf(getDocumentClass("JournalEntryPage").documentName).toEqualTypeOf<"JournalEntryPage">();
+
+  expectTypeOf(getDocumentClass("Level")).toEqualTypeOf<Level.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Level").documentName).toEqualTypeOf<"Level">();
+
+  expectTypeOf(getDocumentClass("Macro")).toEqualTypeOf<Macro.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Macro").documentName).toEqualTypeOf<"Macro">();
+
+  expectTypeOf(getDocumentClass("Note")).toEqualTypeOf<NoteDocument.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Note").documentName).toEqualTypeOf<"Note">();
+
+  expectTypeOf(getDocumentClass("Playlist")).toEqualTypeOf<Playlist.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Playlist").documentName).toEqualTypeOf<"Playlist">();
+
+  expectTypeOf(getDocumentClass("PlaylistSound")).toEqualTypeOf<PlaylistSound.ImplementationClass>();
+  expectTypeOf(getDocumentClass("PlaylistSound").documentName).toEqualTypeOf<"PlaylistSound">();
+
+  expectTypeOf(getDocumentClass("Region")).toEqualTypeOf<RegionDocument.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Region").documentName).toEqualTypeOf<"Region">();
+
+  expectTypeOf(getDocumentClass("RegionBehavior")).toEqualTypeOf<RegionBehavior.ImplementationClass>();
+  expectTypeOf(getDocumentClass("RegionBehavior").documentName).toEqualTypeOf<"RegionBehavior">();
+
+  expectTypeOf(getDocumentClass("RollTable")).toEqualTypeOf<RollTable.ImplementationClass>();
+  expectTypeOf(getDocumentClass("RollTable").documentName).toEqualTypeOf<"RollTable">();
+
+  expectTypeOf(getDocumentClass("Scene")).toEqualTypeOf<Scene.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Scene").documentName).toEqualTypeOf<"Scene">();
+
+  expectTypeOf(getDocumentClass("Setting")).toEqualTypeOf<Setting.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Setting").documentName).toEqualTypeOf<"Setting">();
+
+  expectTypeOf(getDocumentClass("TableResult")).toEqualTypeOf<TableResult.ImplementationClass>();
+  expectTypeOf(getDocumentClass("TableResult").documentName).toEqualTypeOf<"TableResult">();
+
+  expectTypeOf(getDocumentClass("Tile")).toEqualTypeOf<TileDocument.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Tile").documentName).toEqualTypeOf<"Tile">();
+
+  expectTypeOf(getDocumentClass("Token")).toEqualTypeOf<TokenDocument.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Token").documentName).toEqualTypeOf<"Token">();
+
+  expectTypeOf(getDocumentClass("User")).toEqualTypeOf<User.ImplementationClass>();
+  expectTypeOf(getDocumentClass("User").documentName).toEqualTypeOf<"User">();
+
+  expectTypeOf(getDocumentClass("Wall")).toEqualTypeOf<WallDocument.ImplementationClass>();
+  expectTypeOf(getDocumentClass("Wall").documentName).toEqualTypeOf<"Wall">();
+});
+
+test("getPlaceableObjectClass", () => {
+  expectTypeOf(
+    foundry.utils.getPlaceableObjectClass("AmbientLight"),
+  ).toEqualTypeOf<foundry.canvas.placeables.AmbientLight.ImplementationClass>();
+  expectTypeOf(foundry.utils.getPlaceableObjectClass("AmbientLight").embeddedName).toEqualTypeOf<"AmbientLight">();
+
+  expectTypeOf(
+    foundry.utils.getPlaceableObjectClass("AmbientSound"),
+  ).toEqualTypeOf<foundry.canvas.placeables.AmbientSound.ImplementationClass>();
+  expectTypeOf(foundry.utils.getPlaceableObjectClass("AmbientSound").embeddedName).toEqualTypeOf<"AmbientSound">();
+
+  expectTypeOf(
+    foundry.utils.getPlaceableObjectClass("Drawing"),
+  ).toEqualTypeOf<foundry.canvas.placeables.Drawing.ImplementationClass>();
+  expectTypeOf(foundry.utils.getPlaceableObjectClass("Drawing").embeddedName).toEqualTypeOf<"Drawing">();
+
+  expectTypeOf(
+    foundry.utils.getPlaceableObjectClass("Note"),
+  ).toEqualTypeOf<foundry.canvas.placeables.Note.ImplementationClass>();
+  expectTypeOf(foundry.utils.getPlaceableObjectClass("Note").embeddedName).toEqualTypeOf<"Note">();
+
+  expectTypeOf(
+    foundry.utils.getPlaceableObjectClass("Region"),
+  ).toEqualTypeOf<foundry.canvas.placeables.Region.ImplementationClass>();
+  expectTypeOf(foundry.utils.getPlaceableObjectClass("Region").embeddedName).toEqualTypeOf<"Region">();
+
+  expectTypeOf(
+    foundry.utils.getPlaceableObjectClass("Token"),
+  ).toEqualTypeOf<foundry.canvas.placeables.Token.ImplementationClass>();
+  expectTypeOf(foundry.utils.getPlaceableObjectClass("Token").embeddedName).toEqualTypeOf<"Token">();
+
+  expectTypeOf(
+    foundry.utils.getPlaceableObjectClass("Tile"),
+  ).toEqualTypeOf<foundry.canvas.placeables.Tile.ImplementationClass>();
+  expectTypeOf(foundry.utils.getPlaceableObjectClass("Tile").embeddedName).toEqualTypeOf<"Tile">();
+
+  expectTypeOf(
+    foundry.utils.getPlaceableObjectClass("Wall"),
+  ).toEqualTypeOf<foundry.canvas.placeables.Wall.ImplementationClass>();
+  expectTypeOf(foundry.utils.getPlaceableObjectClass("Wall").embeddedName).toEqualTypeOf<"Wall">();
+
+  // @ts-expect-error Not a placeable type
+  foundry.utils.getPlaceableObjectClass("Actor");
+});
+
 interface SortingStructure {
   target: number;
   update: {
@@ -250,6 +413,49 @@ interface SortingStructure {
 }
 
 declare const input: SortingStructure;
-expectTypeOf(foundry.utils.performIntegerSort(input, {})).toEqualTypeOf<
-  Array<{ target: SortingStructure; update: { sort: number } }>
->();
+declare const inputs: SortingStructure[];
+
+test("performIntegerSort", () => {
+  expectTypeOf(foundry.utils.performIntegerSort(input)).toEqualTypeOf<
+    Array<{ target: SortingStructure; update: { sort: number } }>
+  >();
+  expectTypeOf(foundry.utils.performIntegerSort(input, {})).toEqualTypeOf<
+    Array<{ target: SortingStructure; update: { sort: number } }>
+  >();
+  expectTypeOf(foundry.utils.performIntegerSort(input, { sortKey: "foo" })).toEqualTypeOf<
+    Array<{ target: SortingStructure; update: { foo: number } }>
+  >();
+  expectTypeOf(foundry.utils.performIntegerSort(input, { siblings: inputs, sortKey: "foo" })).toEqualTypeOf<
+    Array<{ target: SortingStructure; update: { foo: number } }>
+  >();
+  expectTypeOf(
+    foundry.utils.performIntegerSort(input, { siblings: inputs, sortKey: "foo", target: input }),
+  ).toEqualTypeOf<Array<{ target: SortingStructure; update: { foo: number } }>>();
+  expectTypeOf(
+    foundry.utils.performIntegerSort(input, { siblings: inputs, sortKey: "foo", target: input, sortBefore: false }),
+  ).toEqualTypeOf<Array<{ target: SortingStructure; update: { foo: number } }>>();
+  expectTypeOf(
+    foundry.utils.performIntegerSort(input, {
+      siblings: undefined,
+      sortKey: undefined,
+      target: undefined,
+      sortBefore: undefined,
+    }),
+  ).toEqualTypeOf<Array<{ target: SortingStructure; update: { sort: number } }>>();
+});
+
+test("timeSince", () => {
+  expectTypeOf(foundry.utils.timeSince("foo")).toBeString();
+  expectTypeOf(foundry.utils.timeSince(new Date())).toBeString();
+});
+
+test("getCacheBustURL", () => {
+  expectTypeOf(foundry.utils.getCacheBustURL("foo")).toEqualTypeOf<string | false>();
+});
+
+test("fetchResource", () => {
+  expectTypeOf(foundry.utils.fetchResource("foo")).toEqualTypeOf<Promise<Blob>>();
+  expectTypeOf(foundry.utils.fetchResource("foo", { bustCache: true })).toEqualTypeOf<Promise<Blob>>();
+  expectTypeOf(foundry.utils.fetchResource("foo", { bustCache: false })).toEqualTypeOf<Promise<Blob>>();
+  expectTypeOf(foundry.utils.fetchResource("foo", { bustCache: undefined })).toEqualTypeOf<Promise<Blob>>();
+});
